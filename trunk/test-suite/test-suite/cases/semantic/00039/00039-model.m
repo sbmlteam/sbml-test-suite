4c8d69ae9af225dcb156ef026be774df403d4134
skeating <skeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2008-03-10 17:43:01 +0000

    initial import

---
+++ trunk/test-suite/test-suite/cases/semantic/00039/00039-model.m
@@ -0,0 +1,47 @@
+(*
+
+category:      Test
+synopsis:      Algebraic rule used to determine rate of species change.
+componentTags: Compartment, Species, Reaction, Parameter, AlgebraicRule 
+testTags:      InitialAmount
+testtype:      TimeCourse
+levels:        1.2, 2.1, 2.2, 2.3
+
+The model contains one compartment named compartment.
+  There are two species named S1 and S2 and two parameters named k1 and k2.
+  The model contains one reaction defined as:
+[| | Reaction |||||| Rate |
+ | | ->S1     |||||| $compartment*k2*S2$  |]
+
+  The model contains one rule:
+[|| Type      || Variable || Formula |
+ || Algebraic ||   n/a    || $S1+S2-k1$  |]
+
+
+The initial conditions are as follows:
+[|                                  ||          Value  || Units                     |
+|              Initial amount of S1:|| $          0.5$ || mole                      |
+|              Initial amount of S2:|| $          0.5$ || mole                      |
+|             Value of parameter k1:|| $          1.0$ || mole litre^-1^            |
+|             Value of parameter k2:|| $            1$ || second^-1^                |
+| Volume of compartment compartment:|| $            1$ || litre                     |]
+
+The species values are given as amounts of substance to make it easier to
+use the model in a discrete stochastic simulator, but (as per usual SBML
+principles) they must be treated as concentrations where they appear in
+expressions.
+
+*)
+
+newcase[ "00039" ];
+
+addCompartment[ compartment, size -> 1 ];
+addSpecies[ S1, initialAmount -> 0.5];
+addSpecies[ S2, initialAmount -> 0.5];
+addParameter[ k1, value -> 1.0 ];
+addParameter[ k2, value -> 1 ];
+addRule[ type->AlgebraicRule, math -> S1+S2-k1];
+addReaction[ products->{S1}, modifiers->S2, reversible -> False,
+	     kineticLaw -> compartment*k2*S2 ];
+
+makemodel[]
