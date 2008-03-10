b9101604222b6a4f66d496559ca65740ab348231
skeating <skeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2008-03-10 17:21:46 +0000

    initial import

---
+++ trunk/test-suite/test-suite/cases/semantic/00033/00033-model.m
@@ -0,0 +1,46 @@
+(*
+
+category:      Test
+synopsis:      Rate rule used to determine parameter value which is used 
+               in a reaction.
+componentTags: Compartment, Species, Reaction, Parameter, RateRule 
+testTags:      InitialAmount, NonConstantParameter
+testtype:      TimeCourse
+levels:        1.2, 2.1, 2.2, 2.3
+
+The model contains one compartment named compartment.
+  There are two species named S1 and S2 and one parameter named k1.
+  The model contains one reaction defined as:
+[| | Reaction |||||| Rate                 |
+ | | S1 -> S2 |||||| $k1*S1*compartment$  |]
+
+  The model contains one rule:
+[|| Type || Variable || Formula |
+ || Rate || k1       || $0.5$   |]
+
+
+The initial conditions are as follows:
+[|                                  ||          Value  || Units                     |
+|              Initial amount of S1:|| $1.5 \x 10^-15$ || mole                      |
+|              Initial amount of S2:|| $            0$ || mole                      |
+|             Value of parameter k1:|| $            1$ || second^-1^                |
+| Volume of compartment compartment:|| $            1$ || litre                     |]
+
+The species values are given as amounts of substance to make it easier to
+use the model in a discrete stochastic simulator, but (as per usual SBML
+principles) they must be treated as concentrations where they appear in
+expressions.
+
+*)
+
+newcase[ "00033" ];
+
+addCompartment[ compartment, size -> 1 ];
+addSpecies[ S1, initialAmount -> 1.5 10^-15 ];
+addSpecies[ S2, initialAmount -> 0 ];
+addParameter[ k1, value -> 1, constant->False ];
+addRule[ type->RateRule, variable -> k1, math -> 0.5];
+addReaction[ S1 -> S2, reversible -> False,
+	     kineticLaw -> k1*S1*compartment ];
+
+makemodel[]
