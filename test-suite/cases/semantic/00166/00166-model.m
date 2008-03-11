2aaffc3d4b9271724b7c0d2ef501575a9430c75b
Sarah Keating <skeating@users.noreply.github.com>
2008-03-11 19:48:09 +0000

    initial import

---
+++ test-suite/cases/semantic/00166/00166-model.m
@@ -0,0 +1,45 @@
+(*
+
+category:      Test
+synopsis:      Model using parameters and rules only.
+componentTags: Parameter, RateRule 
+testTags:      InitialAmount, NonConstantParameter
+testtype:      TimeCourse
+levels:        2.1, 2.2, 2.3
+
+The model contains six parameters named S1, S2, S3, S4, k1 and k2.
+
+  The model contains four rules:
+[|| Type || Variable || Formula |
+ || Rate || S1       || $k2*S3*S4-k1*S1*S2$  |
+ || Rate || S2       || $k2*S3*S4-k1*S1*S2$  |
+ || Rate || S3       || $k1*S1*S2-k2*S3*S4$  |
+ || Rate || S4       || $k1*S1*S2-k2*S3*S4$  |]
+
+
+The initial conditions are as follows:
+[|                                  ||          Value  || Units                     |
+|             Value of parameter S1:|| $1.0 \x 10^-15$ || any |
+|             Value of parameter S2:|| $1.0 \x 10^-15$ || same as S1 |
+|             Value of parameter S3:|| $2.0 \x 10^-15$ || same as S1 |
+|             Value of parameter S4:|| $1.0 \x 10^-15$ || same as S1 |
+|             Value of parameter k1:|| $0.75 \x 10^15$ || (units of S1)^-1^ second^-1^ |
+|             Value of parameter k2:|| $0.25 \x 10^15$ || (units of S1)^-1^ second^-1^ |]
+
+
+*)
+
+newcase[ "00166" ];
+
+addParameter[ S1, value -> 1.0 10^-15, constant -> False];
+addParameter[ S2, value -> 1.0 10^-15, constant -> False];
+addParameter[ S3, value -> 2.0 10^-15, constant -> False];
+addParameter[ S4, value -> 1.0 10^-15, constant -> False];
+addParameter[ k1, value -> 0.75 10^15 ];
+addParameter[ k2, value -> 0.25 10^15 ];
+addRule[ type->RateRule, variable -> S1, math -> k2*S3*S4-k1*S1*S2];
+addRule[ type->RateRule, variable -> S2, math -> k2*S3*S4-k1*S1*S2];
+addRule[ type->RateRule, variable -> S3, math -> k1*S1*S2-k2*S3*S4];
+addRule[ type->RateRule, variable -> S4, math -> k1*S1*S2-k2*S3*S4];
+
+makemodel[]
