a5fe600316b73d28ce3798b25967614d9e0f6ec4
skeating <skeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2008-03-12 20:20:29 +0000

    initial import

---
+++ trunk/test-suite/test-suite/cases/semantic/00068/00068-model.m
@@ -0,0 +1,44 @@
+(*
+
+category:      Test
+synopsis:      Basic single forward reaction involving two species and a
+               stoichiometryMath element.
+componentTags: Compartment, Species, Reaction, Parameter 
+testTags:      InitialAmount, StoichiometryMath
+testtype:      TimeCourse
+levels:        2.1, 2.2, 2.3
+
+The model contains one compartment named compartment.
+  There are two species named S1 and S2 and two parameters named k1 and p1.
+  The model contains one reaction defined as:
+[| | Reaction       |||||| Rate |
+ | | S1 -> (2*p1)S2 |||||| $k1*S1*compartment$  |]
+
+
+The initial conditions are as follows:
+[|                                  ||          Value  || Units                     |
+|              Initial amount of S1:|| $          1.5$ || mole                      |
+|              Initial amount of S2:|| $            0$ || mole                      |
+|             Value of parameter k1:|| $          1.5$ || second^-1^ |
+|             Value of parameter p1:|| $            1$ ||  |
+| Volume of compartment compartment:|| $            1$ || litre                     |]
+
+The species values are given as amounts of substance to make it easier to
+use the model in a discrete stochastic simulator, but (as per usual SBML
+principles) they must be treated as concentrations where they appear in
+expressions.
+
+*)
+
+newcase[ "00068" ];
+
+addCompartment[ compartment, size -> 1 ];
+addSpecies[ S1, initialAmount -> 1.5 ];
+addSpecies[ S2, initialAmount -> 0 ];
+addParameter[ k1, value -> 1.5 ];
+addParameter[ p1, value -> 1 ];
+addReaction[ reactants->{S1}, products->{S2}, productStoichiometry->{2*p1},
+       reversible -> False,
+	     kineticLaw -> k1*S1*compartment ];
+
+makemodel[]
