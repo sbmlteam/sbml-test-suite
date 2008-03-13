28dcbf82bce46714d4aceb60057a3b06e6d15778
skeating <skeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2008-03-13 19:41:09 +0000

    initial import

---
+++ trunk/test-suite/test-suite/cases/semantic/00112/00112-model.m
@@ -0,0 +1,57 @@
+(*
+
+category:      Test
+synopsis:      Reactions occurring between two compartments with functionDefinitions.
+componentTags: Compartment, Species, Reaction, Parameter, FunctionDefinition 
+testTags:      InitialAmount, MultiCompartment
+testtype:      TimeCourse
+levels:        2.1, 2.2, 2.3
+
+The model contains two compartments named compartment and compartment1.
+  There are two species named S1 and S3 and two parameters named k1 and k2.
+  Compartment compartment contains species S1.
+  Compartment compartment1 contains species S3.
+  The model contains two reactions defined as:
+[| | Reaction |||||| Rate |
+ | | S1 -> S3  |||||| $multiply(k1,S1)*compartment$  |
+ | | S3 -> S1 |||||| $k2*(S3-S1)*compartment1$  |]
+
+The reactions occur between the species within different compartments:
+
+
+The model contains one functionDefinition defined as:
+[|| Id      | Arguments || Formula |
+ || multiply | x, y || $x*y$ |]
+
+
+The initial conditions are as follows:
+[|                                  ||          Value  || Units                     |
+|              Initial amount of S1:|| $          1.0$ || mole                      |
+|              Initial amount of S3:|| $            0$ || mole                      |
+|             Value of parameter k1:|| $         0.75$ || second^-1^ |
+|             Value of parameter k2:|| $         0.25$ || second^-1^ |
+| Volume of compartment compartment:|| $            1$ || litre                     |
+| Volume of compartment compartment1:|| $            1$ || litre                     |]
+
+The species values are given as amounts of substance to make it easier to
+use the model in a discrete stochastic simulator, but (as per usual SBML
+principles) they must be treated as concentrations where they appear in
+expressions.
+
+*)
+
+newcase[ "00112" ];
+
+addFunction[ multiply, arguments -> {x, y}, math -> x*y];
+addCompartment[ compartment, size -> 1 ];
+addCompartment[ compartment1, size -> 1 ];
+addSpecies[ S1, compartment->compartment, initialAmount -> 1.0];
+addSpecies[ S3, compartment->compartment1, initialAmount -> 0];
+addParameter[ k1, value -> 0.75 ];
+addParameter[ k2, value -> 0.25 ];
+addReaction[ S1 -> S3 , reversible -> False,
+	     kineticLaw -> multiply[k1,S1]*compartment ];
+addReaction[ S3 -> S1, reversible -> False,
+	     kineticLaw -> k2*(S3-S1)*compartment1 ];
+
+makemodel[]
