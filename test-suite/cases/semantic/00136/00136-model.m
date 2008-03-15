dcced2aa2fff9f6e0bd525fb2b7da187f30a232b
Sarah Keating <skeating@users.noreply.github.com>
2008-03-15 17:25:01 +0000

    initial import

---
+++ test-suite/cases/semantic/00136/00136-model.m
@@ -0,0 +1,51 @@
+(*
+
+category:      Test
+synopsis:      Basic single forward reaction with two species in one
+               compartment using initialAssignment to set the initial value of one species.
+componentTags: Compartment, Species, Reaction, Parameter, InitialAssignment 
+testTags:      InitialAmount
+testtype:      TimeCourse
+levels:        2.2, 2.3
+
+The model contains one compartment named compartment.
+  There are two species named S1 and S2 and two parameters named k1 and k2.
+  The model contains one reaction defined as:
+[| | Reaction |||||| Rate |
+ | | S1 -> S2 |||||| $compartment*k1*S1$  |]
+
+  The model contains one initialAssignment:
+[|| Variable || Formula |
+ || k1 || $k2/100$  |]
+
+  Note: InitialAssignments override any declared initial values.  In this case the value 
+  calculated by the initialAssignment is consistent with the value declared.
+
+
+The initial conditions are as follows:
+[|                                  ||          Value  || Units                     |
+|              Initial amount of S1:|| $            1$ || mole                      |
+|              Initial amount of S2:|| $          1.5$ || mole                      |
+|             Value of parameter k1:|| $          0.5$ || second^-1^ |
+|             Value of parameter k2:|| $           50$ || second^-1^ |
+| Volume of compartment compartment:|| $             $ || litre                     |]
+
+The species values are given as amounts of substance to make it easier to
+use the model in a discrete stochastic simulator, but (as per usual SBML
+principles) they must be treated as concentrations where they appear in
+expressions.
+
+*)
+
+newcase[ "00136" ];
+
+addCompartment[ compartment, size -> 1];
+addSpecies[ S1, initialAmount -> 1 ];
+addSpecies[ S2, initialAmount -> 1.5 ];
+addParameter[ k1, value -> .5 ];
+addParameter[ k2, value -> 50 ];
+addInitialAssignment[ k1, math -> k2/100];
+addReaction[ S1 -> S2, reversible -> False,
+	     kineticLaw -> compartment*k1*S1];
+
+makemodel[]
