869f438e13a390ac0ba23d976fcc67929b0a75b9
Sarah Keating <skeating@users.noreply.github.com>
2008-03-20 17:40:11 +0000

    initial import

---
+++ test-suite/cases/semantic/00242/00242-model.m
@@ -0,0 +1,47 @@
+(*
+
+category:      Test
+synopsis:      Basic reactions with three species in a 0-dimensional
+               compartment, with two species acting as a boundary condition.
+componentTags: Compartment, Species, Reaction, Parameter 
+testTags:      InitialAmount, 0D-Compartment, BoundaryCondition
+testtype:      TimeCourse
+levels:        2.1, 2.2, 2.3
+
+The model contains one compartment named compartment.
+  There are three species named S1, S2 and S3 and two parameters named k1 and k2.
+  Compartment compartment is  0-dimensional.
+  Both species S2 and S3 are labeled as an SBML boundary species.
+  The model contains two reactions defined as:
+[| | Reaction    |||||| Rate |
+ | | S1+S2 -> S3 |||||| $k1*S1*S2$  |
+ | | S3 -> S1+S2 |||||| $k2*S3$  |]
+
+The initial conditions are as follows:
+[|                                  ||          Value  || Units                     |
+|              Initial amount of S1:|| $1.0 \x 10^-15$ || mole                      |
+|              Initial amount of S2:|| $2.0 \x 10^-15$ || mole                      |
+|              Initial amount of S3:|| $1.0 \x 10^-15$ || mole                      |
+|             Value of parameter k1:|| $         0.75$ || mole^-1^ second^-1^ |
+|             Value of parameter k2:|| $         0.25$ || second^-1^ |]
+
+In this example the compartment has spatialDimensions set to zero,
+i.e., it represents a point and therefore cannot have size or units.  The 
+species values must be treated as amounts and not concentrations.
+
+*)
+
+newcase[ "00242" ];
+
+addCompartment[ compartment, spatialDimensions-> 0 ];
+addSpecies[ S1, initialAmount -> 1.0 10^-6];
+addSpecies[ S2, initialAmount -> 2.0 10^-6, boundaryCondition -> True ];
+addSpecies[ S3, initialAmount -> 1.0 10^-6, boundaryCondition -> True  ];
+addParameter[ k1, value -> 9.8 ];
+addParameter[ k2, value -> 0.57 ];
+addReaction[ S1+S2 -> S3, reversible -> False,
+	     kineticLaw -> k1*S1*S2 ];
+addReaction[ S3 -> S1+S2, reversible -> False,
+	     kineticLaw -> k2*S3 ];
+
+makemodel[]
