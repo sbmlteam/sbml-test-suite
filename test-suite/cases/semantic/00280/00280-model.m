88fd02184d050054516ff9ecdd0a34fcf5354c2b
Sarah Keating <skeating@users.noreply.github.com>
2008-03-26 16:54:41 +0000

    initial import

---
+++ test-suite/cases/semantic/00280/00280-model.m
@@ -0,0 +1,49 @@
+(*
+
+category:      Test
+synopsis:      Basic single forward reaction with two species in a 2D
+               compartment and nonzero initial amounts using a function to apply 
+               the kinetic law.
+componentTags: Compartment, Species, Reaction, Parameter, FunctionDefinition 
+testTags:      InitialAmount, 2D_Compartment, NonUnityCompartment
+testtype:      TimeCourse
+levels:        2.1, 2.2, 2.3
+
+The model contains one compartment named compartment.
+  There are two species named S1 and S2 and one parameter named k1.
+  Compartment compartment is 2-dimensional.
+  The model contains one reaction defined as:
+[| | Reaction |||||| Rate |
+ | | S1 -> S2 |||||| $compartment*multiply(k1,S1)$  |]
+
+
+The model contains one functionDefinition defined as:
+[|| Id      | Arguments || Formula |
+ || multiply | x, y || $x*y$ |]
+
+
+The initial conditions are as follows:
+[|                                  ||          Value  || Units                     |
+|              Initial amount of S1:|| $1.5 \x 10^-12$ || mole                      |
+|              Initial amount of S2:|| $0.5 \x 10^-12$ || mole                      |
+|             Value of parameter k1:|| $         1.75$ || second^-1^ |
+|   Area of compartment compartment:|| $          3.5$ || metre^2^                  |]
+
+The species values are given as amounts of substance to make it easier to
+use the model in a discrete stochastic simulator, but (as per usual SBML
+principles) they must be treated as concentrations where they appear in
+expressions.
+
+*)
+
+newcase[ "00280" ];
+
+addFunction[ multiply, arguments -> {x, y}, math -> x*y];
+addCompartment[ compartment, spatialDimensions -> 2, size -> 3.5 ];
+addSpecies[ S1, initialAmount -> 1.5 10^-12 ];
+addSpecies[ S2, initialAmount -> 0.5 10^-12 ];
+addParameter[ k1, value -> 1.75 ];
+addReaction[ S1 -> S2, reversible -> False,
+	     kineticLaw -> compartment*multiply[k1,S1] ];
+
+makemodel[]
