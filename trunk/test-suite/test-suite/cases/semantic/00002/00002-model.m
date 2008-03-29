a96834ca27cdab3bcff5ef2d40cd6244589add2f
mhucka <mhucka@c05c2498-8750-0410-b44c-b7d1844e3258>
2008-03-29 05:25:55 +0000

    Small edits to the descriptions.

--- trunk/test-suite/test-suite/cases/semantic/00002/00002-model.m
+++ trunk/test-suite/test-suite/cases/semantic/00002/00002-model.m
@@ -8,30 +8,30 @@ testTags:      InitialAmount, ZeroRate
 testType:      TimeCourse
 levels:        1.2, 2.1, 2.2, 2.3
 
-The model contains one compartment named compartment.
-  There are two species named S1 and S2 and two parameters named k1 and k2.
-  The model contains two reactions defined as:
+The model contains one compartment called "compartment".  There are two
+species called S1 and S2 and two parameters called k1 and k2.  The model
+contains two reactions defined as:
 
 [{width:30em,margin-left:5em}|  *Reaction*  |  *Rate*  |
 | S1 -> S2 | $k1 * S1 * compartment$  |
 | S2 -> S1 | $k2 * S2 * compartment$  |]
 
-  The value of k2 is zero, which means the 
-reverse reaction should occur at zero rate (i.e. not at all).  
+The value of k2 is zero, which means the reverse reaction should occur at
+zero rate (i.e., not at all).
 
 The initial conditions are as follows:
 
-[{width:30em,margin-left:5em}| |  *Value*  |  *Units*  |
-|              Initial amount of S1:| $1.5 \x 10^-15$  | mole           |
-|              Initial amount of S2:| $0 \x$           | mole           |
-|             Value of parameter k1:| $1$              | second^-1^     |
-|             Value of parameter k2:| $0$              | second^-1^     |
-| Volume of compartment compartment:| $1$              | litre          |]
+[{width:30em,margin-left:5em}|     |*Value*          |*Units*        |
+|Initial amount of S1              |$1.5 \x 10^-15$  |mole           |
+|Initial amount of S2              |$0 \x$           |mole           |
+|Value of parameter k1             |$1$              |second^-1^     |
+|Value of parameter k2             |$0$              |second^-1^     |
+|Volume of compartment compartment |$1$              |litre          |]
 
-The species values are given as amounts of substance to make it easier to
+The species' values are given as amounts of substance to make it easier to
 use the model in a discrete stochastic simulator, but (as per usual SBML
-principles) they must be treated as concentrations where they appear in
-expressions.
+principles) their symbols represent the value in concentration units where
+they appear in expressions.
 
 *)
 
