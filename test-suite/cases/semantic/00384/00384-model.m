2177f50209c5ababc21693e01db754a5ab487254
Sarah Keating <skeating@users.noreply.github.com>
2008-04-15 16:00:17 +0000

    initial import

---
+++ test-suite/cases/semantic/00384/00384-model.m
@@ -0,0 +1,58 @@
+(* 
+
+category:      Test
+synopsis:      Basic two reactions with four species in one compartment
+               and one event that assigns value to a species.
+componentTags: Compartment, Species, Reaction, Parameter, EventNoDelay 
+testTags:      InitialAmount, ConstantSpecies
+testType:      TimeCourse
+levels:        2.1, 2.2, 2.3
+
+The model contains one compartment called C.
+  There are four species called S1, S2, S3 and S4 and two parameters called k1 and k2.
+  Species S4 is labeled as constant and therefore cannot be changed by rules or reactions.
+  The model contains two reactions defined as:
+
+[{width:30em,margin-left:5em}|  *Reaction*  |  *Rate*  |
+| S1 + S2 -> S3 | $k1 * S1 * S2 * C$  |
+| S3 -> S1 + S2 | $k2 * S3 * S4 * C$  |]
+
+The model contains one event that assigns value to species S1 defined as:
+
+[{width:30em,margin-left:5em}|        | Trigger    | Delay | Assignments |
+ | Event1 | $S4 > S2$ | $-$   | $S1 -> 2 \x 10^-15$    |]
+
+The initial conditions are as follows:
+
+[{width:30em,margin-left:5em}| |  *Value*  |  *Units*  |
+|              Initial amount of S1:| $1.0 \x 10^-15$  | mole                       |
+|              Initial amount of S2:| $1.0 \x 10^-15$  | mole                       |
+|              Initial amount of S3:| $2.0 \x 10^-15$  | mole                       |
+|              Initial amount of S4:| $1.0 \x 10^-15$  | mole                       |
+|             Value of parameter k1:| $0.75 \x 10^15$  | litre mole^-1^ second^-1^  |
+|             Value of parameter k2:| $0.25 \x 10^15$  | litre mole^-1^ second^-1^  |
+| Volume of compartment C:| $1$              | litre                      |]
+
+The species values are given as amounts of substance to make it easier to
+use the model in a discrete stochastic simulator, but (as per usual SBML
+principles) they must be treated as concentrations where they appear in
+expressions.
+
+*)
+
+newcase[ "00384" ];
+
+addCompartment[ C, size -> 1 ];
+addSpecies[ S1, initialAmount -> 1.0 10^-15];
+addSpecies[ S2, initialAmount -> 1.2 10^-15];
+addSpecies[ S3, initialAmount -> 2.0 10^-15];
+addSpecies[ S4, initialAmount -> 1.0 10^-15, constant -> True];
+addParameter[ k1, value -> 0.75 10^15 ];
+addParameter[ k2, value -> 0.25 10^15 ];
+addReaction[ S1 + S2 -> S3, reversible -> False,
+	     kineticLaw -> k1 * S1 * S2 * C ];
+addReaction[ S3 -> S1 + S2, reversible -> False,
+	     kineticLaw -> k2 * S3 * S4 * C ];
+addEvent[ trigger -> S4 > S2, eventAssignment -> S1->2/10^15 ];
+
+makemodel[]
