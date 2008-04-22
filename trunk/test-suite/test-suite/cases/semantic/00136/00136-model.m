416ff2f99ea1658e7d3520b7dbd83486e5a25014
mhucka <mhucka@c05c2498-8750-0410-b44c-b7d1844e3258>
2008-04-22 19:24:22 +0000

    Various text reformatting and minor edits.

--- trunk/test-suite/test-suite/cases/semantic/00136/00136-model.m
+++ trunk/test-suite/test-suite/cases/semantic/00136/00136-model.m
@@ -2,41 +2,41 @@
 
 category:      Test
 synopsis:      Basic single forward reaction with two species in one
-               compartment using initialAssignment to set the initial value of one parameter.
+compartment using initialAssignment to set the initial value of one parameter.
 componentTags: Compartment, Species, Reaction, Parameter, InitialAssignment 
 testTags:      InitialAmount
 testType:      TimeCourse
 levels:        2.2, 2.3
 
-The model contains one compartment named compartment.
-  There are two species named S1 and S2 and two parameters named k1 and k2.
-  The model contains one reaction defined as:
+The model contains one compartment called "compartment".  There are two
+species named S1 and S2 and two parameters named k1 and k2.  The model
+contains one reaction defined as:
 
 [{width:30em,margin-left:5em}|  *Reaction*  |  *Rate*  |
 | S1 -> S2 | $compartment * k1 * S1$  |]
 
-  The model contains one initialAssignment:
+The model contains one initialAssignment:
 
 [{width:30em,margin-left:5em}| Variable | Formula |
  | k1 | $k2/100$  |]
 
-  Note: InitialAssignments override any declared initial values.  In this case the value 
-  calculated by the initialAssignment is consistent with the value declared.
-
+Note: InitialAssignments override any declared initial values.  In this
+case the value calculated by the initialAssignment is consistent with the
+value declared.
 
 The initial conditions are as follows:
 
-[{width:30em,margin-left:5em}| |  *Value*  |  *Units*  |
-|              Initial amount of S1:| $            1$ | mole                      |
-|              Initial amount of S2:| $          1.5$ | mole                      |
-|             Value of parameter k1:| $          0.5$ | second^-1^ |
-|             Value of parameter k2:| $           50$ | second^-1^ |
-| Volume of compartment compartment:| $             $ | litre                     |]
+[{width:30em,margin-left:5em}|       |*Value*         |*Units*  |
+|Initial amount of S1                |$            1$ |mole                      |
+|Initial amount of S2                |$          1.5$ |mole                      |
+|Value of parameter k1               |$          0.5$ |second^-1^ |
+|Value of parameter k2               |$           50$ |second^-1^ |
+|Volume of compartment "compartment" |$             $ |litre                     |]
 
 The species values are given as amounts of substance to make it easier to
 use the model in a discrete stochastic simulator, but (as per usual SBML
-principles) they must be treated as concentrations where they appear in
-expressions.
+principles) their symbols represent their values in concentration units
+where they appear in expressions.
 
 *)
 
