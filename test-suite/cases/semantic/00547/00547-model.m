13696f17690398ce25da8479f952949bd418c827
Sarah Keating <skeating@users.noreply.github.com>
2008-04-30 19:04:20 +0000

    initial import

---
+++ test-suite/cases/semantic/00547/00547-model.m
@@ -0,0 +1,56 @@
+(* 
+
+category:      Test
+synopsis:      Two reactions with three species in one compartment, 
+with an algebraic rule used to determine value of the compartment size.
+componentTags: Compartment, Species, Reaction, Parameter, AlgebraicRule 
+testTags:      InitialAmount, NonUnityCompartment
+testType:      TimeCourse
+levels:        1.2, 2.1, 2.2, 2.3
+
+The model contains one compartment called C.  There are three
+species called X0, X1 and T and two parameters called k1 and k2.  The model
+contains two reactions defined as:
+
+[{width:30em,margin-left:5em}|  *Reaction*  |  *Rate*  |
+| X0 -> T     | $C * k1 * X0$  |
+| T -> X1     | $C * k2 * T$  |]
+
+The model contains one rule which must be used to determine the value of
+compartment C:
+
+[{width:30em,margin-left:5em}|  *Type*  |  *Variable*  |  *Formula*  |
+ | Algebraic |   n/a    | $C - 2.5$  |]
+
+The initial conditions are as follows:
+
+[{width:30em,margin-left:5em}| |*Value* |*Units*  |
+|Initial amount of X0          |$1$     |mole                      |
+|Initial amount of X1          |$0$     |mole                      |
+|Initial amount of T           |$0$     |mole                      |
+|Value of parameter k1         |$0.1$   |second^-1^            |
+|Value of parameter k2         |$0.375$ |second^-1^                |
+|Volume of compartment C       |$undeclared$     |litre                     |]
+
+The species values are given as amounts of substance to make it easier to
+use the model in a discrete stochastic simulator, but (as per usual SBML
+principles) their symbols represent their values in concentration units
+where they appear in expressions.
+
+*)
+
+newcase[ "00547" ];
+
+addCompartment[ C, constant -> False ];
+addSpecies[ X0, initialAmount -> 1];
+addSpecies[ X1, initialAmount -> 0];
+addSpecies[ T, initialAmount -> 0];
+addParameter[ k1, value -> 0.1];
+addParameter[ k2, value -> 0.375 ];
+addRule[ type->AlgebraicRule, math -> C - 2.5];
+addReaction[ X0 -> T, reversible -> False,
+	     kineticLaw -> C * k1 * X0 ];
+addReaction[ T -> X1, reversible -> False,
+	     kineticLaw -> C * k2 *T ];
+
+makemodel[]
