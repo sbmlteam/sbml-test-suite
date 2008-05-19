3b41f1d004cfb006e58bf37e36bb35a5ad7a1740
skeating <skeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2008-05-19 18:10:10 +0000

    initial import

---
+++ trunk/test-suite/test-suite/cases/semantic/00668/00668-model.m
@@ -0,0 +1,69 @@
+(* 
+
+category:      Test
+synopsis:      Two reactions with four species in one
+compartment using an assignmentRule with a functionDefinition to vary one species.
+componentTags: Compartment, Species, Reaction, Parameter, FunctionDefinition, AssignmentRule 
+testTags:      InitialAmount, NonUnityCompartment
+testType:      TimeCourse
+levels:        2.1, 2.2, 2.3
+generatedBy:   Numeric
+
+The model contains one compartment called C.  There are four
+species called S1, S2, S3 and S4 and three parameters called k1, k2 and p1.
+The model contains two reactions defined as:
+
+[{width:30em,margin-left:5em}|  *Reaction*  |  *Rate*  |
+| S1 + S2 -> S3 | $k1 * S1 * S2 * C$  |
+| S3 -> S1 + S2 | $k2 * S3 * C$  |]
+
+The model contains one rule:
+
+[{width:30em,margin-left:5em}|  *Type*  |  *Variable*  |  *Formula*  |
+ | Assignment                           | S4           | $multiply(p1, S2)$  |]
+
+The model contains one FunctionDefinition defined as:
+
+[{width:30em,margin-left:5em}|  *Id*  |  *Arguments*  |  *Formula*  |
+ | multiply | x, y | $x * y$ |]
+
+Note that in this case the initial value of the species S4 is not explicitly
+declared and must be calculated by the AssignmentRule. 
+
+The initial conditions are as follows:
+
+[{width:30em,margin-left:5em}|       |*Value*          |*Units*  |
+|Initial amount of S1                |$1.0 \x 10^-12$ |mole                      |
+|Initial amount of S2                |$2.0 \x 10^-12$ |mole                      |
+|Initial amount of S3                |$1.5 \x 10^-12$ |mole                      |
+|Initial amount of S4                |$   undeclared$ |mole                      |
+|Value of parameter k1               |$0.75 \x 10^12$ |litre mole^-1^ second^-1^ |
+|Value of parameter k2               |$0.25 \x 10^-6$ |second^-1^ |
+|Value of parameter p1               |$         0.75$ |dimensionless |
+|Volume of compartment C             |$         0.86$ |litre                     |]
+
+The species values are given as amounts of substance to make it easier to
+use the model in a discrete stochastic simulator, but (as per usual SBML
+principles) their symbols represent their values in concentration units
+where they appear in expressions.
+
+*)
+
+newcase[ "00668" ];
+
+addFunction[ multiply, arguments -> {x, y}, math -> x * y];
+addCompartment[ C, size -> 0.86];
+addSpecies[ S1, initialAmount -> 1.0 10^-12];
+addSpecies[ S2, initialAmount -> 2.0 10^-12];
+addSpecies[ S3, initialAmount -> 1.5 10^-12];
+addSpecies[ S4 ];
+addParameter[ k1, value -> .75 10^12];
+addParameter[ k2, value -> .25 10^-6];
+addParameter[ p1, value -> 0.75];
+addRule[ type->AssignmentRule, variable -> S4, math ->multiply[p1,S2]];
+addReaction[ S1 + S2 -> S3, reversible -> False,
+	     kineticLaw -> k1 * S1 * S2 * C ];
+addReaction[ S3 -> S1 + S2, reversible -> False,
+	     kineticLaw -> k2 * S3 * C ];
+
+makemodel[]
