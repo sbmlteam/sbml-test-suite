589c058e440db1ce5cada4dfe701242d9cfd5428
Sarah Keating <skeating@users.noreply.github.com>
2008-05-23 17:02:38 +0000

    updated description

--- test-suite/cases/semantic/00312/00312-model.m
+++ test-suite/cases/semantic/00312/00312-model.m
@@ -9,8 +9,8 @@ testType:      TimeCourse
 levels:        1.2, 2.1, 2.2, 2.3
 generatedBy:   Numeric
 
-The model contains one compartment called "compartment".  There are two
-species named S1 and S2 and two parameters named k1 and p1.  The model
+The model contains one varying compartment called "compartment".  There are two
+species called S1 and S2 and two parameters called k1 and p1.  The model
 contains one reaction defined as:
 
 [{width:30em,margin-left:5em}|  *Reaction*  |  *Rate*  |
