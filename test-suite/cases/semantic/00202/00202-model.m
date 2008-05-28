466a619d81e813059e556ea55849f3426d2cd615
Sarah Keating <skeating@users.noreply.github.com>
2008-05-28 17:18:29 +0000

    updated results to be those generated analytically and added sentence to description

--- test-suite/cases/semantic/00202/00202-model.m
+++ test-suite/cases/semantic/00202/00202-model.m
@@ -7,7 +7,7 @@ componentTags: Compartment, Species, Reaction, Parameter
 testTags:      InitialAmount, 2D-Compartment, NonUnityCompartment
 testType:      TimeCourse
 levels:        2.1, 2.2, 2.3
-generatedBy:   Numeric
+generatedBy:   Analytic
 
 The model contains one compartment called "compartment".  There are two
 species named S1 and S2 and one parameter named k1.  Compartment
@@ -29,6 +29,9 @@ The species values are given as amounts of substance to make it easier to
 use the model in a discrete stochastic simulator, but (as per usual SBML
 principles) their symbols represent their values in concentration units
 where they appear in expressions.
+
+Note: The test data for this model was generated from an analytical solution
+of the system of equations.
 
 *)
 
