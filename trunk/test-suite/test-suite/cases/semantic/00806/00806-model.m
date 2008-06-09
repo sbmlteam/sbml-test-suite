99e0cfd7cd00aa75e7db6d0a7cd2ebe309e1e1b3
skeating <skeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2008-06-09 23:21:09 +0000

    initial import

---
+++ trunk/test-suite/test-suite/cases/semantic/00806/00806-model.m
@@ -0,0 +1,43 @@
+(* 
+
+category:      Test
+synopsis:      Third order mass action kinetics with one reactant.
+componentTags: Compartment, Species, Reaction, Parameter 
+testTags:      InitialAmount
+testType:      TimeCourse
+levels:        1.2, 2.1, 2.2, 2.3
+generatedBy:   Numeric
+
+The model contains one compartment called C.  There are two species called 
+S1 and S2 and one parameter called k.  The model contains one reaction that has
+a kinetic law that defines third order mass action kinetics with one reactant
+(as referenced by SBO:0000057):
+
+[{width:30em,margin-left:5em}|  *Reaction*  |  *Rate*  |
+| S1 -> S2 | $k * S1 * S1 * S1 * C$  |]
+
+The initial conditions are as follows:
+
+[{width:30em,margin-left:5em}| |*Value*       |*Units*  |
+|Initial concentration of S1        |$1.0$ |mole                      |
+|Initial concentration of S2        |$0$ |mole                      |
+|Value of parameter k       |$0.65$          |litre^2^ mole^-2^ second^-1^ |
+|Volume of compartment C     |$1$             |litre                  |]
+
+The species values are given as amounts of substance to make it easier to
+use the model in a discrete stochastic simulator, but (as per usual SBML
+principles) their symbols represent their values in concentration units
+where they appear in expressions.
+
+*)
+
+newcase[ "00806" ];
+
+addCompartment[ C, size -> 1 ];
+addSpecies[ S1, initialConcentration -> 1.0];
+addSpecies[ S2, initialConcentration -> 0];
+addParameter[ k, value -> 0.65 ];
+addReaction[ S1 -> S2, reversible -> False,
+	     kineticLaw -> k * S1 * S1 * S1 * C ];
+
+makemodel[]
