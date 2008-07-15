3343c69cff38888e08b1a0f235ecf1bf706751a0
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2008-07-15 10:08:38 +0000

    changed compartment id to C

--- trunk/test-suite/cases/semantic/00052/00052-model.m
+++ trunk/test-suite/cases/semantic/00052/00052-model.m
@@ -9,19 +9,19 @@ testType:      TimeCourse
 levels:        1.2, 2.1, 2.2, 2.3
 generatedBy:   Numeric
 
-The model contains one varying compartment called "compartment".  There are three
+The model contains one varying compartment called C.  There are three
 species called S1, S2 and S3 and three parameters called k1, k2 and p1.  The
 model contains two reactions defined as:
 
 [{width:30em,margin-left:5em}|  *Reaction*  |  *Rate*  |
-| S1 + S2 -> S3 | $k1 * S1 * S2 * compartment$  |
-| S3 -> S1 + S2 | $k2 * S3 * compartment$  |]
+| S1 + S2 -> S3 | $k1 * S1 * S2 * C$  |
+| S3 -> S1 + S2 | $k2 * S3 * C$  |]
 
 The model contains one rule which defines the rate at which the volume of
 the compartment is changing:
 
 [{width:30em,margin-left:5em}|  *Type*  |  *Variable*  |  *Formula*  |
- | Rate | compartment | $-p1 * compartment$  |]
+ | Rate | C | $-p1 * C$  |]
 
 The initial conditions are as follows:
 
@@ -32,7 +32,7 @@ The initial conditions are as follows:
 |Value of parameter k1               |$         0.75$ |litre mole^-1^ second^-1^ |
 |Value of parameter k2               |$         0.25$ |second^-1^ |
 |Value of parameter p1               |$          0.1$ |second^-1^ |
-|Volume of compartment "compartment" |$            1$ |litre                     |]
+|Volume of compartment C |$            1$ |litre                     |]
 
 The species values are given as amounts of substance to make it easier to
 use the model in a discrete stochastic simulator, but (as per usual SBML
@@ -43,17 +43,17 @@ where they appear in expressions.
 
 newcase[ "00052" ];
 
-addCompartment[ compartment, constant->False, size -> 1 ];
+addCompartment[ C, constant->False, size -> 1 ];
 addSpecies[ S1, initialAmount -> 1.0];
 addSpecies[ S2, initialAmount -> 2.0];
 addSpecies[ S3, initialAmount -> 1.0];
 addParameter[ k1, value -> 0.75 ];
 addParameter[ k2, value -> 0.25 ];
 addParameter[ p1, value -> 0.1 ];
-addRule[ type->RateRule, variable -> compartment, math -> -p1 * compartment];
+addRule[ type->RateRule, variable -> C, math -> -p1 * C];
 addReaction[ S1 + S2 -> S3, reversible -> False,
-	     kineticLaw -> k1 * S1 * S2 * compartment ];
+	     kineticLaw -> k1 * S1 * S2 * C ];
 addReaction[ S3 -> S1 + S2, reversible -> False,
-	     kineticLaw -> k2 * S3 * compartment ];
+	     kineticLaw -> k2 * S3 * C ];
 
 makemodel[]
