0bc488e6ced93649f5a378151ff657f1ef1f02d9
Sarah Keating <skeating@users.noreply.github.com>
2008-08-11 10:44:06 +0000

    initial import syntactic tests involving <notes>

---
+++ cases/syntactic/00021/00021-model.m
@@ -0,0 +1,32 @@
+category:      SBML_ANNOTATION
+synopsis:      SBML Namespace in an annotation.
+levels:        2.2, 2.3
+status:        Fail
+
+This model fails SBML validation rule *10403*:     
+
+Top-level elements within an *%<%annotation%>%* element cannot use any SBML
+namespace, whether explicitly (by declaring the namespace to be one of
+the URIs "http://www.sbml.org/sbml/level1",
+"http://www.sbml.org/sbml/level2",
+"http://www.sbml.org/sbml/level2/version2", or
+"http://www.sbml.org/sbml/level2/version3", or implicitly (by failing 
+to declare any namespace).
+
+References: L2V2 Section 3.3.3; L2V3 Section 3.2.4. 
+
+
+The *%<%annotation%>%* element declares the sbml namespace on line 7:
+
+{>}%<%annotation%>%\\
+%<%rdf:RDF xmlns:sbml="http://www.sbml.org/sbml/level2" ...
+\\
+\\
+----
+
+[{width:10em}| | |
+|2> *TEST SUMMARY* |
+| *Category:*| SBML_ANNOTATION  |
+| *Status:*| Fail  |
+| *Levels:*| L2V2 |
+| | L2V3 |]
---
+++ cases/syntactic/00029/00029-model.m
@@ -0,0 +1,28 @@
+category:      SBML_NOTES
+synopsis:      Invalid notes contents.
+levels:        2.2, 2.3
+status:        Pass
+
+This model passes SBML validation rule *10804*:     
+
+The XHTML content inside a *%<%notes%>%* element can only take one of the
+following general forms: (1) a complete XHTML document beginning with
+the element *%<%html%>%* and ending with *%<%/html%>%*; (2) the "body" portion of
+a document beginning with the element *%<%body%>%* and ending with *%<%/body%>%*; or
+(3) XHTML content that is permitted within a *%<%body%>%* ... *%<%/body%>%* element.
+
+References: L2V2 Section 3.3.2; L2V3 Section 3.2.3.
+
+
+Note: In case (3) above, if there are multiple elements, each element must 
+explicitly declare the XHTML namespace.
+\\
+\\
+----
+
+[{width:10em}| | |
+|2> *TEST SUMMARY* |
+| *Category:*| SBML_NOTES  |
+| *Status:*| Pass  |
+| *Levels:*| L2V2 |
+| | L2V3 |]
