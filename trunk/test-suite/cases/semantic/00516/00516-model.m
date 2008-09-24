14127285022ee4df6ebaf6f76d20e1b6affc716c
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2008-09-24 23:46:57 +0000

    produced analytical solution and tagged model accordingly

--- trunk/test-suite/cases/semantic/00516/00516-model.m
+++ trunk/test-suite/cases/semantic/00516/00516-model.m
@@ -7,7 +7,7 @@ componentTags: Compartment, Species, Reaction, Parameter, InitialAssignment
 testTags:      InitialAmount, StoichiometryMath
 testType:      TimeCourse
 levels:        2.2, 2.3
-generatedBy:   Numeric
+generatedBy:   Analytic
 
 The model contains one compartment called C.  There are two
 species called S1 and S2 and three parameters called k1, k2 and p1.  The model contains 
@@ -39,6 +39,9 @@ The species values are given as amounts of substance to make it easier to
 use the model in a discrete stochastic simulator, but (as per usual SBML
 principles) their symbols represent their values in concentration units
 where they appear in expressions.
+
+Note: The test data for this model was generated from an analytical solution
+of the system of equations.
 
 *)
 
