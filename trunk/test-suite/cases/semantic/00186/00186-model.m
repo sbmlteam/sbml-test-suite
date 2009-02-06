3497a2a0652e2c0564439f94210e889a5d683c4a
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2009-02-06 22:46:02 +0000

    changes values used so that tolerance does not exceed machine double precision

--- trunk/test-suite/cases/semantic/00186/00186-model.m
+++ trunk/test-suite/cases/semantic/00186/00186-model.m
@@ -23,10 +23,10 @@ reverse reaction should occur at zero rate (i.e. not at all).
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*          |*Units*  |
-|Initial amount of S1                |$1.0 \x 10^-15$ |mole                      |
-|Initial amount of S2                |$1.1 \x 10^-15$ |mole                      |
-|Initial amount of S3                |$1.0 \x 10^-15$ |mole                      |
-|Value of parameter k1               |$0.75 \x 10^15$ |litre mole^-1^ second^-1^ |
+|Initial amount of S1                |$1.0 \x 10^-4$ |mole                      |
+|Initial amount of S2                |$1.1 \x 10^-4$ |mole                      |
+|Initial amount of S3                |$1.0 \x 10^-4$ |mole                      |
+|Value of parameter k1               |$0.75 \x 10^4$ |litre mole^-1^ second^-1^ |
 |Value of parameter k2               |$            0$ |second^-1^ |
 |Volume of compartment "compartment" |$            1$ |litre                     |]
 
@@ -43,10 +43,10 @@ of the system of equations.
 newcase[ "00186" ];
 
 addCompartment[ compartment, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.0 10^-15];
-addSpecies[ S2, initialAmount -> 1.1 10^-15];
-addSpecies[ S3, initialAmount -> 1.0 10^-15];
-addParameter[ k1, value -> 0.75 10^15 ];
+addSpecies[ S1, initialAmount -> 1.0 10^-4];
+addSpecies[ S2, initialAmount -> 1.1 10^-4];
+addSpecies[ S3, initialAmount -> 1.0 10^-4];
+addParameter[ k1, value -> 0.75 10^4 ];
 addParameter[ k2, value -> 0 ];
 addReaction[ S1 + S2 -> S3, reversible -> False,
 	     kineticLaw -> k1 * S1 * S2 * compartment ];
--- trunk/test-suite/cases/semantic/00188/00188-model.m
+++ trunk/test-suite/cases/semantic/00188/00188-model.m
@@ -23,11 +23,11 @@ zero rate (i.e. not at all).
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*          |*Units*  |
-|Initial amount of S1                |$1.0 \x 10^-15$ |mole                      |
-|Initial amount of S2                |$1.5 \x 10^-15$ |mole                      |
-|Initial amount of S3                |$2.0 \x 10^-15$ |mole                      |
-|Initial amount of S4                |$1.0 \x 10^-15$ |mole                      |
-|Value of parameter k1               |$0.75 \x 10^15$ |litre mole^-1^ second^-1^ |
+|Initial amount of S1                |$1.0 \x 10^-4$ |mole                      |
+|Initial amount of S2                |$1.5 \x 10^-4$ |mole                      |
+|Initial amount of S3                |$2.0 \x 10^-4$ |mole                      |
+|Initial amount of S4                |$1.0 \x 10^-4$ |mole                      |
+|Value of parameter k1               |$0.75 \x 10^4$ |litre mole^-1^ second^-1^ |
 |Value of parameter k2               |$            0$ |litre mole^-1^ second^-1^ |
 |Volume of compartment "compartment" |$            1$ |litre                     |]
 
@@ -44,11 +44,11 @@ of the system of equations.
 newcase[ "00188" ];
 
 addCompartment[ compartment, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.0 10^-15];
-addSpecies[ S2, initialAmount -> 1.5 10^-15];
-addSpecies[ S3, initialAmount -> 2.0 10^-15];
-addSpecies[ S4, initialAmount -> 1.0 10^-15];
-addParameter[ k1, value -> 0.75 10^15 ];
+addSpecies[ S1, initialAmount -> 1.0 10^-4];
+addSpecies[ S2, initialAmount -> 1.5 10^-4];
+addSpecies[ S3, initialAmount -> 2.0 10^-4];
+addSpecies[ S4, initialAmount -> 1.0 10^-4];
+addParameter[ k1, value -> 0.75 10^4 ];
 addParameter[ k2, value -> 0 ];
 addReaction[ S1 + S2 -> S3 + S4, reversible -> False,
 	     kineticLaw -> k1 * S1 * S2 * compartment ];
