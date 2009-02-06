05b1c6aceff3c52333fa5783943f3c6f0551ccd3
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2009-02-06 18:51:15 +0000

    changes values used so that tolerance does not exceed machine double precision

--- trunk/test-suite/cases/semantic/00103/00103-model.m
+++ trunk/test-suite/cases/semantic/00103/00103-model.m
@@ -27,12 +27,12 @@ The model contains two reactions defined as:
 The initial conditions are as follows:
 
 [{width:25em,margin-left:5em}|      |*Value*            |*Units*              |
-|Initial amount of S1  |$1.0 \x 10^-15$ |mole                |
-|Initial amount of S2  |$1.0 \x 10^-15$ |mole                |
-|Initial amount of S3  |$2.0 \x 10^-15$ |mole                |
-|Initial amount of S4  |$1.0 \x 10^-15$ |mole                |
-|Value of parameter k1 |$0.75 \x 10^15$ |mole^-1^ second^-1^ |
-|Value of parameter k2 |$0.25 \x 10^15$ |mole^-1^ second^-1^ |]
+|Initial amount of S1  |$1.0 \x 10^-4$ |mole                |
+|Initial amount of S2  |$1.0 \x 10^-4$ |mole                |
+|Initial amount of S3  |$2.0 \x 10^-4$ |mole                |
+|Initial amount of S4  |$1.0 \x 10^-4$ |mole                |
+|Value of parameter k1 |$0.75 \x 10^4$ |mole^-1^ second^-1^ |
+|Value of parameter k2 |$0.25 \x 10^4$ |mole^-1^ second^-1^ |]
 
 In this example, the compartment has its spatialDimensions attribute set to
 a value of zero; i.e., the compartment represents a point, and therefore
@@ -46,12 +46,12 @@ newcase[ "00103" ];
 
 addFunction[ multiply, arguments -> {x, y}, math -> x * y];
 addCompartment[ compartment, spatialDimensions-> 0 ];
-addSpecies[ S1, initialAmount -> 1.0 10^-15];
-addSpecies[ S2, initialAmount -> 1.0 10^-15];
-addSpecies[ S3, initialAmount -> 2.0 10^-15];
-addSpecies[ S4, initialAmount -> 1.0 10^-15];
-addParameter[ k1, value -> 0.75 10^15 ];
-addParameter[ k2, value -> 0.25 10^15 ];
+addSpecies[ S1, initialAmount -> 1.0 10^-4];
+addSpecies[ S2, initialAmount -> 1.0 10^-4];
+addSpecies[ S3, initialAmount -> 2.0 10^-4];
+addSpecies[ S4, initialAmount -> 1.0 10^-4];
+addParameter[ k1, value -> 0.75 10^4 ];
+addParameter[ k2, value -> 0.25 10^4 ];
 addReaction[ S1 + S2 -> S3 + S4, reversible -> False,
 	     kineticLaw -> multiply[k1,multiply[S1,S2]] ];
 addReaction[ S3 + S4 -> S1 + S2, reversible -> False,
