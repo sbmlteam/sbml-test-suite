94d212d3f720699daea12a6b2d6d34dc5b0b9366
Sarah Keating <skeating@users.noreply.github.com>
2009-02-06 22:38:04 +0000

    changes values used so that tolerance does not exceed machine double precision

--- cases/semantic/00178/00178-model.m
+++ cases/semantic/00178/00178-model.m
@@ -29,9 +29,9 @@ The model contains one functionDefinition defined as:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|      |*Value*          |*Units*  |
-|Value of parameter S1 |$1.0 \x 10^-15$ |any |
-|Value of parameter S2 |$2.0 \x 10^-15$ |same as S1 |
-|Value of parameter S3 |$1.0 \x 10^-15$ |same as S1 |
+|Value of parameter S1 |$1.0 \x 10^-1$ |any |
+|Value of parameter S2 |$2.0 \x 10^-1$ |same as S1 |
+|Value of parameter S3 |$1.0 \x 10^-1$ |same as S1 |
 |Value of parameter k1 |$         0.75$ |(units of S1)^-1^ second^-1^ |
 |Value of parameter k2 |$         0.25$ |second^-1^ |]
 
@@ -41,9 +41,9 @@ The initial conditions are as follows:
 newcase[ "00178" ];
 
 addFunction[ multiply, arguments -> {x, y, z}, math -> x * y * z];
-addParameter[ S1, value -> 1.0 10^-15, constant -> False ];
-addParameter[ S2, value -> 2.0 10^-15, constant -> False ];
-addParameter[ S3, value -> 1.0 10^-15, constant -> False ];
+addParameter[ S1, value -> 1.0 10^-1, constant -> False ];
+addParameter[ S2, value -> 2.0 10^-1, constant -> False ];
+addParameter[ S3, value -> 1.0 10^-1, constant -> False ];
 addParameter[ k1, value -> 0.75 ];
 addParameter[ k2, value -> 0.25 ];
 addRule[ type->RateRule, variable -> S3, math -> multiply[k1, S1, S2] - k2 * S3];
