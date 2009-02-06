c1c4f4924d585b4bf0a9c84bb87030b30fa55af8
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2009-02-06 22:57:10 +0000

    changes values used so that tolerance does not exceed machine double precision

--- trunk/test-suite/cases/semantic/00210/00210-model.m
+++ trunk/test-suite/cases/semantic/00210/00210-model.m
@@ -21,12 +21,12 @@ to have only substance units.  The model contains two reactions defined as:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|      |*Value*          |*Units*  |
-|Initial amount of S1              |$1.0 \x 10^-15$ |mole                      |
-|Initial amount of S2              |$1.5 \x 10^-15$ |mole                      |
-|Initial amount of S3              |$2.0 \x 10^-15$ |mole                      |
-|Initial amount of S4              |$1.0 \x 10^-15$ |mole                      |
-|Value of parameter k1             |$ 7.5 \x 10^15$ |mole^-1^ second^-1^ |
-|Value of parameter k2             |$0.25 \x 10^15$ |mole^-1^ second^-1^ |
+|Initial amount of S1              |$1.0 \x 10^-3$ |mole                      |
+|Initial amount of S2              |$1.5 \x 10^-3$ |mole                      |
+|Initial amount of S3              |$2.0 \x 10^-3$ |mole                      |
+|Initial amount of S4              |$1.0 \x 10^-3$ |mole                      |
+|Value of parameter k1             |$ 7.5 \x 10^3$ |mole^-1^ second^-1^ |
+|Value of parameter k2             |$0.25 \x 10^3$ |mole^-1^ second^-1^ |
 |Area of compartment "compartment" |$            1$ |metre^2^                  |]
 
 The species have been declared as having substance units only.  Thus, they 
@@ -37,12 +37,12 @@ must be treated as amounts where they appear in expressions.
 newcase[ "00210" ];
 
 addCompartment[ compartment, spatialDimensions-> 2, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.0 10^-15, hasOnlySubstanceUnits->True];
-addSpecies[ S2, initialAmount -> 1.5 10^-15, hasOnlySubstanceUnits->True];
-addSpecies[ S3, initialAmount -> 2.0 10^-15, hasOnlySubstanceUnits->True];
-addSpecies[ S4, initialAmount -> 1.0 10^-15, hasOnlySubstanceUnits->True];
-addParameter[ k1, value -> 7.5 10^15 ];
-addParameter[ k2, value -> 0.25 10^15 ];
+addSpecies[ S1, initialAmount -> 1.0 10^-3, hasOnlySubstanceUnits->True];
+addSpecies[ S2, initialAmount -> 1.5 10^-3, hasOnlySubstanceUnits->True];
+addSpecies[ S3, initialAmount -> 2.0 10^-3, hasOnlySubstanceUnits->True];
+addSpecies[ S4, initialAmount -> 1.0 10^-3, hasOnlySubstanceUnits->True];
+addParameter[ k1, value -> 7.5 10^3 ];
+addParameter[ k2, value -> 0.25 10^3 ];
 addReaction[ S1 + S2 -> S3 + S4, reversible -> False,
 	     kineticLaw -> k1 * S1 * S2 ];
 addReaction[ S3 + S4 -> S1 + S2, reversible -> False,
--- trunk/test-suite/cases/semantic/00211/00211-model.m
+++ trunk/test-suite/cases/semantic/00211/00211-model.m
@@ -20,8 +20,8 @@ boundary species.  The model contains one reaction defined as:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*          |*Units*  |
-|Initial amount of S1              |$1.5 \x 10^-15$ |mole                      |
-|Initial amount of S2              |$  1 \x 10^-15$ |mole                      |
+|Initial amount of S1              |$1.5 \x 10^-3$ |mole                      |
+|Initial amount of S2              |$  1 \x 10^-3$ |mole                      |
 |Value of parameter k1             |$            1$ |second^-1^ |
 |Area of compartment "compartment" |$            1$ |metre^2^                  |]
 
@@ -38,8 +38,8 @@ of the system of equations.
 newcase[ "00211" ];
 
 addCompartment[ compartment, spatialDimensions-> 2, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.5 10^-15, boundaryCondition->True ];
-addSpecies[ S2, initialAmount -> 1 10^-15 ];
+addSpecies[ S1, initialAmount -> 1.5 10^-3, boundaryCondition->True ];
+addSpecies[ S2, initialAmount -> 1 10^-3 ];
 addParameter[ k1, value -> 1 ];
 addReaction[ S1 -> S2, reversible -> False,
 	     kineticLaw -> k1 * S1 * compartment ];
