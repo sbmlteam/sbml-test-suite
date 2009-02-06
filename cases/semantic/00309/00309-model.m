c337974c39a6776021121bbb0ba149aa692efa74
Sarah Keating <skeating@users.noreply.github.com>
2009-02-06 23:40:53 +0000

    changes values used so that tolerance does not exceed machine double precision

--- cases/semantic/00309/00309-model.m
+++ cases/semantic/00309/00309-model.m
@@ -28,8 +28,8 @@ initial value must be calculated by the assignmentRule.
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*          |*Units*  |
-|Initial amount of S1                |$1.0 \x 10^-15$ |mole                      |
-|Initial amount of S2                |$1.5 \x 10^-15$ |mole                      |
+|Initial amount of S1                |$1.0 \x 10^-2$ |mole                      |
+|Initial amount of S2                |$1.5 \x 10^-2$ |mole                      |
 |Initial amount of S3                |$   undeclared$ |mole                      |
 |Value of parameter k1               |$         1.05$ |dimensionless             |
 |Value of parameter k2               |$         1.15$ |second^-1^                |
@@ -45,10 +45,10 @@ where they appear in expressions.
 newcase[ "00309" ];
 
 addCompartment[ compartment, size -> 1];
-addSpecies[ S1, initialAmount-> 1 10^-15];
-addSpecies[ S2, initialAmount -> 1.5 10^-15];
+addSpecies[ S1, initialAmount-> 1 10^-2];
+addSpecies[ S2, initialAmount -> 1.5 10^-2];
 addSpecies[ S3 ];
-addSpecies[ S4, initialAmount -> 1.5 10^-15, constant->True ];
+addSpecies[ S4, initialAmount -> 1.5 10^-2, constant->True ];
 addParameter[ k1, value -> 1.05 ];
 addParameter[ k2, value -> 1.15 ];
 addRule[ type->AssignmentRule, variable -> S3, math ->k1 * S4];
