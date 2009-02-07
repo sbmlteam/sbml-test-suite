2d6a517c0364d08f8dc462ad6e4853ee2349fb14
Sarah Keating <skeating@users.noreply.github.com>
2009-02-07 00:04:10 +0000

    changes values used so that tolerance does not exceed machine double precision

--- cases/semantic/00478/00478-model.m
+++ cases/semantic/00478/00478-model.m
@@ -29,9 +29,9 @@ inconsistent with the value returned by the InitialAssignment.
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}| |*Value*       |*Units*  |
-|Initial amount of S1        |$1.0 \x 10^-15$ |mole                      |
-|Initial amount of S2        |$2.0 \x 10^-15$ |mole                      |
-|Initial amount of S3        |$1.0 \x 10^-15$ |mole                      |
+|Initial amount of S1        |$1.0 \x 10^-2$ |mole                      |
+|Initial amount of S2        |$2.0 \x 10^-2$ |mole                      |
+|Initial amount of S3        |$1.0 \x 10^-2$ |mole                      |
 |Value of parameter k1       |$0.75$          |litre mole^-1^ second^-1^ |
 |Value of parameter k2       |$1.0$           |second^-1^                |
 |Value of parameter p1       |$0.125$         |second^-1^                |
@@ -47,9 +47,9 @@ expressions.
 newcase[ "00478" ];
 
 addCompartment[ C, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.0 10^-15];
-addSpecies[ S2, initialAmount -> 2.0 10^-15];
-addSpecies[ S3, initialAmount -> 1.0 10^-15];
+addSpecies[ S1, initialAmount -> 1.0 10^-2];
+addSpecies[ S2, initialAmount -> 2.0 10^-2];
+addSpecies[ S3, initialAmount -> 1.0 10^-2];
 addParameter[ k1, value -> 0.75 ];
 addParameter[ k2, value -> 1, constant -> False ];
 addParameter[ p1, value -> 0.125 ];
