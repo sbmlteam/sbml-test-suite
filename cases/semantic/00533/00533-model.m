71a9eb581f58d0daee4212398ed85366dc11d233
Sarah Keating <skeating@users.noreply.github.com>
2009-02-08 20:05:44 +0000

    changes values used so that tolerance does not exceed machine double precision

--- cases/semantic/00533/00533-model.m
+++ cases/semantic/00533/00533-model.m
@@ -27,11 +27,11 @@ parameter k2:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*          |*Units*  |
-|Initial amount of S1                |$2.0 \x 10^-15$  |mole                       |
-|Initial amount of S2                |$2.0 \x 10^-15$  |mole                       |
+|Initial amount of S1                |$2.0 \x 10^-1$  |mole                       |
+|Initial amount of S2                |$2.0 \x 10^-1$  |mole                       |
 |Initial amount of S3                |$0$              |mole                       |
 |Initial amount of S4                |$0$              |mole                       |
-|Value of parameter k1               |$1.0 \x 10^15$   |litre mole^-1^ second^-1^  |
+|Value of parameter k1               |$1.0 \x 10^1$   |litre mole^-1^ second^-1^  |
 |Value of parameter k2               |$undeclared$     |second^-1^                 |
 |Value of parameter k3               |$0.7$            |second^-1^                 |
 |Volume of compartment C             |$1$              |litre                      |]
@@ -46,11 +46,11 @@ where they appear in expressions.
 newcase[ "00533" ];
 
 addCompartment[ C, size -> 1 ];
-addSpecies[ S1, initialAmount -> 2.0 10^-15];
-addSpecies[ S2, initialAmount -> 2.0 10^-15];
+addSpecies[ S1, initialAmount -> 2.0 10^-1];
+addSpecies[ S2, initialAmount -> 2.0 10^-1];
 addSpecies[ S3, initialAmount -> 0];
 addSpecies[ S4, initialAmount -> 0];
-addParameter[ k1, value -> 1.0 10^15 ];
+addParameter[ k1, value -> 1.0 10^1 ];
 addParameter[ k2, constant -> False ];
 addParameter[ k3, value -> 0.7 ];
 addRule[ type->AlgebraicRule, math -> k2 - 0.9];
