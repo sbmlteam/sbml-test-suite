6ac91de6b156aefcaa80decfe6a137dec0d5d2b7
Sarah Keating <skeating@users.noreply.github.com>
2009-02-08 19:58:33 +0000

    changes values used so that tolerance does not exceed machine double precision

--- cases/semantic/00519/00519-model.m
+++ cases/semantic/00519/00519-model.m
@@ -29,12 +29,12 @@ inconsistent with the value returned by the InitialAssignment.
 The initial conditions are as follows:
 
 [{width 30em,margin-left 5em}| |*Value*        |*Units*  |
-|Initial amount of S1        |$1.0 \x 10^-15$  |mole                      |
-|Initial amount of S2        |$2.0 \x 10^-15$  |mole                      |
-|Initial amount of S3        |$1.0 \x 10^-15$  |mole                      |
+|Initial amount of S1        |$1.0 \x 10^-2$  |mole                      |
+|Initial amount of S2        |$2.0 \x 10^-2$  |mole                      |
+|Initial amount of S3        |$1.0 \x 10^-2$  |mole                      |
 |Value of parameter k1       |$0.75$           |litre mole^-1^ second^-1^ |
 |Value of parameter k2       |$0.25$           |second^-1^                |
-|Value of parameter p1       |$1.25 \x 10^-16$ |mole                |
+|Value of parameter p1       |$1.25 \x 10^-3$ |mole                |
 |Value of parameter p2       |$0.5$            |dimensionless               |
 |Volume of compartment C     |$1$              |litre                  |]
 
@@ -48,12 +48,12 @@ expressions.
 newcase[ "00519" ];
 
 addCompartment[ C, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.0 10^-15];
-addSpecies[ S2, initialAmount -> 2.0 10^-15];
-addSpecies[ S3, initialAmount -> 1.0 10^-15];
+addSpecies[ S1, initialAmount -> 1.0 10^-2];
+addSpecies[ S2, initialAmount -> 2.0 10^-2];
+addSpecies[ S3, initialAmount -> 1.0 10^-2];
 addParameter[ k1, value -> 0.75 ];
 addParameter[ k2, value -> 0.25 ];
-addParameter[ p1, value -> 0.125 10^-15 ];
+addParameter[ p1, value -> 0.125 10^-2 ];
 addParameter[ p2, value -> 0.5 ];
 addInitialAssignment[ S1, math -> p1*2];
 addReaction[reactants->{S1, S2}, products->{S3}, productStoichiometry->{4 * p1}, 
