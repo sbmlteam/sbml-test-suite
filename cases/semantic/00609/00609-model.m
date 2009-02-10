6242f9f37cfa8329a1e648beb39b1a5d076d2d49
Sarah Keating <skeating@users.noreply.github.com>
2009-02-10 19:18:14 +0000

    changes values used so that tolerance does not exceed machine double precision

--- cases/semantic/00609/00609-model.m
+++ cases/semantic/00609/00609-model.m
@@ -24,8 +24,8 @@ The model contains one rule:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*          |*Units*  |
-|Initial amount of S1                |$1.5 \x 10^-15$ |mole                      |
-|Initial amount of S2                |$1.5 \x 10^-15$ |mole                      |
+|Initial amount of S1                |$1.5 \x 10^-4$ |mole                      |
+|Initial amount of S2                |$1.5 \x 10^-4$ |mole                      |
 |Initial amount of S3                |$            1$ |mole                      |
 |Value of parameter k1               |$         0.75$ |dimensionless |
 |Value of parameter k2               |$           50$ |second^-1^ |
@@ -42,8 +42,8 @@ where they appear in expressions.
 newcase[ "00609" ];
 
 addCompartment[ C, size -> 1];
-addSpecies[ S1, initialAmount->1.5 10^-15 ];
-addSpecies[ S2, initialAmount -> 1.5 10^-15 ];
+addSpecies[ S1, initialAmount->1.5 10^-4 ];
+addSpecies[ S2, initialAmount -> 1.5 10^-4 ];
 addSpecies[ S3, initialAmount -> 1];
 addParameter[ k1, value -> 0.75 ];
 addParameter[ k2, value -> 5 ];
