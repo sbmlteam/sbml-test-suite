dd162b9f39ab16d5351c1a8e6f0444ef743d754a
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2009-02-10 23:30:28 +0000

    changes values used so that tolerance does not exceed machine double precision

--- trunk/test-suite/cases/semantic/00554/00554-model.m
+++ trunk/test-suite/cases/semantic/00554/00554-model.m
@@ -27,10 +27,10 @@ The model contains one rule:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*          |*Units*                   |
-|Initial amount of S1                |$1.0 \x 10^-15$  |mole                      |
-|Initial amount of S2                |$2.0 \x 10^-15$  |mole                      |
-|Initial amount of S3                |$1.0 \x 10^-15$  |mole                      |
-|Initial amount of S4                |$1.0 \x 10^-15$  |mole                      |
+|Initial amount of S1                |$1.0 \x 10^-1$  |mole                      |
+|Initial amount of S2                |$2.0 \x 10^-1$  |mole                      |
+|Initial amount of S3                |$1.0 \x 10^-1$  |mole                      |
+|Initial amount of S4                |$1.0 \x 10^-1$  |mole                      |
 |Value of parameter k1               |$0.75$           |litre mole^-1^ second^-1^ |
 |Value of parameter k2               |$0.25$           |second^-1^                |
 |Volume of compartment C             |$1$              |litre                     |]
@@ -45,10 +45,10 @@ where they appear in expressions.
 newcase[ "00554" ];
 
 addCompartment[ C, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.0 10^-15];
-addSpecies[ S2, initialAmount -> 2.0 10^-15];
-addSpecies[ S3, initialAmount -> 1.0 10^-15];
-addSpecies[ S4, initialAmount -> 1.0 10^-15, boundaryCondition -> True ];
+addSpecies[ S1, initialAmount -> 1.0 10^-1];
+addSpecies[ S2, initialAmount -> 2.0 10^-1];
+addSpecies[ S3, initialAmount -> 1.0 10^-1];
+addSpecies[ S4, initialAmount -> 1.0 10^-1, boundaryCondition -> True ];
 addParameter[ k1, value -> 0.75 ];
 addParameter[ k2, value -> 0.25 ];
 addRule[ type->AlgebraicRule, math -> S4 - S1];
