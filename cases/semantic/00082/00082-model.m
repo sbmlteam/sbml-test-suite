574fb19c233c29478f20044dd7f8b704bf8013eb
Sarah Keating <skeating@users.noreply.github.com>
2009-02-16 20:10:32 +0000

    changes values used so that tolerance does not exceed machine double precision

--- cases/semantic/00082/00082-model.m
+++ cases/semantic/00082/00082-model.m
@@ -19,17 +19,17 @@ model contains two reactions defined as:
 The model contains one rule:
 
 [{width:30em,margin-left:5em}|  *Type*  |  *Variable*  |  *Formula*  |
- | Rate | S4 | $1 10^-12$  |]
+ | Rate | S4 | $1 10^-3$  |]
 
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*          |*Units*  |
-|Initial amount of S1                |$1.5 \x 10^-12$ |mole                      |
-|Initial amount of S2                |$2.0 \x 10^-12$ |mole                      |
-|Initial amount of S3                |$1.5 \x 10^-12$ |mole                      |
-|Initial amount of S4                |$  1 \x 10^-12$ |mole                      |
-|Value of parameter k1               |$0.75 \x 10^12$ |litre mole^-1^ second^-1^ |
-|Value of parameter k2               |$0.25 \x 10^-6$ |second^-1^ |
+|Initial amount of S1                |$1.5 \x 10^-3$ |mole                      |
+|Initial amount of S2                |$2.0 \x 10^-3$ |mole                      |
+|Initial amount of S3                |$1.5 \x 10^-3$ |mole                      |
+|Initial amount of S4                |$  1 \x 10^-3$ |mole                      |
+|Value of parameter k1               |$0.75 \x 10^3$ |litre mole^-1^ second^-1^ |
+|Value of parameter k2               |$0.25 \x 10^-2$ |second^-1^ |
 |Volume of compartment "compartment" |$            1$ |litre                     |]
 
 The species values are given as amounts of substance to make it easier to
@@ -42,13 +42,13 @@ where they appear in expressions.
 newcase[ "00082" ];
 
 addCompartment[ compartment, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.5 10^-12 ];
-addSpecies[ S2, initialAmount -> 2.0 10^-12];
-addSpecies[ S3, initialAmount -> 1.5 10^-12];
-addSpecies[ S4, initialAmount -> 1 10^-12 ];
-addParameter[ k1, value -> .75 10^12];
-addParameter[ k2, value -> .25 10^-6];
-addRule[ type->RateRule, variable -> S4, math -> 1 10^-12];
+addSpecies[ S1, initialAmount -> 1.5 10^-3 ];
+addSpecies[ S2, initialAmount -> 2.0 10^-3];
+addSpecies[ S3, initialAmount -> 1.5 10^-3];
+addSpecies[ S4, initialAmount -> 1 10^-3 ];
+addParameter[ k1, value -> .75 10^3];
+addParameter[ k2, value -> .25 10^-2];
+addRule[ type->RateRule, variable -> S4, math -> 1 10^-3];
 addReaction[ S1 + S2 -> S3, reversible -> False,
 	     kineticLaw -> k1 * S1 * S2 * compartment ];
 addReaction[ S3 -> S1 + S2, reversible -> False,
