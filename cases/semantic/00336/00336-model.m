fd4043b42f486bfb370a36a1a0fb012ff05f7e07
Sarah Keating <skeating@users.noreply.github.com>
2009-02-21 17:56:38 +0000

    changes values used so that tolerance does not exceed machine double precision

--- cases/semantic/00336/00336-model.m
+++ cases/semantic/00336/00336-model.m
@@ -25,11 +25,11 @@ The model contains one rule:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*          |*Units*  |
-|Initial amount of S1                |$1.5 \x 10^-12$ |mole                      |
-|Initial amount of S2                |$1.0 \x 10^-12$ |mole                      |
-|Initial amount of S3                |$1.0 \x 10^-12$ |mole                      |
+|Initial amount of S1                |$1.5 \x 10^-2$ |mole                      |
+|Initial amount of S2                |$1.0 \x 10^-2$ |mole                      |
+|Initial amount of S3                |$1.0 \x 10^-2$ |mole                      |
 |Value of parameter k1               |$         1.75$ |second^-1^ |
-|Value of parameter k2               |$1.5 \x 10^-13$ |mole |
+|Value of parameter k2               |$1.5 \x 10^-3$ |mole |
 |Volume of compartment "compartment" |$            1$ |litre                     |]
 
 The species values are given as amounts of substance to make it easier to
@@ -45,11 +45,11 @@ of the system of equations.
 newcase[ "00336" ];
 
 addCompartment[ compartment, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.5 10^-12 ];
-addSpecies[ S2, initialAmount -> 1.0 10^-12, boundaryCondition->True   ];
-addSpecies[ S3, initialAmount -> 1.0 10^-12];
+addSpecies[ S1, initialAmount -> 1.5 10^-2 ];
+addSpecies[ S2, initialAmount -> 1.0 10^-2, boundaryCondition->True   ];
+addSpecies[ S3, initialAmount -> 1.0 10^-2];
 addParameter[ k1, value -> 1.75 ];
-addParameter[ k2, value -> 1.5 10^-13 ];
+addParameter[ k2, value -> 1.5 10^-3 ];
 addRule[ type->RateRule, variable -> S3, math -> k1 * k2];
 addReaction[ S1 -> S2, reversible -> False,
 	     kineticLaw -> k1 * S1 * compartment ];
