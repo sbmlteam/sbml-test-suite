6b67de4860696c937c8098ffd4210d2f3ab672de
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2009-02-21 17:56:38 +0000

    changes values used so that tolerance does not exceed machine double precision

--- trunk/test-suite/cases/semantic/00331/00331-model.m
+++ trunk/test-suite/cases/semantic/00331/00331-model.m
@@ -24,11 +24,11 @@ The model contains one rule:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|      |*Value*          |*Units*  |
-|Initial amount of S1                |$1.5 \x 10^-12$ |mole                      |
-|Initial amount of S2                |$1.0 \x 10^-12$ |mole                      |
-|Initial amount of S3                |$1.0 \x 10^-12$ |mole                      |
+|Initial amount of S1                |$1.5 \x 10^-1$ |mole                      |
+|Initial amount of S2                |$1.0 \x 10^-1$ |mole                      |
+|Initial amount of S3                |$1.0 \x 10^-1$ |mole                      |
 |Value of parameter k1               |$         1.75$ |second^-1^ |
-|Value of parameter k2               |$1.5 \x 10^-13$ |mole |
+|Value of parameter k2               |$1.5 \x 10^-2$ |mole |
 |Volume of compartment "compartment" |$            1$ |litre                     |]
 
 The species have been declared as having substance units only. Thus, they 
@@ -42,11 +42,11 @@ of the system of equations.
 newcase[ "00331" ];
 
 addCompartment[ compartment, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.5 10^-12, hasOnlySubstanceUnits->True ];
-addSpecies[ S2, initialAmount -> 1.0 10^-12, hasOnlySubstanceUnits->True ];
-addSpecies[ S3, initialAmount -> 1.0 10^-12, hasOnlySubstanceUnits->True ];
+addSpecies[ S1, initialAmount -> 1.5 10^-1, hasOnlySubstanceUnits->True ];
+addSpecies[ S2, initialAmount -> 1.0 10^-1, hasOnlySubstanceUnits->True ];
+addSpecies[ S3, initialAmount -> 1.0 10^-1, hasOnlySubstanceUnits->True ];
 addParameter[ k1, value -> 1.75 ];
-addParameter[ k2, value -> 1.5 10^-13 ];
+addParameter[ k2, value -> 1.5 10^-2 ];
 addRule[ type->RateRule, variable -> S3, math -> k1 * k2];
 addReaction[ S1 -> S2, reversible -> False,
 	     kineticLaw -> k1 * S1 ];
