0ab86f5e112fa1d688e821a614028de3f9a8b130
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2009-03-07 18:08:00 +0000

    changes values used so that tolerance does not exceed machine double precision

--- trunk/test-suite/cases/semantic/00010/00010-model.m
+++ trunk/test-suite/cases/semantic/00010/00010-model.m
@@ -19,11 +19,11 @@ contains two reactions defined as:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*          |*Units*                   |
-|Initial amount of S1                |$1.0 \x 10^-15$  |mole                      |
-|Initial amount of S2                |$2.0 \x 10^-15$  |mole                      |
-|Initial amount of S3                |$1.0 \x 10^-15$  |mole                      |
-|Value of parameter k1               |$0.75$           |litre mole^-1^ second^-1^ |
-|Value of parameter k2               |$0.25$           |second^-1^                |
+|Initial amount of S1                |$1.0 \x 10^-4$  |mole                      |
+|Initial amount of S2                |$2.0 \x 10^-4$  |mole                      |
+|Initial amount of S3                |$1.0 \x 10^-4$  |mole                      |
+|Value of parameter k1               |$3.5$           |litre mole^-1^ second^-1^ |
+|Value of parameter k2               |$1.5$           |second^-1^                |
 |Volume of compartment "compartment" |$1$              |litre                     |]
 
 The species values are given as amounts of substance to make it easier to
@@ -36,11 +36,11 @@ where they appear in expressions.
 newcase[ "00010" ];
 
 addCompartment[ compartment, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.0 10^-15];
-addSpecies[ S2, initialAmount -> 2.0 10^-15];
-addSpecies[ S3, initialAmount -> 1.0 10^-15];
-addParameter[ k1, value -> 0.75 ];
-addParameter[ k2, value -> 0.25 ];
+addSpecies[ S1, initialAmount -> 1.0 10^-4];
+addSpecies[ S2, initialAmount -> 2.0 10^-4];
+addSpecies[ S3, initialAmount -> 1.0 10^-4];
+addParameter[ k1, value -> 3.5 ];
+addParameter[ k2, value -> 1.5 ];
 addReaction[ S1 + S2 -> S3, reversible -> False,
 	     kineticLaw -> k1 * S1 * S2 * compartment ];
 addReaction[ S3 -> S1 + S2, reversible -> False,
