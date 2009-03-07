a4536bd07dcb620034b5e9ccd25b044da4cc320c
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2009-03-07 00:05:07 +0000

    models with non unity compartments had incorrect data

--- trunk/test-suite/cases/semantic/00107/00107-model.m
+++ trunk/test-suite/cases/semantic/00107/00107-model.m
@@ -27,8 +27,8 @@ The model contains one functionDefinition defined as:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*          |*Units*  |
-|Initial amount of S1                |$1.5 \x 10^-15$ |mole                      |
-|Initial amount of S2                |$1.5 \x 10^-15$ |mole                      |
+|Initial amount of S1                |$1.5 \x 10^-1$ |mole                      |
+|Initial amount of S2                |$1.5 \x 10^-1$ |mole                      |
 |Value of parameter k1               |$            1$ |second^-1^ |
 |Volume of compartment "compartment" |$          2.3$ |litre                     |]
 
@@ -46,8 +46,8 @@ newcase[ "00107" ];
 
 addFunction[ multiply, arguments -> {x, y}, math -> x * y];
 addCompartment[ compartment, size -> 2.3 ];
-addSpecies[ S1, initialAmount -> 1.5 10^-15 ];
-addSpecies[ S2, initialAmount -> 1.5 10^-15 ];
+addSpecies[ S1, initialAmount -> 1.5 10^-1 ];
+addSpecies[ S2, initialAmount -> 1.5 10^-1 ];
 addParameter[ k1, value -> 1 ];
 addReaction[ S1 -> S2, reversible -> False,
 	     kineticLaw -> compartment * multiply[k1,S1] ];
