f7eba9a0612d0928da611fe934680886f98378e8
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2009-03-07 23:57:52 +0000

    models with non unity compartments had incorrect data

--- trunk/test-suite/cases/semantic/00548/00548-model.m
+++ trunk/test-suite/cases/semantic/00548/00548-model.m
@@ -26,10 +26,10 @@ compartment C:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*          |*Units*                   |
-|Initial amount of S1                |$1.0 \x 10^-15$  |mole                      |
-|Initial amount of S2                |$2.0 \x 10^-15$  |mole                      |
-|Initial amount of S3                |$1.0 \x 10^-15$  |mole                      |
-|Value of parameter k1               |$0.75$           |litre mole^-1^ second^-1^ |
+|Initial amount of S1                |$1.0 \x 10^-1$  |mole                      |
+|Initial amount of S2                |$2.0 \x 10^-1$  |mole                      |
+|Initial amount of S3                |$1.0 \x 10^-1$  |mole                      |
+|Value of parameter k1               |$7.5$           |litre mole^-1^ second^-1^ |
 |Value of parameter k2               |$0.25$           |second^-1^                |
 |Volume of compartment C             |$undeclared$              |litre                     |]
 
@@ -43,10 +43,10 @@ where they appear in expressions.
 newcase[ "00548" ];
 
 addCompartment[ C, constant -> False ];
-addSpecies[ S1, initialAmount -> 1.0 10^-15];
-addSpecies[ S2, initialAmount -> 2.0 10^-15];
-addSpecies[ S3, initialAmount -> 1.0 10^-15];
-addParameter[ k1, value -> 0.75 ];
+addSpecies[ S1, initialAmount -> 1.0 10^-1];
+addSpecies[ S2, initialAmount -> 2.0 10^-1];
+addSpecies[ S3, initialAmount -> 1.0 10^-1];
+addParameter[ k1, value -> 7.5 ];
 addParameter[ k2, value -> 0.25 ];
 addRule[ type->AlgebraicRule, math -> C - 0.75];
 addReaction[ S1 + S2 -> S3, reversible -> False,
