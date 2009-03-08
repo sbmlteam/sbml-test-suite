f048d9fa0263e0e9e6b2aee7907a65dac29d60b9
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2009-03-08 01:02:14 +0000

    changes values used so that tolerance does not exceed machine double precision

--- trunk/test-suite/cases/semantic/00587/00587-model.m
+++ trunk/test-suite/cases/semantic/00587/00587-model.m
@@ -20,11 +20,11 @@ contains two reactions defined as:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}| |*Value*          |*Units*  |
-|Initial concentration of S1   |$1.0 \x 10^-12$ |mole litre^-1^                      |
-|Initial concentration of S2   |$2.0 \x 10^-12$ |mole litre^-1^                      |
-|Initial concentration of S3   |$1.5 \x 10^-12$ |mole litre^-1^                      |
-|Value of parameter k1         |$0.75 \x 10^12$ |litre mole^-1^ second^-1^ |
-|Value of parameter k2         |$0.25 \x 10^-6$ |second^-1^ |
+|Initial concentration of S1   |$1.0 \x 10^-4$ |mole litre^-1^                      |
+|Initial concentration of S2   |$2.0 \x 10^-4$ |mole litre^-1^                      |
+|Initial concentration of S3   |$1.5 \x 10^-4$ |mole litre^-1^                      |
+|Value of parameter k1         |$0.75 \x 10^4$ |litre mole^-1^ second^-1^ |
+|Value of parameter k2         |$0.25 \x 10^-2$ |second^-1^ |
 |Volume of compartment C       |$          0.7$ |litre                     |]
 
 *)
@@ -32,11 +32,11 @@ The initial conditions are as follows:
 newcase[ "00587" ];
 
 addCompartment[ C, size -> 0.7 ];
-addSpecies[ S1, initialConcentration -> 1.0 10^-12];
-addSpecies[ S2, initialConcentration -> 2.0 10^-12];
-addSpecies[ S3, initialConcentration -> 1.5 10^-12];
-addParameter[ k1, value -> .75 10^12];
-addParameter[ k2, value -> .25 10^-6];
+addSpecies[ S1, initialConcentration -> 1.0 10^-4];
+addSpecies[ S2, initialConcentration -> 2.0 10^-4];
+addSpecies[ S3, initialConcentration -> 1.5 10^-4];
+addParameter[ k1, value -> .75 10^4];
+addParameter[ k2, value -> .25 10^-2];
 addReaction[ S1 + S2 -> S3, reversible -> False,
 	     kineticLaw -> k1 * S1 * S2 * C ];
 addReaction[ S3 -> S1 + S2, reversible -> False,
