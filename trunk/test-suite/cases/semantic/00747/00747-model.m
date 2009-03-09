ab3e0d95c94a557ecfb8b6fc839ccfe2ede60a63
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2009-03-09 22:13:14 +0000

    changes values used so that tolerance does not exceed machine double precision

--- trunk/test-suite/cases/semantic/00747/00747-model.m
+++ trunk/test-suite/cases/semantic/00747/00747-model.m
@@ -20,18 +20,18 @@ two reactions defined as:
 The model contains two events that assign values to species S1 and S4:
 
 [{width:30em,margin-left:5em}| | *Trigger*             | *Delay* | *Assignments* |
- | Event1                      | $S4 > S2$             | $-$     | $S1 -> 2 \x 10^-15$    |]
- | Event2                      | $S3 > 2.25 \x 10^-15$ | $-$     | $S4 -> 1 \x 10^-15$    |]
+ | Event1                      | $S4 > S2$             | $-$     | $S1 -> 2 \x 10^-4$    |]
+ | Event2                      | $S3 > 2.25 \x 10^-4$ | $-$     | $S4 -> 1 \x 10^-4$    |]
 
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}| |*Value*          |*Units*  |
-|Initial concentration of S1          |$1.0 \x 10^-15$  |mole litre^-1^                       |
-|Initial concentration of S2          |$1.0 \x 10^-15$  |mole litre^-1^                       |
-|Initial concentration of S3          |$2.0 \x 10^-15$  |mole litre^-1^                       |
-|Initial concentration of S4          |$1.0 \x 10^-15$  |mole litre^-1^                       |
-|Value of parameter k1         |$0.75 \x 10^15$  |litre mole^-1^ second^-1^  |
-|Value of parameter k2         |$0.25 \x 10^15$  |litre mole^-1^ second^-1^  |
+|Initial concentration of S1          |$1.0 \x 10^-4$  |mole litre^-1^                       |
+|Initial concentration of S2          |$1.0 \x 10^-4$  |mole litre^-1^                       |
+|Initial concentration of S3          |$2.0 \x 10^-4$  |mole litre^-1^                       |
+|Initial concentration of S4          |$1.0 \x 10^-4$  |mole litre^-1^                       |
+|Value of parameter k1         |$0.75 \x 10^4$  |litre mole^-1^ second^-1^  |
+|Value of parameter k2         |$0.25 \x 10^4$  |litre mole^-1^ second^-1^  |
 |Volume of compartment C       |$0.1$            |litre                      |]
 
 *)
@@ -39,17 +39,17 @@ The initial conditions are as follows:
 newcase[ "00747" ];
 
 addCompartment[ C, size -> .1 ];
-addSpecies[ S1, initialConcentration -> 1.0 10^-15];
-addSpecies[ S2, initialConcentration -> 1.2 10^-15];
-addSpecies[ S3, initialConcentration -> 2.0 10^-15];
-addSpecies[ S4, initialConcentration -> 1.0 10^-15];
-addParameter[ k1, value -> 0.75 10^15 ];
-addParameter[ k2, value -> 0.25 10^15 ];
+addSpecies[ S1, initialConcentration -> 1.0 10^-4];
+addSpecies[ S2, initialConcentration -> 1.2 10^-4];
+addSpecies[ S3, initialConcentration -> 2.0 10^-4];
+addSpecies[ S4, initialConcentration -> 1.0 10^-4];
+addParameter[ k1, value -> 0.75 10^4 ];
+addParameter[ k2, value -> 0.25 10^4 ];
 addReaction[ S1 + S2 -> S3 + S4, reversible -> False,
 	     kineticLaw -> k1 * S1 * S2 * C ];
 addReaction[ S3 + S4 -> S1 + S2, reversible -> False,
 	     kineticLaw -> k2 * S3 * S4 * C ];
-addEvent[ trigger -> S4 > S2, eventAssignment -> S1->2/10^15 ];
-addEvent[ trigger -> S3 > 9/(4 10^15), eventAssignment -> S4->1/10^15 ];
+addEvent[ trigger -> S4 > S2, eventAssignment -> S1->2/10^4 ];
+addEvent[ trigger -> S3 > 9/(4 10^4), eventAssignment -> S4->1/10^4 ];
 
 makemodel[]
