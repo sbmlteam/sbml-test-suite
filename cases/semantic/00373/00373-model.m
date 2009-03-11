4f96b483ae29522e914ab59d00e6f793d8170816
Sarah Keating <skeating@users.noreply.github.com>
2009-03-11 02:56:48 +0000

    models with non unity compartments had incorrect data

--- cases/semantic/00373/00373-model.m
+++ cases/semantic/00373/00373-model.m
@@ -20,18 +20,18 @@ two reactions defined as:
 The model contains two events that assign values to species S1 and S4:
 
 [{width:30em,margin-left:5em}| | *Trigger*             | *Delay* | *Assignments* |
- | Event1                      | $S4 > S2$             | $-$     | $S1 -> 2 \x 10^-15$    |]
- | Event2                      | $S3 > 2.25 \x 10^-15$ | $-$     | $S4 -> 1 \x 10^-15$    |]
+ | Event1                      | $S4 > S2$             | $-$     | $S1 -> 1.4$    |]
+ | Event2                      | $S2 < 0.03$           | $-$     | $S4 -> 1 \x 10^-1$    |]
 
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}| |*Value*          |*Units*  |
-|Initial amount of S1          |$1.0 \x 10^-15$  |mole                       |
-|Initial amount of S2          |$1.0 \x 10^-15$  |mole                       |
-|Initial amount of S3          |$2.0 \x 10^-15$  |mole                       |
-|Initial amount of S4          |$1.0 \x 10^-15$  |mole                       |
-|Value of parameter k1         |$0.75 \x 10^15$  |litre mole^-1^ second^-1^  |
-|Value of parameter k2         |$0.25 \x 10^15$  |litre mole^-1^ second^-1^  |
+|Initial amount of S1          |$1.0 \x 10^-1$  |mole                       |
+|Initial amount of S2          |$1.0 \x 10^-1$  |mole                       |
+|Initial amount of S3          |$2.0 \x 10^-1$  |mole                       |
+|Initial amount of S4          |$1.0 \x 10^-1$  |mole                       |
+|Value of parameter k1         |$0.75 \x 10^1$  |litre mole^-1^ second^-1^  |
+|Value of parameter k2         |$0.25 \x 10^-2$  |litre mole^-1^ second^-1^  |
 |Volume of compartment C       |$0.1$            |litre                      |]
 
 The species values are given as amounts of substance to make it easier to
@@ -44,17 +44,17 @@ where they appear in expressions.
 newcase[ "00373" ];
 
 addCompartment[ C, size -> .1 ];
-addSpecies[ S1, initialAmount -> 1.0 10^-15];
-addSpecies[ S2, initialAmount -> 1.2 10^-15];
-addSpecies[ S3, initialAmount -> 2.0 10^-15];
-addSpecies[ S4, initialAmount -> 1.0 10^-15];
-addParameter[ k1, value -> 0.75 10^15 ];
-addParameter[ k2, value -> 0.25 10^15 ];
+addSpecies[ S1, initialAmount -> 1.0 10^-1];
+addSpecies[ S2, initialAmount -> 1.2 10^-1];
+addSpecies[ S3, initialAmount -> 2.0 10^-1];
+addSpecies[ S4, initialAmount -> 1.0 10^-1];
+addParameter[ k1, value -> 0.75 10^1 ];
+addParameter[ k2, value -> 0.25 10^-2 ];
 addReaction[ S1 + S2 -> S3 + S4, reversible -> False,
 	     kineticLaw -> k1 * S1 * S2 * C ];
 addReaction[ S3 + S4 -> S1 + S2, reversible -> False,
 	     kineticLaw -> k2 * S3 * S4 * C ];
-addEvent[ trigger -> S4 > S2, eventAssignment -> S1->2/10^15 ];
-addEvent[ trigger -> S3 > 9/(4 10^15), eventAssignment -> S4->1/10^15 ];
+addEvent[ trigger -> S4 > S2, eventAssignment -> S1->1.4 ];
+addEvent[ trigger -> S2 < 0.03, eventAssignment -> S4->0.1 ];
 
 makemodel[]
