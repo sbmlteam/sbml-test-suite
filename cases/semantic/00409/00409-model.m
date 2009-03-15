eaec8153846d991f232ae6eb50e083c15da4ec9e
Sarah Keating <skeating@users.noreply.github.com>
2009-03-15 01:11:21 +0000

    changes values used so that tolerance does not exceed machine double precision

--- cases/semantic/00409/00409-model.m
+++ cases/semantic/00409/00409-model.m
@@ -20,18 +20,18 @@ two reactions defined as:
 The model contains two events that assign values to species S1 and S4:
 
 [{width:30em,margin-left:5em}| | *Trigger*             | *Delay* | *Assignments* |
- | Event1                      | $S4 > S2$             | $0.5$   | $S1 -> 2 \x 10^-15$    |]
- | Event2                      | $S3 > 2.25 \x 10^-15$ | $0.75$  | $S4 -> 1 \x 10^-15$    |]
+ | Event1                      | $S4 > S2$             | $0.5$   | $S1 -> 2 \x 10^-3$    |]
+ | Event2                      | $S3 > 2.25 \x 10^-3$ | $0.75$  | $S4 -> 1 \x 10^-3$    |]
 
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}| |*Value*          |*Units*  |
-|Initial amount of S1          |$1.0 \x 10^-15$  |mole                       |
-|Initial amount of S2          |$1.0 \x 10^-15$  |mole                       |
-|Initial amount of S3          |$2.0 \x 10^-15$  |mole                       |
-|Initial amount of S4          |$1.0 \x 10^-15$  |mole                       |
-|Value of parameter k1         |$0.75 \x 10^15$  |litre mole^-1^ second^-1^  |
-|Value of parameter k2         |$0.25 \x 10^15$  |litre mole^-1^ second^-1^  |
+|Initial amount of S1          |$1.0 \x 10^-3$  |mole                       |
+|Initial amount of S2          |$1.0 \x 10^-3$  |mole                       |
+|Initial amount of S3          |$2.0 \x 10^-3$  |mole                       |
+|Initial amount of S4          |$1.0 \x 10^-3$  |mole                       |
+|Value of parameter k1         |$0.75 \x 10^3$  |litre mole^-1^ second^-1^  |
+|Value of parameter k2         |$0.25 \x 10^3$  |litre mole^-1^ second^-1^  |
 |Volume of compartment C       |$1$              |litre                      |]
 
 The species values are given as amounts of substance to make it easier to
@@ -44,17 +44,17 @@ where they appear in expressions.
 newcase[ "00409" ];
 
 addCompartment[ C, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.0 10^-15];
-addSpecies[ S2, initialAmount -> 1.2 10^-15];
-addSpecies[ S3, initialAmount -> 2.0 10^-15];
-addSpecies[ S4, initialAmount -> 1.0 10^-15];
-addParameter[ k1, value -> 0.75 10^15 ];
-addParameter[ k2, value -> 0.25 10^15 ];
+addSpecies[ S1, initialAmount -> 1.0 10^-3];
+addSpecies[ S2, initialAmount -> 1.2 10^-3];
+addSpecies[ S3, initialAmount -> 2.0 10^-3];
+addSpecies[ S4, initialAmount -> 1.0 10^-3];
+addParameter[ k1, value -> 0.75 10^3 ];
+addParameter[ k2, value -> 0.25 10^3 ];
 addReaction[ S1 + S2 -> S3 + S4, reversible -> False,
 	     kineticLaw -> k1 * S1 * S2 * C ];
 addReaction[ S3 + S4 -> S1 + S2, reversible -> False,
 	     kineticLaw -> k2 * S3 * S4 * C ];
-addEvent[ trigger -> S4 > S2, delay->0.5, eventAssignment -> S1->2/10^15 ];
-addEvent[ trigger -> S3 > 9/(4 10^15), delay->0.75, eventAssignment -> S4->1/10^15 ];
+addEvent[ trigger -> S4 > S2, delay->0.5, eventAssignment -> S1->2/10^3 ];
+addEvent[ trigger -> S3 > 9/(4 10^3), delay->0.75, eventAssignment -> S4->1/10^3 ];
 
 makemodel[]
