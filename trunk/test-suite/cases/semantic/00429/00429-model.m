c7c118c7dd852c978805b464ebd07b291927ffad
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2009-03-15 18:44:45 +0000

    models with non unity compartments had incorrect data

--- trunk/test-suite/cases/semantic/00429/00429-model.m
+++ trunk/test-suite/cases/semantic/00429/00429-model.m
@@ -20,17 +20,17 @@ two reactions defined as:
 The model contains one event that assigns value to species S1 defined as:
 
 [{width:30em,margin-left:5em}| | *Trigger* | *Delay* | *Assignments*          |
- | Event1                      | $S4 > S2$ | $1.2$   | $S1 -> 2 \x 10^-15$    |]
+ | Event1                      | $S4 > S2$ | $1.2$   | $S1 -> 2 \x 10^-2$    |]
 
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}| |*Value*          |*Units*  |
-|Initial amount of S1          |$1.0 \x 10^-15$  |mole                       |
-|Initial amount of S2          |$1.0 \x 10^-15$  |mole                       |
-|Initial amount of S3          |$2.0 \x 10^-15$  |mole                       |
-|Initial amount of S4          |$1.0 \x 10^-15$  |mole                       |
-|Value of parameter k1         |$0.75 \x 10^15$  |litre mole^-1^ second^-1^  |
-|Value of parameter k2         |$0.25 \x 10^15$  |litre mole^-1^ second^-1^  |
+|Initial amount of S1          |$1.0 \x 10^-2$  |mole                       |
+|Initial amount of S2          |$1.0 \x 10^-2$  |mole                       |
+|Initial amount of S3          |$2.0 \x 10^-2$  |mole                       |
+|Initial amount of S4          |$1.0 \x 10^-2$  |mole                       |
+|Value of parameter k1         |$0.75 \x 10^2$  |litre mole^-1^ second^-1^  |
+|Value of parameter k2         |$0.25 \x 10^2$  |litre mole^-1^ second^-1^  |
 |Volume of compartment C       |$0.24$           |litre                      |]
 
 The species values are given as amounts of substance to make it easier to
@@ -43,16 +43,16 @@ where they appear in expressions.
 newcase[ "00429" ];
 
 addCompartment[ C, size -> 0.24 ];
-addSpecies[ S1, initialAmount -> 1.0 10^-15];
-addSpecies[ S2, initialAmount -> 1.2 10^-15];
-addSpecies[ S3, initialAmount -> 2.0 10^-15];
-addSpecies[ S4, initialAmount -> 1.0 10^-15];
-addParameter[ k1, value -> 0.75 10^15 ];
-addParameter[ k2, value -> 0.25 10^15 ];
+addSpecies[ S1, initialAmount -> 1.0 10^-2];
+addSpecies[ S2, initialAmount -> 1.2 10^-2];
+addSpecies[ S3, initialAmount -> 2.0 10^-2];
+addSpecies[ S4, initialAmount -> 1.0 10^-2];
+addParameter[ k1, value -> 0.75 10^2 ];
+addParameter[ k2, value -> 0.25 10^2 ];
 addReaction[ S1 + S2 -> S3 + S4, reversible -> False,
 	     kineticLaw -> k1 * S1 * S2 * C ];
 addReaction[ S3 + S4 -> S1 + S2, reversible -> False,
 	     kineticLaw -> k2 * S3 * S4 * C ];
-addEvent[ trigger -> S4 > S2, delay->1.2, eventAssignment -> S1->2 10^-15 ];
+addEvent[ trigger -> S4 > S2, delay->1.2, eventAssignment -> S1->2 10^-2 ];
 
 makemodel[]
