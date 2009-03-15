c71996d3666a72b56c937785e969f7fb362bf106
Sarah Keating <skeating@users.noreply.github.com>
2009-03-15 19:55:56 +0000

    models with non unity compartments had incorrect data

--- cases/semantic/00431/00431-model.m
+++ cases/semantic/00431/00431-model.m
@@ -20,18 +20,18 @@ two reactions defined as:
 The model contains one event that assigns values to both species S1 and S4:
 
 [{width:30em,margin-left:5em}| | *Trigger* | *Delay* | *Assignments*         |
- | Event1                      | $S4 > S2$ | $2.5$   | $S1 -> 2 \x 10^-15$   |]
- |                             |           |         | $S4 -> 0.5 \x 10^-15$ |]
+ | Event1                      | $S4 > S2$ | $2.5$   | $S1 -> 2 \x 10^-1$   |]
+ |                             |           |         | $S4 -> 0.5 \x 10^-1$ |]
 
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}| |*Value*          |*Units*  |
-|Initial amount of S1          |$1.0 \x 10^-15$  |mole                       |
-|Initial amount of S2          |$1.0 \x 10^-15$  |mole                       |
-|Initial amount of S3          |$2.0 \x 10^-15$  |mole                       |
-|Initial amount of S4          |$1.0 \x 10^-15$  |mole                       |
-|Value of parameter k1         |$0.75 \x 10^15$  |litre mole^-1^ second^-1^  |
-|Value of parameter k2         |$0.25 \x 10^15$  |litre mole^-1^ second^-1^  |
+|Initial amount of S1          |$1.0 \x 10^-1$  |mole                       |
+|Initial amount of S2          |$1.0 \x 10^-1$  |mole                       |
+|Initial amount of S3          |$2.0 \x 10^-1$  |mole                       |
+|Initial amount of S4          |$1.0 \x 10^-1$  |mole                       |
+|Value of parameter k1         |$0.75 \x 10^1$  |litre mole^-1^ second^-1^  |
+|Value of parameter k2         |$0.25 \x 10^1$  |litre mole^-1^ second^-1^  |
 |Volume of compartment C       |$0.25$           |litre                      |]
 
 The species values are given as amounts of substance to make it easier to
@@ -44,16 +44,16 @@ where they appear in expressions.
 newcase[ "00431" ];
 
 addCompartment[ C, size -> 0.25 ];
-addSpecies[ S1, initialAmount -> 1.0 10^-15];
-addSpecies[ S2, initialAmount -> 1.2 10^-15];
-addSpecies[ S3, initialAmount -> 2.0 10^-15];
-addSpecies[ S4, initialAmount -> 1.0 10^-15];
-addParameter[ k1, value -> 0.75 10^15 ];
-addParameter[ k2, value -> 0.25 10^15 ];
+addSpecies[ S1, initialAmount -> 1.0 10^-1];
+addSpecies[ S2, initialAmount -> 1.2 10^-1];
+addSpecies[ S3, initialAmount -> 2.0 10^-1];
+addSpecies[ S4, initialAmount -> 1.0 10^-1];
+addParameter[ k1, value -> 0.75 10^1 ];
+addParameter[ k2, value -> 0.25 10^1 ];
 addReaction[ S1 + S2 -> S3 + S4, reversible -> False,
 	     kineticLaw -> k1 * S1 * S2 * C ];
 addReaction[ S3 + S4 -> S1 + S2, reversible -> False,
 	     kineticLaw -> k2 * S3 * S4 * C ];
-addEvent[ trigger -> S4 > S2, delay->2.5, eventAssignment -> {S1->2 10^-15, S4->5 10^-16}  ];
+addEvent[ trigger -> S4 > S2, delay->2.5, eventAssignment -> {S1->2 10^-1, S4->5 10^-2}  ];
 
 makemodel[]
