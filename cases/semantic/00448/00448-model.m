560a7c0d7b7c6eb984969f7b6cf8a9dcf73380b4
Sarah Keating <skeating@users.noreply.github.com>
2009-03-15 01:56:08 +0000

    changes values used so that tolerance does not exceed machine double precision

--- cases/semantic/00448/00448-model.m
+++ cases/semantic/00448/00448-model.m
@@ -20,18 +20,18 @@ two reactions defined as:
 The model contains two events that assign value to species S1 and S4:
 
 [{width:30em,margin-left:5em}| | *Trigger*            | *Delay* | *Assignments*       |
- | Event1                      | $S4 > S2$            | $0.5$   | $S1 -> 2 \x 10^-15$ |]
- | Event2                      | $S3 > 2.5 \x 10^-15$ | $1.5$   | $S4 -> 1 \x 10^-15$ |]
+ | Event1                      | $S4 > S2$            | $0.5$   | $S1 -> 2 \x 10^-1$ |]
+ | Event2                      | $S3 > 2.5 \x 10^-1$ | $1.5$   | $S4 -> 1 \x 10^-1$ |]
 
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}| |*Value*          |*Units*  |
-|Initial amount of S1          |$1.0 \x 10^-15$  |mole                       |
-|Initial amount of S2          |$1.0 \x 10^-15$  |mole                       |
-|Initial amount of S3          |$2.0 \x 10^-15$  |mole                       |
-|Initial amount of S4          |$1.0 \x 10^-15$  |mole                       |
-|Value of parameter k1         |$0.75 \x 10^15$  |litre mole^-1^ second^-1^  |
-|Value of parameter k2         |$0.25 \x 10^15$  |litre mole^-1^ second^-1^  |
+|Initial amount of S1          |$1.0 \x 10^-1$  |mole                       |
+|Initial amount of S2          |$1.0 \x 10^-1$  |mole                       |
+|Initial amount of S3          |$2.0 \x 10^-1$  |mole                       |
+|Initial amount of S4          |$1.0 \x 10^-1$  |mole                       |
+|Value of parameter k1         |$0.75 \x 10^1$  |litre mole^-1^ second^-1^  |
+|Value of parameter k2         |$0.25 \x 10^1$  |litre mole^-1^ second^-1^  |
 |Value of parameter p1         |$0.5$            |dimensionless              |
 |Volume of compartment C       |$1$              |litre                      |]
 
@@ -45,19 +45,19 @@ where they appear in expressions.
 newcase[ "00448" ];
 
 addCompartment[ C, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.0 10^-15];
-addSpecies[ S2, initialAmount -> 1.2 10^-15];
-addSpecies[ S3, initialAmount -> 2.0 10^-15];
-addSpecies[ S4, initialAmount -> 1.0 10^-15];
-addParameter[ k1, value -> 0.75 10^15 ];
-addParameter[ k2, value -> 0.25 10^15 ];
+addSpecies[ S1, initialAmount -> 1.0 10^-1];
+addSpecies[ S2, initialAmount -> 1.2 10^-1];
+addSpecies[ S3, initialAmount -> 2.0 10^-1];
+addSpecies[ S4, initialAmount -> 1.0 10^-1];
+addParameter[ k1, value -> 0.75 10^1 ];
+addParameter[ k2, value -> 0.25 10^1 ];
 addParameter[ p1, value -> 0.5 ];
 addReaction[ reactants->{S1, S2}, products->{S3, S4}, productStoichiometry->{4 * p1, 1},
        reversible -> False,
 	     kineticLaw -> k1 * S1 * S2 * C ];
 addReaction[ 2S3 + S4 -> S1 + S2, reversible -> False,
 	     kineticLaw -> k2 * S3 * S4 * C ];
-addEvent[ trigger -> S4 > S2, delay->0.5, eventAssignment -> S1->2 10^-15 ];
-addEvent[ trigger -> S3 > 10/(4 10^15), delay->1.5, eventAssignment -> S4->1 10^-15 ];
+addEvent[ trigger -> S4 > S2, delay->0.5, eventAssignment -> S1->2 10^-1 ];
+addEvent[ trigger -> S3 > 10/(4 10^1), delay->1.5, eventAssignment -> S4->1 10^-1 ];
 
 makemodel[]
