ef6d3bbb86f22f40a3ccc6f3c1d5d95224986ad4
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2009-03-17 17:37:01 +0000

    added 2.4 to the levels tag

--- trunk/test-suite/cases/semantic/00597/00597-model.m
+++ trunk/test-suite/cases/semantic/00597/00597-model.m
@@ -6,7 +6,7 @@ synopsis:      Basic two reactions with three species and parameters both global
 componentTags: Compartment, Species, Reaction, Parameter 
 testTags:      InitialConcentration, LocalParameters
 testType:      TimeCourse
-levels:        2.1, 2.2, 2.3
+levels:        2.1, 2.2, 2.3, 2.4
 generatedBy:   Numeric
 
 The model contains one compartment called C.  There are three
