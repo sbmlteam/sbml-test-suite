25431cc850805910dff0e0078bf4bb8828783623
Sarah Keating <skeating@users.noreply.github.com>
2009-03-17 23:47:48 +0000

    changes values used so that tolerance does not exceed machine double precision

--- cases/semantic/00042/00042-model.m
+++ cases/semantic/00042/00042-model.m
@@ -20,7 +20,7 @@ as:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*       |*Units*  |
-|Initial amount of S1              |$1.5 \x 10^-15$ |mole                      |
+|Initial amount of S1              |$1.5 \x 10^-4$ |mole                      |
 |Initial amount of S2              |$         0$    |mole                      |
 |Value of parameter k1             |$            1$ |second^-1^                |
 |Area of compartment "compartment" |$1$             |metre^2^                  |]
@@ -38,7 +38,7 @@ solution of the system of equations.
 newcase[ "00042" ];
 
 addCompartment[ compartment, spatialDimensions-> 2, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.5 10^-15 ];
+addSpecies[ S1, initialAmount -> 1.5 10^-4 ];
 addSpecies[ S2, initialAmount -> 0 ];
 addParameter[ k1, value -> 1 ];
 addReaction[ S1 -> S2, reversible -> False,
