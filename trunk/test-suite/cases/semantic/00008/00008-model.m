6d96057604042dc2102f7059fb6874e40d638ada
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2009-03-17 23:16:10 +0000

    changes values used so that tolerance does not exceed machine double precision

--- trunk/test-suite/cases/semantic/00008/00008-model.m
+++ trunk/test-suite/cases/semantic/00008/00008-model.m
@@ -21,8 +21,8 @@ The model contains two reactions defined as:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*          |*Units*        |
-|Initial amount of S1                |$1.5 \x 10^-15$  |mole           |
-|Initial amount of S2                |$1.5 \x 10^-15$  |mole           |
+|Initial amount of S1                |$1.5 \x 10^-1$  |mole           |
+|Initial amount of S2                |$1.5 \x 10^-1$  |mole           |
 |Value of parameter k1               |$0.5$            |second^-1^     |
 |Value of parameter k2               |$0.25$           |second^-1^     |
 |Volume of compartment "compartment" |$1$              |litre          |]
@@ -40,8 +40,8 @@ solution of the system of equations.
 newcase[ "00008" ];
 
 addCompartment[ compartment, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.5 10^-15 ];
-addSpecies[ S2, initialAmount -> 1.5 10^-15, boundaryCondition -> True];
+addSpecies[ S1, initialAmount -> 1.5 10^-1 ];
+addSpecies[ S2, initialAmount -> 1.5 10^-1, boundaryCondition -> True];
 addParameter[ k1, value -> 0.5 ];
 addParameter[ k2, value -> 0.25 ];
 addReaction[ S1 -> S2, reversible -> False,
