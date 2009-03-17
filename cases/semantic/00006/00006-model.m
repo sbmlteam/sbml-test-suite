814e5310887f12740410284013cb2a5bb9f661bb
Sarah Keating <skeating@users.noreply.github.com>
2009-03-17 23:16:10 +0000

    changes values used so that tolerance does not exceed machine double precision

--- cases/semantic/00006/00006-model.m
+++ cases/semantic/00006/00006-model.m
@@ -23,8 +23,8 @@ where the species concentrations remain the same.
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*         |*Units*        |
-|Initial amount of S1                |$1.5 \x 10^-15$ |mole           |
-|Initial amount of S2                |$1.5 \x 10^-15$ |mole           |
+|Initial amount of S1                |$1.5 \x 10^-3$ |mole           |
+|Initial amount of S2                |$1.5 \x 10^-3$ |mole           |
 |Value of parameter k1               |$0.5$           |second^-1^     |
 |Value of parameter k2               |$0.5$           |second^-1^     |
 |Volume of compartment "compartment" |$1$             |litre          |]
@@ -46,8 +46,8 @@ solution of the system of equations.
 newcase[ "00006" ];
 
 addCompartment[ compartment, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.5 10^-15 ];
-addSpecies[ S2, initialAmount -> 1.5 10^-15 ];
+addSpecies[ S1, initialAmount -> 1.5 10^-3 ];
+addSpecies[ S2, initialAmount -> 1.5 10^-3 ];
 addParameter[ k1, value -> 0.5 ];
 addParameter[ k2, value -> 0.5 ];
 addReaction[ S1 -> S2, reversible -> False,
