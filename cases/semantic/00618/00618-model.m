44c87d118d8d2aa54edc2e443ad06996952cb0d2
Sarah Keating <skeating@users.noreply.github.com>
2009-03-17 17:41:31 +0000

    added 2.4 to the levels tag

--- cases/semantic/00618/00618-model.m
+++ cases/semantic/00618/00618-model.m
@@ -6,7 +6,7 @@ compartment using an assignmentRule to assign value to a parameter.
 componentTags: Compartment, Species, Reaction, Parameter, AssignmentRule, InitialAssignment 
 testTags:      InitialAmount
 testType:      TimeCourse
-levels:        2.2, 2.3
+levels:        2.2, 2.3, 2.4
 generatedBy:   Numeric
 
 The model contains one compartment called C.  There are two
