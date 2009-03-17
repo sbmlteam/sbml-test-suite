a2402915ded6e2a658ba71a4b1eb4b60d62e6259
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2009-03-17 23:35:01 +0000

    changes values used so that tolerance does not exceed machine double precision

--- trunk/test-suite/cases/semantic/00033/00033-model.m
+++ trunk/test-suite/cases/semantic/00033/00033-model.m
@@ -24,7 +24,7 @@ The model contains one rule:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*         |*Units*  |
-|Initial amount of S1                |$1.5 \x 10^-15$ |mole                      |
+|Initial amount of S1                |$1.5 \x 10^-1$ |mole                      |
 |Initial amount of S2                |$            0$ |mole                      |
 |Value of parameter k1               |$            1$ |second^-1^                |
 |Volume of compartment "compartment" |$            1$ |litre                     |]
@@ -39,7 +39,7 @@ where they appear in expressions.
 newcase[ "00033" ];
 
 addCompartment[ compartment, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.5 10^-15 ];
+addSpecies[ S1, initialAmount -> 1.5 10^-1 ];
 addSpecies[ S2, initialAmount -> 0 ];
 addParameter[ k1, value -> 1, constant->False ];
 addRule[ type->RateRule, variable -> k1, math -> 0.5];
