ad0db5ac5631ad7613bd9546cfc3fbfc3b3fe3c6
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2009-03-17 17:54:22 +0000

    added 2.4 to the levels tag

--- trunk/test-suite/cases/semantic/00760/00760-model.m
+++ trunk/test-suite/cases/semantic/00760/00760-model.m
@@ -5,7 +5,7 @@ synopsis:      Algebraic rule used to determine rate of species change.
 componentTags: Compartment, Species, Reaction, Parameter, AlgebraicRule, EventNoDelay, FunctionDefinition 
 testTags:      InitialAmount
 testType:      TimeCourse
-levels:        2.1, 2.2, 2.3
+levels:        2.1, 2.2, 2.3, 2.4
 generatedBy:   Numeric
 
 The model contains one compartment called C.  There are four
