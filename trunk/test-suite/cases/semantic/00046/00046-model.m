8a95122ed2c11dd40b6ea3d04cc60aa4c9b2fb09
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2009-03-18 00:05:24 +0000

    changes values used so that tolerance does not exceed machine double precision

--- trunk/test-suite/cases/semantic/00046/00046-model.m
+++ trunk/test-suite/cases/semantic/00046/00046-model.m
@@ -20,9 +20,9 @@ defined as:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*         |*Units*  |
-|Initial amount of S1                |$1.0 \x 10^-15$ |mole                      |
-|Initial amount of S2                |$2.0 \x 10^-15$ |mole                      |
-|Initial amount of S3                |$1.0 \x 10^-15$ |mole                      |
+|Initial amount of S1                |$1.0 \x 10^-4$ |mole                      |
+|Initial amount of S2                |$2.0 \x 10^-4$ |mole                      |
+|Initial amount of S3                |$1.0 \x 10^-4$ |mole                      |
 |Value of parameter k1               |$         0.75$ |metre mole^-1^ second^-1^ |
 |Value of parameter k2               |$         0.25$ |second^-1^ |
 |Length of compartment "compartment" |$            1$ |metre                     |]
@@ -37,9 +37,9 @@ where they appear in expressions.
 newcase[ "00046" ];
 
 addCompartment[ compartment, spatialDimensions-> 1, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.0 10^-15];
-addSpecies[ S2, initialAmount -> 2.0 10^-15];
-addSpecies[ S3, initialAmount -> 1.0 10^-15];
+addSpecies[ S1, initialAmount -> 1.0 10^-4];
+addSpecies[ S2, initialAmount -> 2.0 10^-4];
+addSpecies[ S3, initialAmount -> 1.0 10^-4];
 addParameter[ k1, value -> 0.75 ];
 addParameter[ k2, value -> 0.25 ];
 addReaction[ S1 + S2 -> S3, reversible -> False,
--- trunk/test-suite/cases/semantic/00048/00048-model.m
+++ trunk/test-suite/cases/semantic/00048/00048-model.m
@@ -19,7 +19,7 @@ compartment is 0-dimensional.  The model contains one reaction defined as:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}| |*Value*          |*Units*     |
-|Initial amount of S1          | $1.5 \x 10^-15$ | mole       |
+|Initial amount of S1          | $1.5 \x 10^-2$ | mole       |
 |Initial amount of S2          | $            0$ | mole       |
 |Value of parameter k1         | $            1$ | second^-1^ |]
 
@@ -35,7 +35,7 @@ solution of the system of equations.
 newcase[ "00048" ];
 
 addCompartment[ compartment, spatialDimensions-> 0 ];
-addSpecies[ S1, initialAmount -> 1.5 10^-15 ];
+addSpecies[ S1, initialAmount -> 1.5 10^-2 ];
 addSpecies[ S2, initialAmount -> 0 ];
 addParameter[ k1, value -> 1 ];
 addReaction[ S1 -> S2, reversible -> False,
