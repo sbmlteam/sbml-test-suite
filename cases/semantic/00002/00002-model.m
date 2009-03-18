f46e97d7f7a9c1cc8e70066c8f29f6a469df4228
Sarah Keating <skeating@users.noreply.github.com>
2009-03-18 00:16:23 +0000

    changes values used so that tolerance does not exceed machine double precision

--- cases/semantic/00002/00002-model.m
+++ cases/semantic/00002/00002-model.m
@@ -23,8 +23,8 @@ zero rate (i.e., not at all).
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*          |*Units*        |
-|Initial amount of S1                |$1.5 \x 10^-15$  |mole           |
-|Initial amount of S2                |$0 \x$           |mole           |
+|Initial amount of S1                |$1.5 \x 10^-3$  |mole           |
+|Initial amount of S2                |$0$           |mole           |
 |Value of parameter k1               |$1$              |second^-1^     |
 |Value of parameter k2               |$0$              |second^-1^     |
 |Volume of compartment "compartment" |$1$              |litre          |]
@@ -42,7 +42,7 @@ solution of the system of equations.
 newcase[ "00002" ];
 
 addCompartment[ compartment, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.5 10^-15 ];
+addSpecies[ S1, initialAmount -> 1.5 10^-3 ];
 addSpecies[ S2, initialAmount -> 0 ];
 addParameter[ k1, value -> 1 ];
 addParameter[ k2, value -> 0 ];
--- cases/semantic/00005/00005-model.m
+++ cases/semantic/00005/00005-model.m
@@ -19,8 +19,8 @@ reaction defined as:
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*          |*Units*        |
-|Initial amount of S1                |$1.5 \x 10^-15$  |mole           |
-|Initial amount of S2                |$1.5 \x 10^-15$  |mole           |
+|Initial amount of S1                |$1.5 \x 10^-4$  |mole           |
+|Initial amount of S2                |$1.5 \x 10^-4$  |mole           |
 |Value of parameter k1               |$1$              |second^-1^     |
 |Volume of compartment "compartment" |$1$              |litre          |]
 
@@ -37,8 +37,8 @@ solution of the system of equations.
 newcase[ "00005" ];
 
 addCompartment[ compartment, size -> 1 ];
-addSpecies[ S1, initialAmount -> 1.5 10^-15 ];
-addSpecies[ S2, initialAmount -> 1.5 10^-15 ];
+addSpecies[ S1, initialAmount -> 1.5 10^-4 ];
+addSpecies[ S2, initialAmount -> 1.5 10^-4 ];
 addParameter[ k1, value -> 1 ];
 addReaction[ S1 -> S2, reversible -> False,
 	     kineticLaw -> k1 * S1 * compartment ];
