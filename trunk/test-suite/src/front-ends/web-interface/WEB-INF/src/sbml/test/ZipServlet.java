734e93081f462208e426df6c79ee8ebb1533e785
mhucka <mhucka@c05c2498-8750-0410-b44c-b7d1844e3258>
2010-01-25 08:25:23 +0000

    Did a serious bout of cleaning up.

--- trunk/test-suite/src/front-ends/web-interface/WEB-INF/src/sbml/test/ZipServlet.java
+++ trunk/test-suite/src/front-ends/web-interface/WEB-INF/src/sbml/test/ZipServlet.java
@@ -1,16 +1,17 @@
-
-// 
-// @file    ZipServelt.java
+//
+// @file    ZipServlet.java
 // @brief   Servlet to zip the test cases for the user.
 // @author  Kimberly Begley
+// @author  Michael Hucka
 // @date    Created Jul 30, 2008, 9:25:21 AM
+// @id      $Id$
+// $HeadURL$
 //
+// ----------------------------------------------------------------------------
+// This file is part of the SBML Test Suite.  Please visit http://sbml.org for 
+// more information about SBML, and the latest version of the SBML Test Suite.
 //
-//----------------------------------------------------------------------------
-//This file is part of the SBML Test Suite.  Please visit http://sbml.org for 
-//more information about SBML, and the latest version of the SBML Test Suite.
-//
-// Copyright 2008      California Institute of Technology.
+// Copyright 2008-2010 California Institute of Technology.
 // Copyright 2004-2007 California Institute of Technology (USA) and
 //                     University of Hertfordshire (UK).
 // 
@@ -20,141 +21,157 @@
 // in the file named "LICENSE.txt" included with this software distribution
 // and also available at http://sbml.org/Software/SBML_Test_Suite/License
 // ----------------------------------------------------------------------------
-//
+
 package sbml.test;
+
 import java.io.*;
 import java.net.*;
-
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Vector;
+import java.util.regex.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 
-public class ZipServlet extends HttpServlet {
+/**
+ * Called by the Online STS's process.jsp to create a zip archive of the
+ * test cases selected by the user.  It omits certain files from the zip
+ * archive, based on a regular expression defined below.
+ * <p>
+ * This class gets the selection parameters from the HTTP session data; it
+ * also gets other data from the session, such as the location of the test
+ * cases directory.
+ */
+public class ZipServlet extends HttpServlet
+{
+  private final String archiveFileBaseName = new String("SBML_test_cases");
+  private final String omitFileRegexp =
+    new String("\\d{5}-(model.m|results.csv|plot.(jpg|eps))");
 
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     * @param request servlet request
-     * @param response servlet response
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        // response.setContentType("text/html;charset=UTF-8");
-        // PrintWriter out = response.getWriter();
+  /** 
+   * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+   * methods. 
+   * @param request servlet request
+   * @param response servlet response
+   */
+  protected void processRequest(HttpServletRequest request,
+                                HttpServletResponse response)
+    throws ServletException, IOException
+  {
+    try
+    {
+      HttpSession  session = request.getSession(true);
+      File path            = (File) session.getAttribute("path");
+      Vector cases         = (Vector) session.getAttribute("cases");
 
-        try {
-            
-            response.setContentType("application/zip");
-	//response.setContentType("application/octet-stream");
-            response.setHeader("Content-disposition", "attachment; filename=selectedSbmlTestCases.zip");
-            
-            HttpSession session = request.getSession(true);
+      if (path == null)
+        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, 
+                           "Session does not contain data path information.");
 
-            File path = (File) session.getAttribute("path");
-            @SuppressWarnings("unchecked") // FIXME
-            Vector<String> selectedCases = (Vector<String>) session.getAttribute("tcases");
-            
-            
-            if (path == null) {
-                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Session does not contain data path information.");
-            } 
-            
-            if (selectedCases == null) {
-                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Session does not contain test case information");
-            } 
-            
-            if (selectedCases.size() < 1) {
-                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "No test cases selected");
-            }
-            
+      if (cases == null)
+        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                           "Session does not contain test case information");
 
+      if (cases.size() < 1)
+        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                           "No test cases selected");
             
-            ZipOutputStream zipOutputStream = new ZipOutputStream(response.getOutputStream());
+      response.setContentType("application/zip");
+      response.setHeader("Content-disposition",
+                         "attachment; filename=" + archiveFileBaseName + ".zip");
 
-    //        File path = new File(request.getParameter("path"));
-            Iterator tc = selectedCases.iterator();
-            
-            while(tc.hasNext()) {
-                String tcase = (String)tc.next();
-                addFilesToZip(new File(path+File.separator+tcase),File.separator+tcase+File.separator , zipOutputStream);
-            }
-            
-        //    addFilesToZip(path, File.separator, zipOutputStream);
+      ZipOutputStream zos = new ZipOutputStream(response.getOutputStream());
 
+      Iterator tc = cases.iterator();
+      while (tc.hasNext())
+      {
+        addFilesToZip((String) tc.next(), path, archiveFileBaseName, zos);
+      }
 
-            zipOutputStream.flush();
-            zipOutputStream.close();
-        } catch (Exception e) {
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.toString());
-        } finally {
+      zos.flush();
+      zos.close();
+    }
+    catch (Exception e)
+    {
+      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                         e.toString());
+    }
+  }
+
+  /**
+   * Recursive function to add a case and all its contents to the zip
+   * output stream.
+   */
+  private void addFilesToZip(String tcase, File root,
+                             String prefix, ZipOutputStream zos)
+    throws Exception
+  {
+    File dir = new File(root + File.separator + tcase);
+
+    if (! dir.exists())
+      throw new Exception("File '" + dir.toString() + "' not found");
+
+    String pathInZip = prefix + File.separator + tcase + File.separator;
+
+    Iterator entries = Arrays.asList(dir.listFiles()).iterator();
+    while (entries.hasNext())
+    {
+      File   file = (File) entries.next();
+      String name = file.getName();
+
+      if (file.isDirectory())
+      {
+        addFilesToZip(pathInZip + name + File.separator, file, "", zos);
+      }
+      else if (! Pattern.matches(omitFileRegexp, name))
+      {
+        zos.putNextEntry(new ZipEntry(pathInZip + name));
+
+        FileInputStream in = new FileInputStream(file);
+        byte buf[]         = new byte[4096];
+        int len;
+
+        while ((len = in.read(buf)) > 0)
+        {
+          zos.write(buf, 0, len);
         }
+
+        zos.closeEntry();
+        in.close();
+      }
     }
+  }
 
-    private void addFilesToZip(File path, String pathInZip, ZipOutputStream zipOutputStream) throws Exception {
+  /** 
+   * Handles the HTTP <code>GET</code> method.
+   * @param request servlet request
+   * @param response servlet response
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException
+  {
+    processRequest(request, response);
+  }
 
-        if (path.exists()) {
-            File[] files = path.listFiles();
-            for (int i = 0; i < files.length; i++) {
-                if (files[i].isDirectory()) {
-                    addFilesToZip(files[i], pathInZip + files[i].getName() + File.separator, zipOutputStream);
-                } else {
-                    try {
-                        FileInputStream in = new FileInputStream(files[i]);
+  /** 
+   * Handles the HTTP <code>POST</code> method.
+   * @param request servlet request
+   * @param response servlet response
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException
+  {
+    processRequest(request, response);
+  }
 
-
-                        zipOutputStream.putNextEntry(new ZipEntry(pathInZip + files[i].getName()));
-
-                        byte buf[] = new byte[2048];
-                        int len;
-                        while ((len = in.read(buf)) > 0) {
-                            zipOutputStream.write(buf, 0, len);
-                        }
-                        zipOutputStream.closeEntry();
-                        in.close();
-
-                    } catch (IOException e) {
-                        throw e;
-                    }
-                }
-            }
-        } else {
-            throw new Exception("File not found");
-        }
-
-
-
-
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
-     * Handles the HTTP <code>GET</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /** 
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /** 
-     * Returns a short description of the servlet.
-     */
-    public String getServletInfo() {
-        return "Servlet to zip the test cases for user";
-    }
-    // </editor-fold>
+  /** 
+   * Returns a short description of the servlet.
+   */
+  public String getServletInfo()
+  {
+    return "Servlet to zip the test cases for user";
+  }
 }
