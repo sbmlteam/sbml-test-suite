8a8c3525ec441b82084ae99a1fb54f1d01bc8dd7
Mike Hucka <mhucka@users.noreply.github.com>
2010-01-25 08:26:08 +0000

    Did a serious bout of cleaning up.

--- src/front-ends/web-interface/WEB-INF/src/sbml/test/FormBean.java
+++ src/front-ends/web-interface/WEB-INF/src/sbml/test/FormBean.java
@@ -2,14 +2,16 @@
 // @file    FormBean.java
 // @brief   Servlet to get selections from user test case selection web page
 // @author  Kimberly Begley
+// @author  Michael Hucka
 // @date    Created Jul 30, 2008, 9:25:21 AM
+// @id      $Id$
+// $HeadURL$
 //
+// ----------------------------------------------------------------------------
+// This file is part of the SBML Test Suite.  Please visit http://sbml.org for 
+// more information about SBML, and the latest version of the SBML Test Suite.
 //
-//----------------------------------------------------------------------------
-//This file is part of the SBML Test Suite.  Please visit http://sbml.org for 
-//more information about SBML, and the latest version of the SBML Test Suite.
-//
-// Copyright 2008      California Institute of Technology.
+// Copyright 2008-2010 California Institute of Technology.
 // Copyright 2004-2007 California Institute of Technology (USA) and
 //                     University of Hertfordshire (UK).
 // 
@@ -19,80 +21,47 @@
 // in the file named "LICENSE.txt" included with this software distribution
 // and also available at http://sbml.org/Software/SBML_Test_Suite/License
 // ----------------------------------------------------------------------------
-//
-
-
 
 package sbml.test;
+
 import java.io.*;
 import java.util.*;
 
-public class FormBean {
-   
-    /** 
-    * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-    * @param request servlet request
-    * @param response servlet response
-    */
-    
-    private String[] levels;
-    private String[] testtype;
+/** 
+ * Java Bean for interfacing the JSP front-end code in selecttests.jsp to
+ * the processing code in process.jsp.
+ */
+public class FormBean
+{
+    // All are arrays because the calling page uses a form with multiple
+    // choices for each one.  All the set & get methods also must take
+    // arrays args and return arrays.
+
+    private String[] levelAndVersion;
     private String[] ctags;
     private String[] ttags;
     private HashMap<String, String>  errors;
 
-    public boolean validate() {
-        boolean allOk = true;
-        return allOk;
+    public FormBean() {
+        levelAndVersion = new String[]{"unset"};
+        ctags           = new String[]{"unset"};
+        ttags           = new String[]{"unset"};
+        errors          = new HashMap<String, String>();
     }
+
+    public void setLevelAndVersion(String[] lv) { levelAndVersion = lv; }
+    public void setCtags(String[] ctag)         { ctags = ctag; }
+    public void setTtags(String[] ttag)         { ttags = ttag; }
+
+    public String[] getLevelAndVersion()        { return levelAndVersion; }
+    public String[] getCtags()                  { return ctags; }
+    public String[] getTtags()                  { return ttags; }
+
+    public void setErrors(String key, String msg) { errors.put(key, msg); }
 
     public String getErrorMsg(String s) {
         String errorMsg = (String) errors.get(s.trim());
         return (errorMsg == null) ? "" : errorMsg;
-    }
-
-    public FormBean() {
-        levels = new String[]{"1"};
-        testtype = new String[]{"1"};
-        ctags = new String[]{"1"};
-        ttags = new String[]{"1"};
-        errors = new HashMap<String, String>();
-    }
-    
-    public String[] getLevels() {
-        return levels;
-    }
-
-    public String[] getTesttype() {
-        return testtype;
-    }
-
-    public String[] getCtags() {
-        return ctags;
-    }
-
-    public String[] getTtags() {
-        return ttags;
-    }
-
-    public void setLevels(String[] level) {
-        levels = level;
-    }
-
-    public void setTesttype(String[] type) {
-        testtype = type;
-    }
-
-    public void setCtags(String[] ctag) {
-        ctags = ctag;
-    }
-
-    public void setTtags(String[] ttag) {
-        ttags = ttag;
-    }
-
-    public void setErrors(String key, String msg) {
-        errors.put(key, msg);
     }
 
 }
