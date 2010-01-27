68e7aba210af5c6901a78df4c549353a3683a896
Mike Hucka <mhucka@users.noreply.github.com>
2010-01-27 21:14:35 +0000

    1) Rewrote file omission check to include levels/versions that were not selected.  Now returns just the level+version requested. 2) Reindented the code (which unfortunately screws up the ability to diff, but oh well).

--- src/front-ends/web-interface/WEB-INF/src/sbml/test/ZipServlet.java
+++ src/front-ends/web-interface/WEB-INF/src/sbml/test/ZipServlet.java
@@ -47,131 +47,163 @@ import javax.servlet.http.*;
  */
 public class ZipServlet extends HttpServlet
 {
-  private final String archiveFileBaseName = new String("SBML_test_cases");
-  private final String omitFileRegexp =
-    new String("\\d{5}-(model.m|results.csv|plot.(jpg|eps))");
+    private final String archiveName = new String("SBML_test_cases");
 
-  /** 
-   * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-   * methods. 
-   * @param request servlet request
-   * @param response servlet response
-   */
-  protected void processRequest(HttpServletRequest request,
-                                HttpServletResponse response)
-    throws ServletException, IOException
-  {
-    try
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods. 
+     * @param req servlet request
+     * @param resp servlet response
+     */
+    protected void processRequest(HttpServletRequest request,
+                                  HttpServletResponse resp)
+        throws ServletException, IOException
     {
-      HttpSession  session = request.getSession(true);
-      File path            = (File) session.getAttribute("path");
-      Vector cases         = (Vector) session.getAttribute("cases");
-
-      if (path == null)
-        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, 
-                           "Session does not contain data path information.");
-
-      if (cases == null)
-        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-                           "Session does not contain test case information");
-
-      if (cases.size() < 1)
-        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-                           "No test cases selected");
-            
-      response.setContentType("application/zip");
-      response.setHeader("Content-disposition",
-                         "attachment; filename=" + archiveFileBaseName + ".zip");
-
-      ZipOutputStream zos = new ZipOutputStream(response.getOutputStream());
-
-      Iterator tc = cases.iterator();
-      while (tc.hasNext())
-      {
-        addFilesToZip((String) tc.next(), path, archiveFileBaseName, zos);
-      }
-
-      zos.flush();
-      zos.close();
-    }
-    catch (Exception e)
-    {
-      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-                         e.toString());
-    }
-  }
-
-  /**
-   * Recursive function to add a case and all its contents to the zip
-   * output stream.
-   */
-  private void addFilesToZip(String tcase, File root,
-                             String prefix, ZipOutputStream zos)
-    throws Exception
-  {
-    File dir = new File(root + File.separator + tcase);
-
-    if (! dir.exists())
-      throw new Exception("File '" + dir.toString() + "' not found");
-
-    String pathInZip = prefix + File.separator + tcase + File.separator;
-
-    Iterator entries = Arrays.asList(dir.listFiles()).iterator();
-    while (entries.hasNext())
-    {
-      File   file = (File) entries.next();
-      String name = file.getName();
-
-      if (file.isDirectory())
-      {
-        addFilesToZip(pathInZip + name + File.separator, file, "", zos);
-      }
-      else if (! Pattern.matches(omitFileRegexp, name))
-      {
-        zos.putNextEntry(new ZipEntry(pathInZip + name));
-
-        FileInputStream in = new FileInputStream(file);
-        byte buf[]         = new byte[4096];
-        int len;
-
-        while ((len = in.read(buf)) > 0)
+        try
         {
-          zos.write(buf, 0, len);
+            HttpSession s          = request.getSession(true);
+            File path              = (File) s.getAttribute("path");
+            Vector cases           = (Vector) s.getAttribute("cases");
+            String levelAndVersion = (String) s.getAttribute("levelAndVersion");
+
+            String omitFileRegexp  = buildFileOmitRegexp(levelAndVersion);
+
+            resp.setContentType("application/zip");
+            resp.setHeader("Content-disposition",
+                           "attachment; filename=" + archiveName + ".zip");
+
+            if (path == null)
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, 
+                               "Session does not contain data path information.");
+
+            if (cases == null)
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                               "Session does not contain test case information");
+
+            if (cases.size() < 1)
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                               "No test cases selected");
+
+            ZipOutputStream zos = new ZipOutputStream(resp.getOutputStream());
+
+            Iterator tc = cases.iterator();
+            while (tc.hasNext())
+            {
+                String theCase = (String) tc.next();
+                addFilesToZip(theCase, path, archiveName, omitFileRegexp, zos);
+            }
+
+            zos.flush();
+            zos.close();
         }
-
-        zos.closeEntry();
-        in.close();
-      }
+        catch (Exception e)
+        {
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                           e.toString());
+        }
     }
-  }
 
-  /** 
-   * Handles the HTTP <code>GET</code> method.
-   * @param request servlet request
-   * @param response servlet response
-   */
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException
-  {
-    processRequest(request, response);
-  }
+    /**
+     * Recursive function to add a case and all its contents to the zip
+     * output stream.
+     */
+    private void addFilesToZip(String tcase, File root, String prefix,
+                               String omitFileRegexp, ZipOutputStream zos)
+        throws Exception
+    {
+        File dir = new File(root + File.separator + tcase);
 
-  /** 
-   * Handles the HTTP <code>POST</code> method.
-   * @param request servlet request
-   * @param response servlet response
-   */
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException
-  {
-    processRequest(request, response);
-  }
+        if (! dir.exists())
+            throw new Exception("File '" + dir.toString() + "' not found");
 
-  /** 
-   * Returns a short description of the servlet.
-   */
-  public String getServletInfo()
-  {
-    return "Servlet to zip the test cases for user";
-  }
+        String pathInZip = prefix + File.separator + tcase + File.separator;
+
+        Iterator entries = Arrays.asList(dir.listFiles()).iterator();
+        while (entries.hasNext())
+        {
+            File   file = (File) entries.next();
+            String name = file.getName();
+
+            if (file.isDirectory())
+            {
+                addFilesToZip(pathInZip + name + File.separator, file, "",
+                              omitFileRegexp, zos);
+            }
+            else if (! Pattern.matches(omitFileRegexp, name))
+            {
+                zos.putNextEntry(new ZipEntry(pathInZip + name));
+
+                FileInputStream in = new FileInputStream(file);
+                byte buf[]         = new byte[4096];
+                int len;
+
+                while ((len = in.read(buf)) > 0)
+                {
+                    zos.write(buf, 0, len);
+                }
+
+                zos.closeEntry();
+                in.close();
+            }
+        }
+    }
+
+
+    /**
+     * Builds a regular expression of file names to omit from the archive.
+     */
+    private String buildFileOmitRegexp(String levelAndVersion)
+    {
+        // 'rx' is the start of the regexp, but note that it's not terminated.
+        // We add some more pieces below, then close it off at the end.
+
+        String rx = new String("\\d{5}-(model.m|results.csv|plot.(jpg|eps)");
+        String versions[][] = {
+            {"1.2", "sbml-l1v2.xml"},
+            {"2.1", "sbml-l2v1.xml"},
+            {"2.2", "sbml-l2v2.xml"},
+            {"2.3", "sbml-l2v3.xml"},
+            {"2.4", "sbml-l2v4.xml"},
+            {"3.1", "sbml-l3v1.xml"},
+        };
+      
+        if (levelAndVersion != null)      // Shouldn't happen, but let's be safe.
+            for (int i = 0; i < versions.length; i++)
+                if (! versions[i][0].equals(levelAndVersion))
+                    rx += "|" + versions[i][1];
+
+        return rx + ")";
+    }
+
+
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param req servlet request
+     * @param resp servlet response
+     */
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {
+        processRequest(req, resp);
+    }
+
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param req servlet request
+     * @param resp servlet response
+     */
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {
+        processRequest(req, resp);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     */
+    public String getServletInfo()
+    {
+        return "Servlet to zip the test cases for user";
+    }
 }
