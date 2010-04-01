f95cdacb3bc8449ad7b0821faddaf2dce1f9e532
Sarah Keating <skeating@users.noreply.github.com>
2010-04-01 10:27:46 +0000

    LocalParameter(s) tag missing the s meant that didnt get converted

--- cases/semantic/00027/00027-model.m
+++ cases/semantic/00027/00027-model.m
@@ -4,7 +4,7 @@ category:      Test
 synopsis:      Basic single forward reaction with two species in one
 compartment using initialAssignment to set the compartment size.
 componentTags: Compartment, Species, Reaction, InitialAssignment 
-testTags:      Amount, NonUnityCompartment, LocalParameter, InitialValueReassigned
+testTags:      Amount, NonUnityCompartment, LocalParameters, InitialValueReassigned
 testType:      TimeCourse
 levels:        2.2, 2.3, 2.4, 3.1
 generatedBy:   Numeric
