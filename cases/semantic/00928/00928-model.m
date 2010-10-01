34e84fab4367be6e464b876c2a9e0197df3ae4a3
Sarah Keating <skeating@users.noreply.github.com>
2010-10-01 10:36:29 +0000

    tidied up descriptions and files

--- cases/semantic/00928/00928-model.m
+++ cases/semantic/00928/00928-model.m
@@ -8,22 +8,28 @@ testType:      TimeCourse
 levels:        3.1
 generatedBy:   Analytic
 
-The model contains one compartment called "compartment".  There are two
+The model contains one compartment called "C".  There are two
 species called S1 and S2 and one parameter called k1.  The model contains
 one reaction:
 
 [{width:30em,margin-left:5em}|  *Reaction*  |  *Rate*  |
-| S1 -> S2 | $k1 * S1 * compartment$ |]
+| S1 -> S2 | $k1 * S1 * C$ |]
 
-The model contains one event that is triggered at t=0.
+The model contains one event that assigns value to species S1 defined as:
+
+[{width:30em,margin-left:5em}| | *Trigger*    | *Delay* | *Assignments* |
+ | Event1 | $t >= 0$ | $-$   | $S1 = 1.5 \x 10^-4$    |]
+ 
+The initialValue of the Trigger is set to 'false'.  Thus at t = 0 the Trigger
+transitions from 'false' to 'true' and thus the event is fired.
 
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*          |*Units*        |
-|Initial amount of S1                |$1.5 \x 10^-4$  |mole           |
+|Initial amount of S1                |$0$  |mole           |
 |Initial amount of S2                |$0$              |mole           |
 |Value of parameter k1               |$1$              |second^-1^     |
-|Volume of compartment "compartment" |$1$              |litre          |]
+|Volume of compartment "C" |$1$              |litre          |]
 
 The species' initial quantities are given in terms of substance units to
 make it easier to use the model in a discrete stochastic simulator, but (as
