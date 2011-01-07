fef2f8c5028af400220e801c5d0aa717650f43da
luciansmith <luciansmith@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-01-07 17:35:05 +0000

    Added a new test case!  I'm sure I didn't get all the tags right, and I didn't try to integrate it into the rest of the suite, and I haven't tried actually simulating the models to see if they perform the way I expected them to--this is sort of a test to see if I'm getting the basics down right.  Also, I don't know what format the .m file is, but I tried to mimic the .m file I copied from a different test case.

---
+++ trunk/test-suite/cases/semantic/00936/00936-model.m
@@ -0,0 +1,28 @@
+(* 
+
+category:      Test
+synopsis:      Limited-time oscillation that triggers three identical delayed events
+componentTags: Compartment, Species, Event
+testTags:      Amount
+testType:      TimeCourse
+levels:        2.4, 3.1
+generatedBy:   Analytic
+
+The model contains one compartment called "default_compartment".  There are two
+species called S1 and S2. 
+
+
+ The model contains one species (S1) that oscillates (from the 'sin' function) for two seconds, then stops.  During that oscillation, the single event is triggered three times with a delay of two seconds each, increasing the value of the second species (S2) by one each time.  As 'useValuesFromTriggerTime' is false, this means that first S1 oscillates, then S2 steps up three times.
+
+The initial conditions are as follows:
+
+[{width:30em,margin-left:5em}|       |*Value*          |
+|Initial concentration of S1                |$0$  |
+|Initial concentration of S2                |$0$              |
+|Volume of compartment "default_compartment" |$1$              |]
+
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+
+*)
+
