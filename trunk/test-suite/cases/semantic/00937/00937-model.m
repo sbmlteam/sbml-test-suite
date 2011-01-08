1dd72c8e1985112ac954bda751090e1e86150050
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-01-08 10:31:42 +0000

    moved NonConstantParameter testTag and added table of initial values

--- trunk/test-suite/cases/semantic/00937/00937-model.m
+++ trunk/test-suite/cases/semantic/00937/00937-model.m
@@ -3,16 +3,21 @@
 category:      Test
 synopsis:      Two oscillators; one that echoes the first by a delay.
 componentTags: Compartment, Parameter, AssignmentRule
-testTags:      CSymbolDelay, CSymbolTime
-testType:      TimeCourse, NonConstantParameter
+testTags:      CSymbolDelay, CSymbolTime, NonConstantParameter
+testType:      TimeCourse
 levels:        1.2, 2.1, 2.2, 2.3, 2.4, 3.1
 generatedBy:   Analytic
 
-The model contains one compartment called "default_compartment".  There are two
-species called S1 and S2. 
-
+The model contains one compartment called "default_compartment".  
 
  The model contains one parameter (x) that oscillates (from the 'sin' function), and a second parameter (y) that echoes the first after a 0.2 second delay.
+
+The initial conditions are as follows:
+
+[{width:30em,margin-left:5em}|               |*Value*          |
+|Initial value of x                          |$0$              |
+|Initial value of y                          |$1.090703$       |
+|Volume of compartment "default_compartment" |$1$              |]
 
 Note: The test data for this model was generated from an analytical
 solution of the system of equations.
