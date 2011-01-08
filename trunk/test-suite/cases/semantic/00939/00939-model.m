890dfba712ed54abbb24f449f8a158cccab55e38
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-01-08 10:58:21 +0000

    added AssignmentRule tag removed InitialConcentration and CSymbolTime tags

--- trunk/test-suite/cases/semantic/00939/00939-model.m
+++ trunk/test-suite/cases/semantic/00939/00939-model.m
@@ -2,8 +2,8 @@
 
 category:      Test
 synopsis:      A species that with an initial concentration set, and subsequently produced by a reaction, being echoed by a parameter viewing it through a delay.
-componentTags: Compartment, Parameter, Species, Reaction
-testTags:      CSymbolDelay, CSymbolTime, InitialConcentration, NonConstantParameter
+componentTags: Compartment, Parameter, Species, Reaction, AssignmentRule
+testTags:      CSymbolDelay, NonConstantParameter
 testType:      TimeCourse
 levels:        1.2, 2.1, 2.2, 2.3, 2.4, 3.1
 generatedBy:   Analytic
