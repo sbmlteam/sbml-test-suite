955406557d5e4f6b0cc70fff54d1fdfde09edbfc
luciansmith <luciansmith@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-01-08 00:16:13 +0000

    Slight cleaning up of 937; an additional 938, with a species with an initial assignment, switching to being reaction-controlled.  A second parameter echoes it with a delay, revealing the oscillating initial assignment.

---
+++ trunk/test-suite/cases/semantic/00938/00938-model.m
@@ -0,0 +1,17 @@
+(* 
+
+category:      Test
+synopsis:      A species that changes behavior between its IntialAssignment annd , then is created by a reaction afterwards, being echoed by a parameter viewing it through a delay.
+componentTags: Compartment, Parameter, Species, InitialAssignment, Reaction
+testTags:      CSymbolDelay, CSymbolTime
+testType:      TimeCourse, NonConstantParameter
+levels:        2.4, 3.1
+generatedBy:   Analytic
+
+ The model contains one compartment called "default_compartment".  There is a single species S1 whose InitialAssignment claims that it has been oscillating (the sine function) up until the start of the simulation, at which point a reaction begins to create it.  A parameter (y) echoes this species with a 0.2 second delay, meaning that it starts by 'seeing' the oscillations before echoing the linear increase.
+
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+
+*)
+
