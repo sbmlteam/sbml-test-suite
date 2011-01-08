7a36861e69373b47d51630139d9a767ace4d1b26
Lucian Smith <luciansmith@users.noreply.github.com>
2011-01-08 01:37:51 +0000

    Fixed the column headers; added a new model to check initialconcentration noticed by a delay.

--- cases/semantic/00938/00938-model.m
+++ cases/semantic/00938/00938-model.m
@@ -1,11 +1,11 @@
 (* 
 
 category:      Test
-synopsis:      A species that changes behavior between its IntialAssignment annd , then is created by a reaction afterwards, being echoed by a parameter viewing it through a delay.
+synopsis:      A species that changes behavior between its IntialAssignment andsubsequently produced by a reaction, being echoed by a parameter viewing it through a delay.
 componentTags: Compartment, Parameter, Species, InitialAssignment, Reaction
 testTags:      CSymbolDelay, CSymbolTime
 testType:      TimeCourse, NonConstantParameter
-levels:        2.4, 3.1
+levels:        2.2, 2.3, 2.4, 3.1
 generatedBy:   Analytic
 
  The model contains one compartment called "default_compartment".  There is a single species S1 whose InitialAssignment claims that it has been oscillating (the sine function) up until the start of the simulation, at which point a reaction begins to create it.  A parameter (y) echoes this species with a 0.2 second delay, meaning that it starts by 'seeing' the oscillations before echoing the linear increase.
