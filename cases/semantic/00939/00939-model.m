74990619e7d865ddc1c3304743d804a64f02c96f
Lucian Smith <luciansmith@users.noreply.github.com>
2011-01-11 02:15:20 +0000

    Four new cases that are all slight variations on one another.  All have x with an InitialAssignment that varies in time, and is constant thereafter, and y which echoes it through a delay, seeing the otherwise-hidden initial assignment values.  In 940, x is a species, 941 a parameter, 942 a constant species, and 942 a constant parameter.
    Additionally, a table of initial values were added to 938 and 939.

--- cases/semantic/00939/00939-model.m
+++ cases/semantic/00939/00939-model.m
@@ -10,6 +10,11 @@ generatedBy:   Analytic
 
  The model contains one compartment called "default_compartment".  There is a single species x with an initial concentration, which at the start of the simulation increases due to a reaction.  A parameter (y) echoes this species with a 0.2 second delay, meaning that it starts by 'seeing' the initial constant value before echoing the linear increase.
 
+[{width:30em,margin-left:5em}|       |*Value*          |
+|Initial amount of x                |$0$  |
+|Initial amount of y                |$1.090703$              |
+|Volume of compartment "default_compartment" |$1$              |]
+
 Note: The test data for this model was generated from an analytical
 solution of the system of equations.
 
