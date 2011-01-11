02ae43fdeff1a7cb09c872f3cf9543aa072f0cd1
luciansmith <luciansmith@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-01-11 02:15:20 +0000

    Four new cases that are all slight variations on one another.  All have x with an InitialAssignment that varies in time, and is constant thereafter, and y which echoes it through a delay, seeing the otherwise-hidden initial assignment values.  In 940, x is a species, 941 a parameter, 942 a constant species, and 942 a constant parameter.
    Additionally, a table of initial values were added to 938 and 939.

--- trunk/test-suite/cases/semantic/00940/00940-model.m
+++ trunk/test-suite/cases/semantic/00940/00940-model.m
@@ -1,22 +1,24 @@
 (* 
 
 category:      Test
-synopsis:      A species that changes behavior between its IntialAssignment andsubsequently produced by a reaction, being echoed by a parameter viewing it through a delay.
-componentTags: Compartment, Parameter, Species, InitialAssignment, Reaction, AssignmentRule
+synopsis:     A variable species that varies in its IntialAssignment, but is constant thereafte, being echoed by a parameter viewing it through a delay.
+componentTags: Compartment, Parameter, Species, InitialAssignment, AssignmentRule
 testTags:      Amount, CSymbolDelay, CSymbolTime, NonConstantParameter
 testType:      TimeCourse
 levels:        2.2, 2.3, 2.4, 3.1
 generatedBy:   Analytic
 
- The model contains one compartment called "default_compartment".  There is a single species S1 whose InitialAssignment claims that it has been oscillating (the sine function) up until the start of the simulation, at which point a reaction begins to create it.  A parameter (y) echoes this species with a 0.2 second delay, meaning that it starts by 'seeing' the oscillations before echoing the linear increase.
+ The model contains one compartment called "default_compartment".  There is a single species x whose InitialAssignment claims that it has been oscillating (the sine function) up until the start of the simulation, at which point it stops changing.  A parameter (y) echoes this species with a 0.5 second delay, meaning that it starts by 'seeing' the oscillations before flattening out.
 
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}|       |*Value*          |
 |Initial amount of x                |$0$  |
-|Initial amount of y                |$1.090703$              |
+|Initial amount of y                |$2.958924275$              |
 |Volume of compartment "default_compartment" |$1$              |]
 
+
+ 
 The species values are given as amounts of substance to make it easier to
 use the model in a discrete stochastic simulator, but (as per usual SBML
 principles) their symbols represent their values in concentration units
