8742118fefa677a520aaa577bcd840a4db2d5d45
Lucian Smith <luciansmith@users.noreply.github.com>
2011-01-13 23:26:44 +0000

    The simplest model in the history of simple models:  a single parameter, which doesn't change.
    (Actually, this might make a reasonable template to test the constants from.)

---
+++ cases/semantic/00949/00949-model.m
@@ -0,0 +1,19 @@
+(* 
+
+category:      Test
+synopsis:      One constant parameter.
+componentTags: Parameter
+testTags:      InitialValue
+testType:      TimeCourse
+levels:        1.2, 2.1, 2.2, 2.3, 2.4, 3.1
+generatedBy:   Numeric
+
+The model contains one parameter P. It doesn't change.
+
+The initial condition is as follows:
+
+[{width:30em,margin-left:5em}| |*Value*|
+|Value of parameter P          |$3.7$  |]
+
+*)
+
