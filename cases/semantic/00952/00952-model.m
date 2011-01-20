22637f6e4fdebf85df3f7a99a44a44dfa87a9f46
Lucian Smith <luciansmith@users.noreply.github.com>
2011-01-20 01:36:20 +0000

    Actually analytic results.

--- cases/semantic/00952/00952-model.m
+++ cases/semantic/00952/00952-model.m
@@ -6,7 +6,7 @@ componentTags: Parameter, EventNoDelay, EventPriority, AssignmentRule
 testTags:      InitialValue, CSymbolTime
 testType:      TimeCourse
 levels:        3.1
-generatedBy:   Numeric
+generatedBy:   Analytic
 
 This model contains two events with the same trigger, the same priority, both set 'persistent=true', and both of which disable the trigger of the other.  This means that every .01 seconds, one fires and the other does not, at random, and increases the parameters Q or R, respectively.  A third parameter, S, is assigned the value of Q+R, meaning that it doesn't matter which one fires; S will increase monotonically.  A final parameter, 'error' checks to make sure neither Q nor R are chosen more frequently than the other--if the difference gets higher than 0.05, it triggers. 
  
@@ -27,6 +27,8 @@ The events are:
 |Value of parameter reset      |$0$  |
 |Value of parameter error      |$0$  |]
 
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
 *)
 
 (*
