7fed6599543f814a244e23d42ea9a6c37e7ebbd5
luciansmith <luciansmith@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-01-20 02:10:23 +0000

    ...and this is analytic too.

--- trunk/test-suite/cases/semantic/00953/00953-model.m
+++ trunk/test-suite/cases/semantic/00953/00953-model.m
@@ -6,7 +6,7 @@ componentTags: Parameter, EventNoDelay, EventPriority, AssignmentRule
 testTags:      InitialValue, CSymbolTime
 testType:      TimeCourse
 levels:        3.1
-generatedBy:   Numeric
+generatedBy:   Analytic
 
 This model contains two events with the same trigger, no priority, both set 'persistent=true', and both of which disable the trigger of the other.  This means that every .01 seconds, one fires and the other does not, at random, and increases the parameters Q or R, respectively.  A third parameter, S, is assigned the value of Q+R, meaning that it doesn't matter which one fires; S will increase monotonically.  This model is identical to 952, except that there are no priorities, so Qinc and Rinc do not have to be chosen between at random, but can be chosen in any ratio.
  
@@ -26,6 +26,8 @@ The events are:
 |Value of parameter reset      |$0$  |
 |Value of parameter error      |$0$  |]
 
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
 *)
 
 (*
