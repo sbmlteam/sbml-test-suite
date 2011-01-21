778d2144dab40d34d6206eb0279b6416126d050a
luciansmith <luciansmith@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-01-21 00:08:54 +0000

    Two tests--one for the math constructs present in L1, and one for using those constructs with time.

---
+++ trunk/test-suite/cases/semantic/00954/00954-model.m
@@ -0,0 +1,13 @@
+(* 
+
+category:      Test
+synopsis:      Several parameters with assignment rules, testing various L1 built-in functions acting on constants.
+componentTags: Parameter, AssignmentRule
+testTags:      InitialValueReassigned
+testType:      TimeCourse
+levels:        1.2, 2.1, 2.2, 2.3, 2.4, 3.1
+generatedBy:   Numeric
+
+ The model tests the various mathematical constructs present in L1, as applied to the time variable.  Ironically, this doesn't work in L1, since time isn't present, but hey.
+
+This file doesn't contain a list of the initial conditions, since there are a ton of them, and the whole point is that you are supposed to calculate them yourself.
