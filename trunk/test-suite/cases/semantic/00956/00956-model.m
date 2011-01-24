91749e3e2747bdee9580f4305cdc7c745e390398
luciansmith <luciansmith@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-01-24 19:39:51 +0000

    Same test as 954, but with initial assignments instead of assignment rules (various l1 functions).

---
+++ trunk/test-suite/cases/semantic/00956/00956-model.m
@@ -0,0 +1,13 @@
+(* 
+
+category:      Test
+synopsis:      Several parameters with initial assignment rules, testing various L1 built-in functions acting on constants.
+componentTags: Parameter, InitialAssignment
+testTags:      InitialValueReassigned
+testType:      TimeCourse
+levels:        2.2, 2.3, 2.4, 3.1
+generatedBy:   Numeric
+
+ The model tests the various mathematical constructs present in L1, used in InitialAssignments.
+
+This file doesn't contain a list of the initial conditions, since there are a ton of them, and the whole point is that you are supposed to calculate them yourself.
