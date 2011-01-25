d012b04decfde6f9f96a10722aa619822bfa4062
Lucian Smith <luciansmith@users.noreply.github.com>
2011-01-25 22:55:44 +0000

    Two tests for avogadro's number, one for initial assignment and one for assignment rules.  We give them misleading names, too, just in case.

---
+++ cases/semantic/00960/00960-model.m
@@ -0,0 +1,21 @@
+(* 
+
+category:      Test
+synopsis:      Three different ways of writing the avogadro number in an initial assignment.
+componentTags: Parameter, InitialAssignment
+testTags:      InitialValueReassigned
+testType:      TimeCourse
+levels:        3.1
+generatedBy:   Analytic
+
+The model assigns avogadro's number as a csymbol to four parameters.  It tries to fool you by calling two of them 'time' and 'delay', and by not giving one a name at all.
+
+The initial conditions are as follows:
+
+[{width:30em,margin-left:5em}|       |*Value*          |
+|Value of parameter P1               |$6.02214179e+23)$   |
+|Value of parameter P2               |$6.02214179e+23)$   |
+|Value of parameter P3               |$6.02214179e+23)$   |
+|Value of parameter P4               |$6.02214179e+23)$   |]
+
+
