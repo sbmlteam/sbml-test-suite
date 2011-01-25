d3b4f0c6953d52255d06feac50afb3c2f7349abc
Lucian Smith <luciansmith@users.noreply.github.com>
2011-01-25 00:14:19 +0000

    Cases adding the functions mentioned in the L2v1 spec that were not in the L1 spec.  The first two are for constants, and the last tests the trigonometric functions vs. time. Or a subset of time; the boundaries were kind of hard to track down in some cases!

---
+++ cases/semantic/00958/00958-model.m
@@ -0,0 +1,13 @@
+(* 
+
+category:      Test
+synopsis:      Several parameters with initial assignments, testing various L2v1 built-in functions acting on constants.
+componentTags: Parameter, InitialAssignment
+testTags:      InitialValueReassigned
+testType:      TimeCourse
+levels:        2.2, 2.3, 2.4, 3.1
+generatedBy:   Numeric
+
+ The model tests the various mathematical constructs added in L2v1, as initial assignments.  As such, the results are identical to case 957.
+
+This file doesn't contain a list of the initial conditions, since there are a ton of them, and the whole point is that you are supposed to calculate them yourself.
