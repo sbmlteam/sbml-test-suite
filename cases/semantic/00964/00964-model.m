a57130f01d23ef18afa08534078d1df6727e24de
Lucian Smith <luciansmith@users.noreply.github.com>
2011-02-01 23:03:23 +0000

    964: Similar to 952, but with three equivalent events instead of two, to test non-50/50 ratios. 965: 952 checking for runs.  In my simulations, the longest run in 1,000,000 runs was 15, with an average of 13.25.  I set the cutoff at 20--much longer than that, but still a lot smaller than when RR was having problems (it had runs in the 100's).

---
+++ cases/semantic/00964/00964-model.m
@@ -0,0 +1,35 @@
+(* 
+
+category:      Test
+synopsis:      Three competing events with the same priority, jointly causing a parameter to monotonically increase, checking to make sure one event doesn't severely out-compete the other two.   NOTE:  STOCHASTIC TEST. This test is designed to fail only one time in a million, but it still may happen.
+componentTags: Parameter, EventNoDelay, EventPriority, AssignmentRule
+testTags:      InitialValue, CSymbolTime, RandomEventExecution
+testType:      TimeCourse
+levels:        3.1
+generatedBy:   Analytic
+
+This model contains three events with the same trigger, the same priority, and all set 'persistent=false', and all of which disable the trigger of the others.  This means that every .01 seconds, one fires and the others do not, at random, and increases the parameters Q, R, and T, respectively.  A fourth parameter, S, is assigned the value of Q+R+T, meaning that it doesn't matter which one fires; S will increase monotonically.  A final parameter, 'error' checks to make sure none of the three parameters are chosen more frequently than the other two--if the difference gets higher than 5, it triggers.  From our simulations, this should happen less often than one time in a million, so if you see it happen in your simulator more often than once, there's a good chance something is wrong.
+ 
+The events are:
+
+[{width:30em,margin-left:5em}| | *Trigger*   | *Delay* | *Assignments* |
+ | Qinc | $time - reset >= 0.01$ | $-$   | $Q = Q + 0.01, reset = time$      |
+ | Rinc | $time - reset >= 0.01$ | $-$   | $R = R + 0.01, reset = time$      |
+ | Tinc | $time - reset >= 0.01$ | $-$   | $T = T + 0.01, reset = time$      |
+ | error_check | $or((abs(Q - R) >= tolerance), (abs(Q - T) >= tolerance), (abs(T - R) >= tolerance))$ | $-$ |  $error = 1$      |]
+
+
+ The initial conditions are as follows:
+
+[{width:30em,margin-left:5em}| |*Value*|
+|Value of parameter Q          |$0$  |
+|Value of parameter R          |$0$  |
+|Value of parameter S          |$0$  |
+|Value of parameter T          |$0$  |
+|Value of parameter tolerance  |$5$  |
+|Value of parameter reset      |$0$  |
+|Value of parameter error      |$0$  |]
+
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+*)
