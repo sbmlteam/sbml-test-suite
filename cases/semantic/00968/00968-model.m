ce6777eb7fc99737238eef9662a0a1db4cc84c25
Lucian Smith <luciansmith@users.noreply.github.com>
2011-02-08 23:12:56 +0000

    Several new speciesreference tests; the new stoichiometryMath thing for L3: 968:  sr set by initialValue, no 'stoichiometry' attribute. 969:  sr set by initialValue, 'stoichiometry="1"' (which is then overridden) 970:  sr set by assignmentrule, no attribute. 971:  sr set by assignmentrule, s=1. 972:  sr re-set by event.

---
+++ cases/semantic/00968/00968-model.m
@@ -0,0 +1,26 @@
+(* 
+
+category:      Test
+synopsis:      A very simple reaction with stoichiometry set by an initialAssignment.
+componentTags: Parameter, Species, InitialAssignment
+testTags:      InitialAmount, SpeciesReferenceMath
+testType:      TimeCourse
+levels:        3.1
+generatedBy:   Analytic
+
+ This model contains a single rule:
+[{width:30em,margin-left:5em}|  *Reaction*  |  *Rate*  |
+|  -> nX | $k1$  |]
+
+ Where 'n' is a speciesReference (actually 'Xref') with an initialAssignment of 3.  No 'stoichiometry' value is provided in the speciesReference in the reaction itself.
+ 
+The initial conditions are as follows:
+
+[{width:30em,margin-left:5em}| |*Value*       |
+|Initial amount of X        |$0$                  |
+|Value of parameter k1       |$1$          |
+|Volume of compartment default_compartment     |$1$             |]
+
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+*)
---
+++ cases/semantic/00971/00971-model.m
@@ -0,0 +1,26 @@
+(* 
+
+category:      Test
+synopsis:      A very simple reaction with stoichiometry set by an assignmentRule.
+componentTags: Parameter, Species, AssignmentRule
+testTags:      InitialAmount, SpeciesReferenceMath
+testType:      TimeCourse
+levels:        3.1
+generatedBy:   Analytic
+
+ This model contains a single rule:
+[{width:30em,margin-left:5em}|  *Reaction*  |  *Rate*  |
+|  -> nX | $k1$  |]
+
+ Where 'n' is a speciesReference (actually 'Xref') with an assignmentRule of 3.  A 'stoichiometry' value of '1' is provided in the speciesReference in the reaction itself, which should be overridden by the assignment rule.
+ 
+The initial conditions are as follows:
+
+[{width:30em,margin-left:5em}| |*Value*       |
+|Initial amount of X        |$0$                  |
+|Value of parameter k1       |$1$          |
+|Volume of compartment default_compartment     |$1$             |]
+
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+*)
