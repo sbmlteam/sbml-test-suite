4ee92b34811e792976215d3b2ea682259ac47b15
luciansmith <luciansmith@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-02-08 23:12:56 +0000

    Several new speciesreference tests; the new stoichiometryMath thing for L3: 968:  sr set by initialValue, no 'stoichiometry' attribute. 969:  sr set by initialValue, 'stoichiometry="1"' (which is then overridden) 970:  sr set by assignmentrule, no attribute. 971:  sr set by assignmentrule, s=1. 972:  sr re-set by event.

---
+++ trunk/test-suite/cases/semantic/00969/00969-model.m
@@ -0,0 +1,26 @@
+(* 
+
+category:      Test
+synopsis:      A very simple reaction with stoichiometry set by an initialAssignment.
+componentTags: Parameter, Species, InitialAssignment
+testTags:      InitialAmount, SpeciesReferenceMath
+testType:      TimeCourse
+levels:        3.1
+generatedBy:   Analytic
+
+ This model contains a single rule:
+[{width:30em,margin-left:5em}|  *Reaction*  |  *Rate*  |
+|  -> nX | $k1$  |]
+
+ Where 'n' is a speciesReference (actually 'Xref') with an initialAssignment of 3.  A 'stoichiometry' value is provided in the speciesReference in the reaction itself of '1'.  This should be replaced by the value in the initialAssignment.
+ 
+The initial conditions are as follows:
+
+[{width:30em,margin-left:5em}| |*Value*       |
+|Initial amount of X        |$0$                  |
+|Value of parameter k1       |$1$          |
+|Volume of compartment default_compartment     |$1$             |]
+
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+*)
---
+++ trunk/test-suite/cases/semantic/00972/00972-model.m
@@ -0,0 +1,28 @@
+(* 
+
+category:      Test
+synopsis:      A very simple reaction with stoichiometry changed by an event.
+componentTags: Parameter, Species, Event
+testTags:      InitialAmount, SpeciesReferenceMath
+testType:      TimeCourse
+levels:        3.1
+generatedBy:   Analytic
+
+ This model contains a single rule:
+[{width:30em,margin-left:5em}|  *Reaction*  |  *Rate*  |
+|  -> nX | $k1$  |]
+
+ Where 'n' is a speciesReference (actually 'Xref'), and has a 'stoichiometry' value of '1'one in the speciesReference in the reaction itself.
+
+ When X goes over 5, however, this stoichiometry increases to 3 through an event.
+ 
+The initial conditions are as follows:
+
+[{width:30em,margin-left:5em}| |*Value*       |
+|Initial amount of X        |$0$                  |
+|Value of parameter k1       |$1$          |
+|Volume of compartment default_compartment     |$1$             |]
+
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+*)
