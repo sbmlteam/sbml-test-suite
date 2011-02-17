eb7db93049cacd810aa3df279d58d13780edecc3
luciansmith <luciansmith@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-02-17 22:31:51 +0000

    As Chris pointed out, my first test case would indeed execute five times, but would set its value to the same new value each time, since 'useValuesFromTriggerTime' was set true.  This version adds a new case there with uVFTT=false, which should increase as in my original design.

--- trunk/test-suite/cases/semantic/00978/00978-model.m
+++ trunk/test-suite/cases/semantic/00978/00978-model.m
@@ -1,7 +1,7 @@
 (* 
 
 category:      Test
-synopsis:      Several events conspire within the same time step to trigger two events multiple times, one persistent and the other non-persistent.
+synopsis:      Several events conspire within the same time step to trigger three events multiple times, with different outcomes.
 componentTags: EventNoDelay, EventPriority, Parameter
 testTags:      InitialValue, CSymbolTime, 
 testType:      TimeCourse
@@ -9,10 +9,17 @@ levels:        3.1
 generatedBy:   Analytic
 
 This model contains two events that trigger when the parameter multitrig increases to 
-above 1.  The first is set 'persistent=true', and sets x=x+1, meaning that every time
-it triggers, it ends up increasing x by one.  The second is set 'persistent=false', 
-and sets y=y+1, meaning that every time it triggers, it has the potential to increase y
-by one, but if multitrig drops below 1 again, it will not actually do so.
+above 1.  The first is set 'persistent=true' and 'useValuesFromTriggerTime=false', and sets x=x+1, meaning that every time
+it triggers, it ends up increasing x by one.  
+
+The second is set 'persistent=true' but 'useValuesFromTriggerTime=true', meaning that it executes
+multiple times, but each time uses the value of y from the time it first triggered, meaning
+that y gets reset to one four times.
+
+The third is set 'persistent=false' and 'useValuesFromTriggerTime=false',
+and sets z=z+1, meaning that every time it triggers, it has the potential to increase y
+by one, but if multitrig drops below 1 again, it will not actually do so.  In the end,
+it only executes a single time.
 
 The model also contains nine events with the same trigger and with decreasing priorities 
 that alternate setting the value of the parameter 'multitrig' from zero to 2, then to 
