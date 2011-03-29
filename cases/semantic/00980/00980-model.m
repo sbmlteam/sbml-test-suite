823566eaca61202e2beae740e87f48a1aac9377a
Lucian Smith <luciansmith@users.noreply.github.com>
2011-03-29 07:44:44 +0000

    Fixing Event tags so that they're consistent before Mike goes and changes them all--Now they're all the standard 3 EventWithDelay, EventNoDelay, and EventPriority.  The changed models had 'Event' and 'EventDelay', which should have both been illegal.

--- cases/semantic/00980/00980-model.m
+++ cases/semantic/00980/00980-model.m
@@ -2,7 +2,7 @@
 
 category:      Test
 synopsis:      Two event where the two event assignments should not interfere with each other. 
-componentTags: EventDelay, Parameter
+componentTags: EventWithDelay, Parameter
 testTags:      CSymbolTime, NonConstantParameter
 testType:      TimeCourse
 levels:        2.4, 3.1
