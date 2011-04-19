3df400d7a8e550f151f9b50c5343078f74108063
Lucian Smith <luciansmith@users.noreply.github.com>
2011-04-19 15:15:24 +0000

    Changed the event triggers so they're not simultaneous (it wasn't supposed to be part of the test; the results are the same.)

--- cases/semantic/00980/00980-model.m
+++ cases/semantic/00980/00980-model.m
@@ -8,7 +8,7 @@ testType:      TimeCourse
 levels:        2.4, 3.1
 generatedBy:   Analytic
 
- This model contains two events that trigger when time > 0.99.  In each, there is on event assignment that changes the value of a parameter, and a second event assignment that uses that parameter to change the value of a second parameter.  However, the second event assignment should not use the changed value from the first parameter, but rather the value it had at the beginning of event assignment.  (This is true regardless of the value of 'useValuesFromTriggerTime', but the value of that flag here happens to be false.  In order to test this in l2v4, there had to be a delay.)
+ This model contains two events that trigger when time > 0.98 and 0.99.  In each, there is on event assignment that changes the value of a parameter, and a second event assignment that uses that parameter to change the value of a second parameter.  However, the second event assignment should not use the changed value from the first parameter, but rather the value it had at the beginning of event assignment.  (This is true regardless of the value of 'useValuesFromTriggerTime', but the value of that flag here happens to be false.  In order to test this in l2v4, there had to be a delay.)
 
 The initial conditions are as follows:
 
@@ -21,7 +21,7 @@ The initial conditions are as follows:
 The events are:
 
 [{width:30em,margin-left:5em}| | *Trigger*   | *Delay* | *Assignments* |
- | _E0 | $time >= 0.99$ | $0.1$  | $x = 2, y = y + x$      |
+ | _E0 | $time >= 0.98$ | $0.1$  | $x = 2, y = y + x$      |
  | _E1 | $time >= 0.99$ | $0.1$  | $q = p + 1, p = 3$      |]
 
 
