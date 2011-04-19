b22868013ab9c5bec82e088ba9d351c8dead6eb1
Sarah Keating <skeating@users.noreply.github.com>
2011-04-19 15:28:21 +0000

    no delay in L1 - so removed the l1 model and the tag

--- cases/semantic/00939/00939-model.m
+++ cases/semantic/00939/00939-model.m
@@ -5,7 +5,7 @@ synopsis:      A species that with an initial concentration set, and subsequentl
 componentTags: Compartment, Parameter, Species, Reaction, CSymbolDelay, AssignmentRule
 testTags:      Amount, NonConstantParameter
 testType:      TimeCourse
-levels:        1.2, 2.1, 2.2, 2.3, 2.4, 3.1
+levels:        2.1, 2.2, 2.3, 2.4, 3.1
 generatedBy:   Analytic
 
  The model contains one compartment called "default_compartment".  There is a single species x with an initial concentration, which at the start of the simulation increases due to a reaction.  A parameter (y) echoes this species with a 0.2 second delay, meaning that it starts by 'seeing' the initial constant value before echoing the linear increase.
