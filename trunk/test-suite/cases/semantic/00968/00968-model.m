a03db16fded284b1a1bae9962662aaf40457d105
luciansmith <luciansmith@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-06-01 17:44:31 +0000

    Two cases with a changed tag:  AssignedConstantStoichiometry.

--- trunk/test-suite/cases/semantic/00968/00968-model.m
+++ trunk/test-suite/cases/semantic/00968/00968-model.m
@@ -3,7 +3,7 @@
 category:      Test
 synopsis:      A very simple reaction with stoichiometry set by an initialAssignment.
 componentTags: Parameter, Species, InitialAssignment
-testTags:      Amount, AssignedStoichiometry
+testTags:      Amount, AssignedConstantStoichiometry
 testType:      TimeCourse
 levels:        3.1
 generatedBy:   Analytic
