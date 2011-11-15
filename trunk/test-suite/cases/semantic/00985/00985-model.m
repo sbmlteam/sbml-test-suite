09eac995ac3f5974845775b4f53d3e3771f3fe37
luciansmith <luciansmith@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-11-15 17:40:59 +0000

    Various tests for non-constant CSymbolDelay:  delays that vary directly with reference to time, and delays that refer to variables that change with assignment rules, events, and delays of their own.

---
+++ trunk/test-suite/cases/semantic/00985/00985-model.m
@@ -0,0 +1,20 @@
+(* 
+
+category:      Test
+synopsis:      An assignment rule with a continually-changing delay. 
+componentTags: Parameter, CSymbolTime, CSymbolDelay
+testTags:      NonConstantParameter
+testType:      TimeCourse
+levels:        2.1, 2.2, 2.3, 2.4, 3.1
+generatedBy:   Analytic
+
+ This model contains a single parameter x changing at a constant rate, and a second parameter z that follows x with a delay of 1, and a third parameter y that follows x with a delay of z.
+
+The initial conditions are as follows:
+
+[{width:30em,margin-left:5em}| |*Value*       |
+|Value of parameter x       |$0$          |
+|Value of parameter y       |$0$          |]
+
+
+*)
