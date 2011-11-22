1638f18ff04327b923ce06cf537ec824377b2cbb
luciansmith <luciansmith@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-11-22 18:50:12 +0000

    Fixed the description of this test.

--- trunk/test-suite/cases/semantic/00988/00988-model.m
+++ trunk/test-suite/cases/semantic/00988/00988-model.m
@@ -8,13 +8,13 @@ testType:      TimeCourse
 levels:        2.1, 2.2, 2.3, 2.4, 3.1
 generatedBy:   Analytic
 
-After the event fires, the 'fast' reaction should go to completion instantly, converting all of A to B.  Then B converts to C normally.
+ After B is converted to A by the slow reaction for a while, eventually the two events trigger, setting the reaction rate of the fast reaction to 1*A--consuming all of A and turning it back into B.  Then it turns back off, and B is once again slowly converted to A.  This would cycle again, but we only test one cycle.
 
 The initial conditions are as follows:
 
 [{width:30em,margin-left:5em}| |*Value*       |
 |Value of species A       |$0$          |
-|Value of species B       |$1$          |]
+|Value of species B       |$2$          |]
 
 
 *)
