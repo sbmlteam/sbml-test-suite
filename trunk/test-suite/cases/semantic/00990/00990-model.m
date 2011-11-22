9d897fc3231ce1e18c98d53f720c942fc91c2f52
luciansmith <luciansmith@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-11-22 18:49:56 +0000

    Several new models to check AssignedVariableStoichiometry.  The stoichiometry of the reaction is set, either directly or by proxy, to variables that change because they are species, or because they are changed by rules or events.

---
+++ trunk/test-suite/cases/semantic/00990/00990-model.m
@@ -0,0 +1,20 @@
+(* 
+
+category:      Test
+ synopsis:      A simple reaction with stoichiometry set to a parameter that varies due to a rate law.
+componentTags: Parameter, Species, Compartment, RateRule
+testTags:      Amount, AssignedVariableStoichiometry
+testType:      TimeCourse
+levels:        2.1, 2.2, 2.3, 2.4, 3.1
+generatedBy:   Numeric
+
+ The stoichiometry of the reaction '-> nX' is set to be equal to p1, a parameter that starts at 1 and increases at a rate of 1.
+
+ The initial conditions are as follows:
+
+[{width:30em,margin-left:5em}| |*Value*       |
+|Initial amount of X        |$1$                  |
+|Value of parameter p1       |$1$          |
+|Volume of compartment default_compartment     |$1$             |]
+
+*)
---
+++ trunk/test-suite/cases/semantic/00992/00992-model.m
@@ -0,0 +1,21 @@
+(* 
+
+category:      Test
+synopsis:      A simple reaction with stoichiometry set to a parameter, which in turn is set to be the same as its species.
+componentTags: Parameter, Species, Compartment, AssignmentRule
+testTags:      Amount, AssignedVariableStoichiometry
+testType:      TimeCourse
+levels:        2.1, 2.2, 2.3, 2.4, 3.1
+generatedBy:   Numeric
+
+ The stoichiometry of the reaction '-> nX' is set to be equal to p1, a parameter that is set by an assignment rule to be equal to X.
+
+The initial conditions are as follows:
+
+[{width:30em,margin-left:5em}| |*Value*       |
+|Initial amount of X        |$1$                  |
+|Value of parameter k1       |$1$          |
+|Value of parameter p1       |$1$          |
+|Volume of compartment default_compartment     |$1$             |]
+
+*)
