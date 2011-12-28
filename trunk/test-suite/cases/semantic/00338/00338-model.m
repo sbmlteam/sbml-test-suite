a0a674b7d6e8b0e419b2bf090c85d3337a9ff9cb
luciansmith <luciansmith@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-12-28 21:08:08 +0000

    testtype -> testType.  In case anyone ever uses that at any time for anything.

--- trunk/test-suite/cases/semantic/00338/00338-model.m
+++ trunk/test-suite/cases/semantic/00338/00338-model.m
@@ -4,7 +4,7 @@ category:      Test
 synopsis:      Two reactions and a rate rule with four species in a compartment. 
 componentTags: Compartment, Species, Reaction, Parameter, RateRule
 testTags:      Amount, BoundaryCondition
-testtype:      TimeCourse
+testType:      TimeCourse
 levels:        1.2, 2.1, 2.2, 2.3, 2.4, 3.1
 generatedBy:   Analytic
 
