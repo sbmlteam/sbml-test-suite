d8d1140e7636156b413e367cbc3699beb9bb9872
Lucian Smith <luciansmith@users.noreply.github.com>
2012-01-05 00:16:34 +0000

    Two new tests!  999 test various combinations of species being set iV or iC, vs hOSU=true/false. 1000 is the kitchen sink model, with everything but Algebraic rules, fast reactions, delay equations, and random event assignments.  Yay model 1000!  I figured we should have something special in it to commemorate it.
    I haven't found a simulator to run either of these tests--my version of roadrunner doesn't get the results I expect from 999, though I think Frank has a version that will do it correctly.  For model 1000, I didn't put anything in it that RoadRunner shouldn't handle, but it fell over and died anyway.  Do I get travel support to HARMONY? ;-)
    
    The results file for 1000 is completely wrong; it doesn't even have the right variables listed.  I'm hoping once Frank gets me a version of RoadRunner that will produce some output, I can stick that in there instead, and then make other people try to defend their own results, which will almost certainly be different ;-)

---
+++ cases/semantic/01000/01000-model.m
@@ -0,0 +1,66 @@
+(*
+
+category:      Test
+synopsis:      A 'kitchen sink' model with 34 tags.  Woo-hoo!
+componentTags: AssignmentRule, CSymbolAvogadro, CSymbolTime, Compartment, EventNoDelay, EventPriority, EventWithDelay, FunctionDefinition, InitialAssignment, Parameter, RateRule, Reaction, Species
+testTags:      Amount, Concentration, AssignedConstantStoichiometry, AssignedVariableStoichiometry, BoundaryCondition, ConstantSpecies, ConversionFactors, EventIsNotPersistent, EventIsPersistent, EventT0Firing, EventUsesAssignmentTimeValues, EventUsesTriggerTimeValues, HasOnlySubstanceUnits, InitialValueReassigned, LocalParameters, MultiCompartment, NonConstantCompartment, NonConstantParameter, NonUnityCompartment, NonUnityStoichiometry, ReversibleReaction
+testType:      TimeCourse
+levels:        3.1
+generatedBy:   Numeric
+
+ This is a 'kitchen sink' model with almost everything in it.  It's a RoadRunner-friendly model in that it only doesn't have bits that RoadRunner can't handle:  AlgebraicRules, CSymbolDelays, and FastReactions.  It also does not test RandomEventExecution because those tests are fundamentally difficult to put together, and inherently non-precise in their output.  It is, frankly, a fairly ridiculous model with things thrown in willy-nilly with no regard to unit correctness in the interest of simply testing everything together.  The canonical results were simply taken from RoadRunner output:  no other guarantee is made of its correctness.
+
+The model contains:
+* 4 species (S1, S2, S3, S4)
+* 7 parameters (k1, k3, k4, k5, k2, conversion1, conversion2)
+* 2 compartments (comp, comp2)
+
+It also contains 1 function definition(s):
+; kinetics: $-a * b * c * d * e * f * g * h * i / j$
+
+There is one reaction:
+
+[{width:30em,margin-left:5em}|  *Reaction*  |  *Rate*  |
+| S3 + S1ref S1 -> S2ref S2 | $kinetics(k1, k2, k3, k4, k5, S1, S1ref, S3, S4, S2)$ |]
+Note:  the following stoichiometries are set separately:  S1ref, S2ref
+
+
+There are 8 events:
+
+[{width:55em,margin-left:5em}|  *Event*  |  *Trigger*  |  *Priority*  |  *Persistent*  |  *initialValue*  |  *Use values from:*  |  *Delay*  | *Event Assignments* |
+| _E0 | $geq(time, 0.5)$ | (unset) | true | true | Trigger time | $0$ | $k4 = 0$ |
+| _E1 | $geq(time, 1)$ | $1$ | true | true | Trigger time | $2$ | $k3 = 4$ |
+| _E2 | $geq(time, 1)$ | $2$ | true | true | Assignment time | $2$ | $k4 = k3$ |
+| _E3 | $geq(time, 1)$ | $3$ | true | true | Trigger time | $2$ | $k5 = k4$ |
+| _E4 | $geq(time, 1)$ | $4$ | true | true | Assignment time | $2$ | $k3 = k5$ |
+| _E5 | $gt(comp, 5)$ | (unset) | true | false | Trigger time | $1$ | $S3 = 4$ |
+| _E6 | $and(geq(time, 1), leq(time, 4))$ | (unset) | true | true | Trigger time | $5$ | $k4 = 0$ |
+| _E7 | $and(geq(time, 1), leq(time, 4))$ | (unset) | false | true | Trigger time | $5$ | $k5 = 0$ |]
+
+
+There are 4 rules:
+
+[{width:30em,margin-left:5em}|  *Type*  |  *Variable*  |  *Formula*  |
+| Rate | comp | $1$ |
+| Assignment | comp2 | $k4$ |
+| Rate | k4 | $time$ |
+| Assignment | S2ref | $k1 * S1$ |]
+
+The initial conditions are as follows:
+
+[{width:35em,margin-left:5em}|       | *Value* | *Constant* |
+| Initial concentration of species S4 | $2$ | constant |
+| Initial amount of species S1 | $1$ | variable |
+| Initial concentration of species S2 | $3$ | variable |
+| Initial amount of species S3 | $4$ | variable |
+| Initial value of parameter k1 | $avogadro$ | constant |
+| Initial value of parameter k2 | $8.12$ | constant |
+| Initial value of parameter conversion1 | $10$ | constant |
+| Initial value of parameter conversion2 | $100$ | constant |
+| Initial value of parameter k3 | $2.5$ | variable |
+| Initial value of parameter k4 | $time$ | variable |
+| Initial value of parameter k5 | $2.8$ | variable |
+| Initial volume of compartment 'comp' | $1$ | variable |
+| Initial volume of compartment 'comp2' | $k4$ | variable |]
+
+*)
