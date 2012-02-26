6feb3f7b704a4b650ad0e3953d0b1fae7b7e422c
luciansmith <luciansmith@c05c2498-8750-0410-b44c-b7d1844e3258>
2012-02-26 21:21:59 +0000

    New tests that test:  - Proper interpretation of n-ary MathML constructs plus, times, and, or, and xor.  Previous implementations of libsbml required these constructs to have at least two children, but this was not MathML-compliant.  Proper interpretation of these constructs requires that you allow 0 or 1 arguments as well.  If you're using pre-5.4 libsbml, these models will fail validation, but they should not; you may have to upgrade to be able to pass these tests.  - Proper interpretation of piecewise functions without 'otherwise'  - Proper interpretation of species concentrations where only the compartment volume changes (the species amount should remain constant).  - Proper interpretation of event priorities for events with identical triggers and delays (as found as a part of test 1000 that wasn't being tested elsewhere).

---
+++ trunk/test-suite/cases/semantic/01118/01118-model.m
@@ -0,0 +1,32 @@
+(*
+
+category:      Test
+synopsis:      A model that tests how not to vary a species.
+componentTags: Compartment, RateRule, Species
+testTags:      Amount, ConstantSpecies, NonConstantCompartment, NonUnityCompartment
+testType:      TimeCourse
+levels:        1.2, 2.1, 2.2, 2.3, 2.4, 3.1
+generatedBy:   Analytic
+
+ This model tests that the simulator should, by default, assume that a species set boundaryCondition=true and hasOnlySubstanceUnits=false, residing in a compartment that changes over time, should keep its *amount* constant, not its concentration.
+
+The model contains:
+* 1 species (S1)
+* 1 compartment (C)
+
+There is one rule:
+
+[{width:30em,margin-left:5em}|  *Type*  |  *Variable*  |  *Formula*  |
+| Rate | C | $1$ |]
+
+The initial conditions are as follows:
+
+[{width:35em,margin-left:5em}|       | *Value* | *Constant* |
+| Initial concentration of species S1 | $1$ | constant |
+| Initial volume of compartment 'C' | $1$ | variable |]
+
+{Keep this next line if 'generatedBy' is 'Analytic':}
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+
+*)
