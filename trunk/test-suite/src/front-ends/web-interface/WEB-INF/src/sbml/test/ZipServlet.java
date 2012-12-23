f1b46bc6955f4a7ef4f43998b32d2f682592c48b
mhucka <mhucka@c05c2498-8750-0410-b44c-b7d1844e3258>
2012-12-23 23:46:01 +0000

    We currently also provide the .m file in the downloads.

--- trunk/test-suite/src/front-ends/web-interface/WEB-INF/src/sbml/test/ZipServlet.java
+++ trunk/test-suite/src/front-ends/web-interface/WEB-INF/src/sbml/test/ZipServlet.java
@@ -224,7 +224,7 @@ public class ZipServlet extends HttpServlet
         // 'rx' is the start of the regex, but note that it's not terminated.
         // We add some more pieces below, then close it off at the end.
 
-        String rx = new String("(\\d{5}-plot.jpg|\\d{5}-settings.txt|\\d{5}-model.html|\\d{5}-results.csv");
+        String rx = new String("(\\d{5}-plot.jpg|\\d{5}-settings.txt|\\d{5}-model.html|\\d{5}-model.m|\\d{5}-results.csv");
       
         if (levelAndVersion != null)      // Shouldn't happen, but let's be safe.
             for (int i = 0; i < KNOWN_LV_COMBOS.length; i++)
