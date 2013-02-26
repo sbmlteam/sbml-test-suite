c8f7d1a0ed130c3a50c8b67a4e0cbcca5b0098fb
Mike Hucka <mhucka@users.noreply.github.com>
2013-02-26 03:41:01 +0000

    Made TaskExecutor its own separate class.

---
+++ src/front-ends/new-standalone/testsuite-ui/src/org/sbml/testsuite/ui/TaskExecutor.java
@@ -0,0 +1,89 @@
+//
+// @file   TaskExecutor.java
+// @brief  Helper class for executing processes
+// @author Michael Hucka
+// @date   Created 2013-02-20 <mhucka@caltech.edu>
+//
+// ----------------------------------------------------------------------------
+// This file is part of the SBML Testsuite. Please visit http://sbml.org for
+// more information about SBML, and the latest version of the SBML Test Suite.
+//
+// Copyright (C) 2009-2012 jointly by the following organizations:
+// 1. California Institute of Technology, Pasadena, CA, USA
+// 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+//
+// Copyright (C) 2006-2008 by the California Institute of Technology,
+// Pasadena, CA, USA
+//
+// Copyright (C) 2002-2005 jointly by the following organizations:
+// 1. California Institute of Technology, Pasadena, CA, USA
+// 2. Japan Science and Technology Agency, Japan
+//
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation. A copy of the license agreement is provided
+// in the file named "LICENSE.txt" included with this software distribution
+// and also available online as http://sbml.org/software/libsbml/license.html
+// ----------------------------------------------------------------------------
+
+package org.sbml.testsuite.ui;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.eclipse.swt.widgets.Display;
+
+
+/**
+ * Helper class for executing processes.
+ */
+class TaskExecutor
+{
+    private ExecutorService ex;
+
+    public TaskExecutor()
+    {
+        init(false);
+    }
+
+
+    public void init(boolean concurrencyOK)
+    {
+        if (ex != null && ! ex.isTerminated())
+            ex.shutdownNow();
+
+        if (concurrencyOK)
+        {
+            int numProcs   = Runtime.getRuntime().availableProcessors();
+            int numThreads = Math.max(1, numProcs - 1);
+            ex = Executors.newFixedThreadPool(numThreads);
+        }
+        else
+        {
+            ex = Executors.newSingleThreadExecutor();
+        }
+    }
+
+
+    public void execute(Runnable command)
+    {
+        ex.execute(command);
+    }
+
+
+    public void waitForProcesses(Display display)
+    {
+        if (ex == null)
+            return;
+        ex.shutdown();        // Finish the threads we started.
+        while (!ex.isTerminated())
+        {
+            display.readAndDispatch();
+        }
+    }
+
+
+    public void shutdownNow()
+    {
+        ex.shutdownNow();
+    }
+}
