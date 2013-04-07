49598641450c82fa2acae6e36cc26a904c332a3e
Mike Hucka <mhucka@users.noreply.github.com>
2013-04-07 03:13:31 +0000

    New class to display a banner under the toolbar, used to indicate when filters are in effect.

---
+++ src/front-ends/new-standalone/testsuite-ui/src/org/sbml/testsuite/ui/NotificationBanner.java
@@ -0,0 +1,85 @@
+//
+// @file   NotificationBanner.java
+// @brief  Banner used to show notifications such as when filters are in effect
+// @author Michael Hucka
+// @@date  Created 2013-04-01 <mhucka@caltech.edu>
+//
+// ----------------------------------------------------------------------------
+// This file is part of the SBML Testsuite. Please visit http://sbml.org for
+// more information about SBML, and the latest version of the SBML Test Suite.
+//
+// Copyright (C) 2009-2012 jointly by the following organizations:
+// 1. California Institute of Technology, Pasadena, CA, USA
+// 2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+//
+// Copyright (C) 2006-2008 by the California Institute of Technology,
+// Pasadena, CA, USA
+//
+// Copyright (C) 2002-2005 jointly by the following organizations:
+// 1. California Institute of Technology, Pasadena, CA, USA
+// 2. Japan Science and Technology Agency, Japan
+//
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation. A copy of the license agreement is provided
+// in the file named "LICENSE.txt" included with this software distribution
+// and also available online as http://sbml.org/software/libsbml/license.html
+// ----------------------------------------------------------------------------
+
+package org.sbml.testsuite.ui;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.widgets.Shell;
+import org.sbml.testsuite.core.TestCase;
+
+
+public class NotificationBanner
+    extends CLabel
+{
+    private FormAttachment bottomAttachment = new FormAttachment(0, 0);
+
+
+    public NotificationBanner(Shell shell, int style)
+    {
+        super(shell, style);
+
+        FormData formData = new FormData();
+        formData.top = new FormAttachment(0, 0);
+        formData.bottom = bottomAttachment;
+        formData.left = new FormAttachment(0, 0);
+        formData.right = new FormAttachment(100, 0);
+        setLayoutData(formData);
+    }
+
+
+    public void show(boolean doShow)
+    {
+        if (doShow)
+        {
+            FontData[] fontData = getFont().getFontData();
+            int tallest = 0;
+            for (int i = 0; i < fontData.length; ++i)
+                if (fontData[i].getHeight() > tallest)
+                    tallest = fontData[i].getHeight();
+            tallest += 4;
+            bottomAttachment.offset = tallest;
+            setVisible(true);
+        }
+        else
+        {
+            bottomAttachment.offset = 0;
+            setVisible(false);
+        }
+
+        computeSize(SWT.DEFAULT, SWT.DEFAULT, true);
+        getShell().layout();
+    }
+}
