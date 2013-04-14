b8775da380b89125b7b699e8ebc559dc63fc59b2
mhucka <mhucka@c05c2498-8750-0410-b44c-b7d1844e3258>
2013-04-14 01:36:51 +0000

    Removed unneeded imports.

--- trunk/test-suite/src/front-ends/new-standalone/testsuite-ui/src/org/sbml/testsuite/ui/EditListOfWrappers.java
+++ trunk/test-suite/src/front-ends/new-standalone/testsuite-ui/src/org/sbml/testsuite/ui/EditListOfWrappers.java
@@ -33,8 +33,6 @@ package org.sbml.testsuite.ui;
 import java.util.Vector;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
-import org.eclipse.swt.events.KeyEvent;
-import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FormAttachment;
@@ -44,9 +42,8 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Shell;
 import org.sbml.testsuite.core.WrapperConfig;
 
 /**
