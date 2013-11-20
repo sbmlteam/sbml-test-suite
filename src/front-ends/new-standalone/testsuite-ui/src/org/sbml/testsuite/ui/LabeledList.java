d573215c12089c6b58e00fa647d6e0b3a0a8bee9
Mike Hucka <mhucka@users.noreply.github.com>
2013-11-20 03:23:46 +0000

    Discovered that SWT's List class doesn't respond to mouse hover events.  (I checked the SWT source code.) In order to implement tooltips summarizing the meanings of the tags, I had to use something other than List, so I changed the implementation here to use Table.

--- src/front-ends/new-standalone/testsuite-ui/src/org/sbml/testsuite/ui/LabeledList.java
+++ src/front-ends/new-standalone/testsuite-ui/src/org/sbml/testsuite/ui/LabeledList.java
@@ -1,9 +1,9 @@
 //
-// @file LabeledList.java
-// @brief Utility Composite holding a list and a label
+// @file   LabeledList.java
+// @brief  A composite holding a selectable list, a title, and buttons
 // @author Frank T. Bergmann
-// @date Created 2012-06-06 <fbergman@caltech.edu>
-//
+// @author Michael Hucka
+// @date   Created 2012-06-06 <fbergman@caltech.edu>
 //
 // ----------------------------------------------------------------------------
 // This file is part of the SBML Testsuite. Please visit http://sbml.org for
@@ -26,25 +26,23 @@
 // in the file named "LICENSE.txt" included with this software distribution
 // and also available online as http://sbml.org/software/libsbml/license.html
 // ----------------------------------------------------------------------------
-//
 
 package org.sbml.testsuite.ui;
 
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.layout.FormLayout;
-import org.eclipse.swt.widgets.Label;
+import java.util.Collection;
+import java.util.Vector;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Listener;
-import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Item;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
 
 
 /**
@@ -54,14 +52,14 @@ public class LabeledList
     extends Composite
 {
     private Label  label;
-    private List   list;
+    private Table  table;
     private Button button1;
     private Button button2;
 
 
     /**
      * Create the composite.
-     * 
+     *
      * @param parent
      * @param style
      */
@@ -79,13 +77,13 @@ public class LabeledList
         label.setLayoutData(fd_lblLabel);
         label.setText("New Label");
 
-        list = new List(this, SWT.MULTI | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
-        FormData fd_list = new FormData();
-        fd_list.bottom = new FormAttachment(100, -margin);
-        fd_list.left = new FormAttachment(0, 2*margin);
-        fd_list.right = new FormAttachment(100, -2*margin);
-        list.setLayoutData(fd_list);
-        
+        table = new Table(this, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+        FormData fd_table = new FormData();
+        fd_table.bottom = new FormAttachment(100, -margin);
+        fd_table.left = new FormAttachment(0, 2*margin);
+        fd_table.right = new FormAttachment(100, -2*margin);
+        table.setLayoutData(fd_table);
+
         int buttonWidth = 80;
 
         // button1 is left of button2, but we use button2 to align button1.
@@ -106,7 +104,7 @@ public class LabeledList
         fd_button1.right = new FormAttachment(button2, -offset);
         button1.setLayoutData(fd_button1);
 
-        fd_list.top = new FormAttachment(button1, offset);
+        fd_table.top = new FormAttachment(button1, offset);
         fd_lblLabel.top = new FormAttachment(button1, margin, SWT.TOP);
 
         pack();
@@ -150,15 +148,15 @@ public class LabeledList
     /**
      * @return the list
      */
-    public List getList()
+    public Table getTable()
     {
-        return list;
+        return table;
     }
 
 
     /**
-     * toggles visibility of the add button
-     * 
+     * Toggles visibility of the add button
+     *
      * @param isVisible
      *            indication whether the button should be visible
      */
@@ -170,8 +168,8 @@ public class LabeledList
 
 
     /**
-     * set the label text
-     * 
+     * Sets the label text.
+     *
      * @param label
      *            the text
      */
@@ -182,14 +180,139 @@ public class LabeledList
 
 
     /**
-     * Sets the list
-     * 
-     * @param list
-     *            the list
+     * Adds a mouse listener to the Control containing the list of items.
      */
-    public void setList(List list)
+    public void addMouseListener(final MouseAdapter listener)
     {
-        this.list = list;
+        table.addMouseListener(listener);
     }
 
+
+    /**
+     * Returns the set of currently selected items, as an array of strings.
+     */
+    public String[] getSelection()
+    {
+        TableItem[] items = table.getSelection();
+        String[] selection = new String[items.length];
+        for (int i = 0; i < items.length; i++)
+            selection[i] = items[i].getText();
+        return selection;
+    }
+
+
+    /**
+     * Add a single string to the list of items shown.
+     */
+    public void add(String name)
+    {
+        if (name == null || name.length() == 0) return;
+        if (indexOf(name) != -1) return;
+        TableItem item = new TableItem(table, SWT.NONE);
+        item.setText(name);
+    }
+
+
+    /**
+     * Add all the given strings to the list of items shown.
+     */
+    public void add(String[] names)
+    {
+        if (names == null) return;
+        for (String name : names)
+            add(name);
+    }
+
+
+    /**
+     * Add a collection of strings to the list of items shown.
+     */
+    public void add(Collection<String> collection)
+    {
+        if (collection == null) return;
+        for (String name : collection)
+            add(name);
+    }
+
+
+    /**
+     * Remove the named item from the list.
+     */
+    public void remove(String name)
+    {
+        if (name == null) return;
+        int index = indexOf(name);
+        if (index > -1) table.remove(index);
+    }
+
+
+    /**
+     * Remove all the given items from the list.
+     */
+    public void remove(String[] names)
+    {
+        if (names == null) return;
+        for (String name : names)
+            remove(name);
+    }
+
+
+    /**
+     * Remove all items from the list.
+     */
+    public void removeAll()
+    {
+        table.removeAll();
+    }
+
+
+    /**
+     * Returns the control that implements the list.
+     *
+     * This is useful for getting at the layout or other properties of the
+     * control.  However, do not use this to reset the contents of the control
+     * directly.
+     */
+    public Control getControl()
+    {
+        return table;
+    }
+
+
+    /**
+     * Return all the Item objects in the list.
+     */
+    public Collection<Item> getItems()
+    {
+        Vector<Item> result = new Vector<Item>();
+        for (TableItem item : table.getItems())
+            result.add(item);
+        return result;
+    }
+
+
+    /**
+     * Return all the list items, as plain strings.
+     */
+    public Collection<String> getStrings()
+    {
+        Vector<String> result = new Vector<String>();
+        for (TableItem item : table.getItems())
+            result.add(item.getText());
+        return result;
+    }
+
+
+    /**
+     * Return an integer index for the named item.
+     */
+    public int indexOf(String name)
+    {
+        if (name == null) return -1;
+        int totalItems = table.getItemCount();
+        for (int i = 0; i < totalItems; i++)
+            if (table.getItem(i).getText().equals(name))
+                return i;
+        return -1;
+    }
 }
