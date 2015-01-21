40411b2f835449d7e9da0c7165195b68f5a9f315
luciansmith <luciansmith@c05c2498-8750-0410-b44c-b7d1844e3258>
2015-01-21 00:06:19 +0000

    New tests:   * A compartment with spatialDimensions of 7.   * A species under control of two rate rules (one for itself; one for its compartment)   * Various forms of nested 'piecewise' functions   * A species under control of only one rate rule, assigned to a parameter (a simpler version of the above).

---
+++ trunk/test-suite/cases/semantic/01197/01197-model.m
@@ -0,0 +1,24 @@
+(*
+
+category:        Test
+synopsis:        Compartment with spatial dimensions of 7.
+componentTags:   Compartment
+testTags:        NonUnityCompartment
+testType:        TimeCourse
+levels:          3.1
+generatedBy:     Analytic
+packagesPresent: 
+
+This model contains only a compartment, but it has a spatial dimension of 7.  The numerics are not affected at all; this simply tests that the existence of this dimensionality doesn't affect the simulation.
+
+The model contains:
+* 1 compartment (c)
+The initial conditions are as follows:
+
+[{width:35em,margin: 1em auto}|       | *Value* | *Constant* |
+| Initial volume of compartment 'c' | $4$ | constant |]
+
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+
+*)
