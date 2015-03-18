e33437d626435db4cae45ce01fe1d7b6ebc3b8d5
Lucian Smith <luciansmith@users.noreply.github.com>
2015-03-18 00:25:37 +0000

    These models do not actually have valid l1v2 versions.

--- cases/semantic/01202/01202-model.m
+++ cases/semantic/01202/01202-model.m
@@ -5,7 +5,7 @@ synopsis:        Nested piecewise in otherwise.
 componentTags:   AssignmentRule, Parameter, RateRule
 testTags:        InitialValueReassigned, NonConstantParameter, UncommonMathML
 testType:        TimeCourse
-levels:          1.2, 2.1, 2.2, 2.3, 2.4, 3.1
+levels:          2.1, 2.2, 2.3, 2.4, 3.1
 generatedBy:     Analytic
 packagesPresent: 
 
