612f2e815b1c5ee94b85ef97c48ea729fc88b41f
Lucian Smith <lpsmith@uw.edu>
2016-08-09 13:57:18 -0700

    Originally, these tests were supposed to have species in multiple compartments, but somewhere along the way, all the species ended up in a single compartment, leaving the second compartment empty.  This moves the species back into their originally-intended compartments.  No results are changed as a result.

--- cases/semantic/00289/00289-model.m
+++ cases/semantic/00289/00289-model.m
@@ -1,22 +1,29 @@
-(* 
+(*
 
-category:      Test
-synopsis:      Reactions occurring between two compartments. 
-componentTags: Compartment, Species, Reaction, Parameter, AssignmentRule 
-testTags:      Amount, MultiCompartment, InitialValueReassigned, NonUnityStoichiometry
-testType:      TimeCourse
-levels:        1.2, 2.1, 2.2, 2.3, 2.4, 2.5, 3.1
-generatedBy:   Numeric
+category:        Test
+synopsis:        Reactions occurring between two compartments. 
+componentTags:   AssignmentRule, Compartment, Parameter, Reaction, Species
+testTags:        Amount, InitialValueReassigned, MultiCompartment, NonUnityStoichiometry
+testType:        TimeCourse
+levels:          1.2, 2.1, 2.2, 2.3, 2.4, 2.5, 3.1
+generatedBy:     Numeric
+packagesPresent: 
 
 The model contains two compartments named compartment and compartment1.
 There are four species named S1, S2, S3 and S4 and three parameters named
 k1, k2 and k3.  Compartment "compartment" contains species S1 and S2.
-Compartment "compartment"1 contains species S3 and S4.  The model contains
-two reactions defined as:
+Compartment "compartment1" contains species S3 and S4.
+
+The model contains:
+* 4 species (S1, S2, S3, S4)
+* 3 parameters (k1, k2, k3)
+* 2 compartments (compartment, compartment1)
+
+There are 2 reactions:
 
 [{width:30em,margin: 1em auto}|  *Reaction*  |  *Rate*  |
-| S1 + S2 -> S2  +  S2 | $k1 * S1 * S2 * compartment$  |
-| S2 -> S3         | $k2 * (S2-S3) * compartment1$  |]
+| reaction1: S1 + S2 -> 2S2 | $compartment * k1 * S1 * S2$ |
+| reaction2: S2 -> S3 | $compartment1 * k2 * (S2 + -1 * S3)$ |]
 
 The first reaction occurs entirely in compartment, whereas the second reaction
 occurs between a species in compartment and a species in compartment1.
@@ -24,7 +31,7 @@ occurs between a species in compartment and a species in compartment1.
 The model contains one rule which assigns value to species S4:
 
 [{width:30em,margin: 1em auto}|  *Type*  |  *Variable*  |  *Formula*  |
- | Assignment | S4 | $k3 * S3$  |]
+| Assignment | S4 | $k3 * S3$ |]
 
 In this case the initial value declared for species S4 is inconsistent with
 that calculated by the assignmentRule; the calculated value should be used.
@@ -33,21 +40,21 @@ considered during simulation.
 
 The initial conditions are as follows:
 
-[{width:30em,margin: 1em auto}|       |*Value*          |*Units*  |
-|Initial amount of S1                 |$          1.0$ |mole                      |
-|Initial amount of S2                 |$          1.0$ |mole                      |
-|Initial amount of S3                 |$            0$ |mole                      |
-|Initial amount of S4                 |$          1.0$ |mole                      |
-|Value of parameter k1                |$         0.75$ |litre mole^-1^ second^-1^ |
-|Value of parameter k2                |$         0.25$ |second^-1^ |
-|Value of parameter k2                |$         0.25$ |dimensionless |
-|Volume of compartment "compartment"  |$            1$ |litre                     |
-|Volume of compartment "compartment"1 |$            1$ |litre                     |]
+[{width:35em,margin: 1em auto}|       | *Value* | *Constant* |
+| Initial amount of species S1 | $1$ | variable |
+| Initial amount of species S2 | $1$ | variable |
+| Initial amount of species S3 | $0$ | variable |
+| Initial concentration of species S4 | $k3 * S3$ | variable |
+| Initial value of parameter k1 | $0.75$ | constant |
+| Initial value of parameter k2 | $0.25$ | constant |
+| Initial value of parameter k3 | $0.25$ | constant |
+| Initial volume of compartment 'compartment' | $1$ | constant |
+| Initial volume of compartment 'compartment1' | $1$ | constant |]
 
-The species values are given as amounts of substance to make it easier to
-use the model in a discrete stochastic simulator, but (as per usual SBML
-principles) their symbols represent their values in concentration units
-where they appear in expressions.
+The species' initial quantities are given in terms of substance units to
+make it easier to use the model in a discrete stochastic simulator, but
+their symbols represent their values in concentration units where they
+appear in expressions.
 
 *)
 
