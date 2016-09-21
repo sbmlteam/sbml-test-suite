6844c8e5012d2f9975cbb565cb9a0d111368b213
Lucian Smith <lpsmith@uw.edu>
2016-09-21 09:01:12 +0100

    Fix for Frank:  the two assignment rules were actually identical, and the results were wrong.  Expanded to four parameters to cover all combinations of < or >, for 5.5 and 2.5.

--- cases/semantic/01279/01279-model.m
+++ cases/semantic/01279/01279-model.m
@@ -9,21 +9,25 @@ levels:          3.2
 generatedBy:     Numeric
 packagesPresent: 
 
- In this model, the 'implies' MathML symbol is used in assignment rules.  'Implies' means 'if A is true, then B is also true', or '(not A) or B'.  With the automatic translation (in l3v2) of boolean values to numeric values, this means that only p2 becomes 0 (false) during the middle of the run.
+ In this model, the 'implies' MathML symbol is used in assignment rules.  'Implies' means 'if A is true, then B is also true', or '(not A) or B'.  With the automatic translation (in l3v2) of boolean values to numeric values, this means that p1 is always 1 (true); p2 becomes 0 (false) halfway through the run; p3 becomes 0 only from 2.5<t<5.5, and p4 is 0 while t<2.5.
 
 The model contains:
-* 2 parameters (p1, p2)
+* 4 parameters (p1, p2, p3, p4)
 
-There are 2 rules:
+There are 4 rules:
 
 [{width:30em,margin: 1em auto}|  *Type*  |  *Variable*  |  *Formula*  |
 | Assignment | p1 | $implies((time > 5.5), (time > 2.5))$ |
-| Assignment | p2 | $implies((time > 2.5), (time > 5.5))$ |]
+| Assignment | p2 | $implies((time > 5.5), (time < 2.5))$ |
+| Assignment | p3 | $implies((time < 5.5), (time > 2.5))$ |
+| Assignment | p4 | $implies((time < 5.5), (time < 2.5))$ |]
 
 The initial conditions are as follows:
 
 [{width:35em,margin: 1em auto}|       | *Value* | *Constant* |
 | Initial value of parameter p1 | $implies((time > 5.5), (time > 2.5))$ | variable |
-| Initial value of parameter p2 | $implies((time > 2.5), (time > 5.5))$ | variable |]
+| Initial value of parameter p2 | $implies((time > 5.5), (time < 2.5))$ | variable |
+| Initial value of parameter p3 | $implies((time < 5.5), (time > 2.5))$ | variable |
+| Initial value of parameter p4 | $implies((time < 5.5), (time < 2.5))$ | variable |]
 
 *)
