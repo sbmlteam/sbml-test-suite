29a56ffbaace8eee05f1d66d2e49922f277ad0f8
Lucian Smith <lpsmith@uw.edu>
2016-09-22 11:01:17 +0100

    Trigger was incorrect: fixed.

--- cases/semantic/01261/01261-model.m
+++ cases/semantic/01261/01261-model.m
@@ -2,8 +2,8 @@
 
 category:        Test
 synopsis:        A rateOf csymbol in an event trigger.
-componentTags:   CSymbolRateOf, EventNoDelay, Parameter, RateRule
-testTags:        NonConstantParameter
+componentTags:   AssignmentRule, CSymbolRateOf, EventNoDelay, Parameter, RateRule
+testTags:        InitialValueReassigned, NonConstantParameter
 testType:        TimeCourse
 levels:          3.2
 generatedBy:     Numeric
@@ -12,26 +12,25 @@ packagesPresent:
  A rateOf csymbol is used here in an event trigger that assigns to p2.
 
 The model contains:
-* 2 parameters (p1, p2)
+* 3 parameters (p1, p3, p2)
 
 There is one event:
 
 [{width:30em,margin: 1em auto}|  *Event*  |  *Trigger*  | *Event Assignments* |
-| _E0 | $rateOf(p1) > 1.05$ | $p2 = 5$ |]
+| _E0 | $rateOf(p1) > 0.0105$ | $p2 = 5$ |]
 
 
-There is one rule:
+There are 2 rules:
 
 [{width:30em,margin: 1em auto}|  *Type*  |  *Variable*  |  *Formula*  |
-| Rate | p1 | $0.01 * p1$ |]
+| Rate | p1 | $0.01 * p1$ |
+| Assignment | p3 | $rateOf(p1)$ |]
 
 The initial conditions are as follows:
 
 [{width:35em,margin: 1em auto}|       | *Value* | *Constant* |
 | Initial value of parameter p1 | $1$ | variable |
+| Initial value of parameter p3 | $rateOf(p1)$ | variable |
 | Initial value of parameter p2 | $10$ | variable |]
-
-Note: The test data for this model was generated from an analytical
-solution of the system of equations.
 
 *)
