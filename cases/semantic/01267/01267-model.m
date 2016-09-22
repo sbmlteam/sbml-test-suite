7e3a0f32d6f602b2def2b9348b464a732a0bc52b
Lucian Smith <lpsmith@uw.edu>
2016-09-22 11:29:09 +0100

    Another fix--rateOf(p1) wasn't same as p1.

--- cases/semantic/01267/01267-model.m
+++ cases/semantic/01267/01267-model.m
@@ -9,7 +9,7 @@ levels:          3.2
 generatedBy:     Analytic
 packagesPresent: 
 
- A variable rateOf csymbol is used here in an event priority that assigns to p2.  At the time the event triggers, the priority is greater than 2.1, meaning that it fires first, and E0 fires second, leaving the value of p2 at 5.
+ A variable rateOf csymbol is used here in an event priority that assigns to p2.  At the time the event triggers, the priority is greater than .021, meaning that it fires first, and E0 fires second, leaving the value of p2 at 5.
 
 The model contains:
 * 2 parameters (p1, p2)
@@ -17,7 +17,7 @@ The model contains:
 There are 2 events:
 
 [{width:35em,margin: 1em auto}|  *Event*  |  *Trigger*  |  *Priority*  | *Event Assignments* |
-| E0 | $time > 5.5$ | $2.1$ | $p2 = 5$ |
+| E0 | $time > 5.5$ | $0.021$ | $p2 = 5$ |
 | E1 | $time > 5.5$ | $rateOf(p1)$ | $p2 = 3$ |]
 
 
