b57efc17d2250594c01757586a86238d6260543b
Lucian Smith <lpsmith@uw.edu>
2016-10-19 11:53:44 -0700

    Reaction identifiers from reactions with no reactants nor products, used in various Math contexts.

---
+++ cases/semantic/01301/01301-model.m
@@ -0,0 +1,30 @@
+(*
+
+category:        Test
+synopsis:        An empty reaction's identifier used in an initial assignment.
+componentTags:   InitialAssignment, Parameter, Reaction
+testTags:        InitialValueReassigned
+testType:        TimeCourse
+levels:          3.2
+generatedBy:     Analytic
+packagesPresent: 
+
+ A reaction with no products nor reactants, but which has a kinetic law, has its identifier used in an initial assignment.
+
+The model contains:
+* 1 parameter (p1)
+
+There is one reaction:
+
+[{width:30em,margin: 1em auto}|  *Reaction*  |  *Rate*  |
+| J0: -> | $3$ |]
+
+The initial conditions are as follows:
+
+[{width:35em,margin: 1em auto}|       | *Value* | *Constant* |
+| Initial value of parameter p1 | $J0$ | variable |]
+
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+
+*)
