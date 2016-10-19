79642281877c5154ddca8934cfcfc744e2d25cfc
Lucian Smith <lpsmith@uw.edu>
2016-10-19 16:54:33 -0700

    Compartment with a spatial dimension of 2.7

---
+++ cases/semantic/01310/01310-model.m
@@ -0,0 +1,24 @@
+(*
+
+category:        Test
+synopsis:        Compartment with spatial dimensions of 2.7.
+componentTags:   Compartment
+testTags:        NonUnityCompartment
+testType:        TimeCourse
+levels:          3.1, 3.2
+generatedBy:     Analytic
+packagesPresent: 
+
+This model contains only a compartment, but it has a spatial dimension of 2.7.  The numerics are not affected at all; this simply tests that the existence of this dimensionality doesn't affect the simulation.
+
+The model contains:
+* 1 compartment (C)
+The initial conditions are as follows:
+
+[{width:35em,margin: 1em auto}|       | *Value* | *Constant* |
+| Initial volume of compartment 'C' | $4$ | constant |]
+
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+
+*)
