16eb69bfdcce89b2ae04f9041b61887dc6d442b8
Lucian Smith <lpsmith@uw.edu>
2017-02-16 15:11:17 -0800

    Tests of uncommon mathML in algebraic rules.

---
+++ cases/semantic/01502/01502-model.m
@@ -0,0 +1,100 @@
+(*
+
+category:        Test
+synopsis:        Several parameters with algebraic rules, testing various L1 built-in functions acting on constants.
+componentTags:   AlgebraicRule, Parameter
+testTags:        InitialValueReassigned, UncommonMathML
+testType:        TimeCourse
+levels:          2.1, 2.2, 2.3, 2.4, 2.5, 3.1, 3.2
+generatedBy:     Analytic
+packagesPresent: 
+
+ The model tests the various mathematical constructs present in L1, in algebraic rules.
+
+The model contains:
+* 36 parameters (P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19, P20, P21, P22, P23, P24, P25, P26, P27, P28, P29, P30, P31, P32, P33, P34, P35, P36, P37)
+
+There are 36 rules:
+
+[{width:30em,margin: 1em auto}|  *Type*  |  *Variable*  |  *Formula*  |
+| Algebraic | $0$ | $P2 - abs(-1)$ |
+| Algebraic | $0$ | $P3 - abs(1)$ |
+| Algebraic | $0$ | $P4 - acos(-1)$ |
+| Algebraic | $0$ | $P5 - acos(0.5)$ |
+| Algebraic | $0$ | $P6 - asin(1)$ |
+| Algebraic | $0$ | $P7 - asin(-0.5)$ |
+| Algebraic | $0$ | $P8 - atan(2.8)$ |
+| Algebraic | $0$ | $P9 - atan(-7.09)$ |
+| Algebraic | $0$ | $P10 - ceil(0.5)$ |
+| Algebraic | $0$ | $P11 - ceil(3.55)$ |
+| Algebraic | $0$ | $P12 - ceil(-4.6)$ |
+| Algebraic | $0$ | $P13 - cos(9.1)$ |
+| Algebraic | $0$ | $P14 - cos(-0.22)$ |
+| Algebraic | $0$ | $P15 - exp(0)$ |
+| Algebraic | $0$ | $P16 - exp(1)$ |
+| Algebraic | $0$ | $P17 - exp(0.77)$ |
+| Algebraic | $0$ | $P18 - floor(-4.6)$ |
+| Algebraic | $0$ | $P19 - floor(9.1)$ |
+| Algebraic | $0$ | $P20 - ln(0.2)$ |
+| Algebraic | $0$ | $P21 - ln(1)$ |
+| Algebraic | $0$ | $P22 - log10(0.2)$ |
+| Algebraic | $0$ | $P23 - log10(1)$ |
+| Algebraic | $0$ | $P24 - 1^2$ |
+| Algebraic | $0$ | $P25 - 2^2$ |
+| Algebraic | $0$ | $P26 - 1.4^5.1$ |
+| Algebraic | $0$ | $P27 - 4^2$ |
+| Algebraic | $0$ | $P28 - 3.1^2$ |
+| Algebraic | $0$ | $P29 - sqrt(4)$ |
+| Algebraic | $0$ | $P30 - sqrt(7.4)$ |
+| Algebraic | $0$ | $P31 - sin(2.1)$ |
+| Algebraic | $0$ | $P32 - sin(0)$ |
+| Algebraic | $0$ | $P33 - sin(-5.9)$ |
+| Algebraic | $0$ | $P34 - tan(0)$ |
+| Algebraic | $0$ | $P35 - tan(1.11)$ |
+| Algebraic | $0$ | $P36 - tan(-6)$ |
+| Algebraic | $0$ | $P37 - tanh(-0.4)$ |]
+
+The initial conditions are as follows:
+
+[{width:35em,margin: 1em auto}|       | *Value* | *Constant* |
+| Initial value of parameter P2 | $unknown$ | variable |
+| Initial value of parameter P3 | $unknown$ | variable |
+| Initial value of parameter P4 | $unknown$ | variable |
+| Initial value of parameter P5 | $unknown$ | variable |
+| Initial value of parameter P6 | $unknown$ | variable |
+| Initial value of parameter P7 | $unknown$ | variable |
+| Initial value of parameter P8 | $unknown$ | variable |
+| Initial value of parameter P9 | $unknown$ | variable |
+| Initial value of parameter P10 | $unknown$ | variable |
+| Initial value of parameter P11 | $unknown$ | variable |
+| Initial value of parameter P12 | $unknown$ | variable |
+| Initial value of parameter P13 | $unknown$ | variable |
+| Initial value of parameter P14 | $unknown$ | variable |
+| Initial value of parameter P15 | $unknown$ | variable |
+| Initial value of parameter P16 | $unknown$ | variable |
+| Initial value of parameter P17 | $unknown$ | variable |
+| Initial value of parameter P18 | $unknown$ | variable |
+| Initial value of parameter P19 | $unknown$ | variable |
+| Initial value of parameter P20 | $unknown$ | variable |
+| Initial value of parameter P21 | $unknown$ | variable |
+| Initial value of parameter P22 | $unknown$ | variable |
+| Initial value of parameter P23 | $unknown$ | variable |
+| Initial value of parameter P24 | $unknown$ | variable |
+| Initial value of parameter P25 | $unknown$ | variable |
+| Initial value of parameter P26 | $unknown$ | variable |
+| Initial value of parameter P27 | $unknown$ | variable |
+| Initial value of parameter P28 | $unknown$ | variable |
+| Initial value of parameter P29 | $unknown$ | variable |
+| Initial value of parameter P30 | $unknown$ | variable |
+| Initial value of parameter P31 | $unknown$ | variable |
+| Initial value of parameter P32 | $unknown$ | variable |
+| Initial value of parameter P33 | $unknown$ | variable |
+| Initial value of parameter P34 | $unknown$ | variable |
+| Initial value of parameter P35 | $unknown$ | variable |
+| Initial value of parameter P36 | $unknown$ | variable |
+| Initial value of parameter P37 | $unknown$ | variable |]
+
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+
+*)
