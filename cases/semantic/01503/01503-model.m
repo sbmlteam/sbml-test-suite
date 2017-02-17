7f269101021b4bc3e0b6a0a79207a58203208f89
Lucian Smith <lpsmith@uw.edu>
2017-02-16 18:13:18 -0800

    A few fixes to the test.

--- cases/semantic/01503/01503-model.m
+++ cases/semantic/01503/01503-model.m
@@ -5,7 +5,7 @@ synopsis:        Several parameters with algebraic rules, testing various L1 bui
 componentTags:   AlgebraicRule, Parameter
 testTags:        InitialValueReassigned, UncommonMathML
 testType:        TimeCourse
-levels:          1.2, 2.1, 2.2, 2.3, 2.4, 2.5, 3.1, 3.2
+levels:          2.1, 2.2, 2.3, 2.4, 2.5, 3.1, 3.2
 generatedBy:     Analytic
 packagesPresent: 
 
