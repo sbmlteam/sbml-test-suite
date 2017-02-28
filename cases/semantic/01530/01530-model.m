ec77a7c9a00ec94804f8ad0f37370d43b7a8c44b
Lucian Smith <lpsmith@uw.edu>
2017-02-28 13:44:24 -0800

    Testing uncommon MathML in event triggers, assignments, priorities, and delays.

---
+++ cases/semantic/01530/01530-model.m
@@ -0,0 +1,132 @@
+(*
+
+category:        Test
+synopsis:        Uncommon MathML in event assignments.
+componentTags:   CSymbolTime, EventNoDelay, Parameter
+testTags:        NonConstantParameter, UncommonMathML
+testType:        TimeCourse
+levels:          2.1, 2.2, 2.3, 2.4, 2.5, 3.1, 3.2
+generatedBy:     Analytic
+packagesPresent: 
+
+The model tests various uncommon mathematical constructs in event assignments.
+
+The model contains:
+* 53 parameters (P0, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19, P20, P21, P22, P23, P24, P25, P26, P27, P28, P29, P30, P31, P32, P33, P34, P35, P36, P37, P38, P39, P40, P41, P42, P43, P44, P45, P46, P47, P48, P49, P52, P50, P51)
+
+There are 51 events:
+
+[{width:30em,margin: 1em auto}|  *Event*  |  *Trigger*  | *Event Assignments* |
+| E0 | $time > 1.5$ | $P0 = exponentiale$ |
+| E1 | $time > 1.5$ | $P1 = exp(exponentiale)$ |
+| E2 | $time > 1.5$ | $P2 = abs(-1)$ |
+| E3 | $time > 1.5$ | $P3 = abs(1)$ |
+| E4 | $time > 1.5$ | $P4 = acos(-1)$ |
+| E5 | $time > 1.5$ | $P5 = acos(0.5)$ |
+| E6 | $time > 1.5$ | $P6 = asin(1)$ |
+| E7 | $time > 1.5$ | $P7 = asin(-0.5)$ |
+| E8 | $time > 1.5$ | $P8 = atan(2.8)$ |
+| E9 | $time > 1.5$ | $P9 = atan(-7.09)$ |
+| E10 | $time > 1.5$ | $P10 = ceil(0.5)$ |
+| E11 | $time > 1.5$ | $P11 = ceil(3.55)$ |
+| E12 | $time > 1.5$ | $P12 = ceil(-4.6)$ |
+| E13 | $time > 1.5$ | $P13 = cos(9.1)$ |
+| E14 | $time > 1.5$ | $P14 = cos(-0.22)$ |
+| E15 | $time > 1.5$ | $P15 = exp(0)$ |
+| E16 | $time > 1.5$ | $P16 = exp(1)$ |
+| E17 | $time > 1.5$ | $P17 = exp(0.77)$ |
+| E18 | $time > 1.5$ | $P18 = floor(-4.6)$ |
+| E19 | $time > 1.5$ | $P19 = floor(9.1)$ |
+| E20 | $time > 1.5$ | $P20 = ln(0.2)$ |
+| E21 | $time > 1.5$ | $P21 = ln(1)$ |
+| E22 | $time > 1.5$ | $P22 = log10(0.2)$ |
+| E23 | $time > 1.5$ | $P23 = log10(1)$ |
+| E24 | $time > 1.5$ | $P24 = 1^2$ |
+| E25 | $time > 1.5$ | $P25 = 2^2$ |
+| E26 | $time > 1.5$ | $P26 = 1.4^5.1$ |
+| E27 | $time > 1.5$ | $P27 = 4^2$ |
+| E28 | $time > 1.5$ | $P28 = 3.1^2$ |
+| E29 | $time > 1.5$ | $P29 = sqrt(4)$ |
+| E30 | $time > 1.5$ | $P30 = sqrt(7.4)$ |
+| E31 | $time > 1.5$ | $P31 = sin(2.1)$ |
+| E32 | $time > 1.5$ | $P32 = sin(0)$ |
+| E33 | $time > 1.5$ | $P33 = sin(-5.9)$ |
+| E34 | $time > 1.5$ | $P34 = tan(0)$ |
+| E35 | $time > 1.5$ | $P35 = tan(1.11)$ |
+| E36 | $time > 1.5$ | $P36 = tan(-6)$ |
+| E37 | $time > 1.5$ | $P37 = sec(0.5)$ |
+| E38 | $time > 1.5$ | $P38 = csc(4.5)$ |
+| E39 | $time > 1.5$ | $P39 = cot(0.2)$ |
+| E40 | $time > 1.5$ | $P40 = sinh(0.3)$ |
+| E41 | $time > 1.5$ | $P41 = cosh(1.7)$ |
+| E42 | $time > 1.5$ | $P42 = arcsec(2.3)$ |
+| E43 | $time > 1.5$ | $P43 = arccsc(1.1)$ |
+| E44 | $time > 1.5$ | $P44 = arccot(-0.1)$ |
+| E45 | $time > 1.5$ | $P45 = arcsinh(99)$ |
+| E46 | $time > 1.5$ | $P46 = arccosh(1.34)$ |
+| E47 | $time > 1.5$ | $P47 = arctanh(-0.7)$ |
+| E48 | $time > 1.5$ | $P48 = arcsech(0.42)$ |
+| E49 | $time > 1.5$ | $P49 = arccsch(0.01)$ |
+| E50 | $time > 1.5$ | $P52 = times()$ |]
+
+The initial conditions are as follows:
+
+[{width:35em,margin: 1em auto}|       | *Value* | *Constant* |
+| Initial value of parameter P50 | $0$ | constant |
+| Initial value of parameter P51 | $0$ | constant |
+| Initial value of parameter P0 | $0$ | variable |
+| Initial value of parameter P1 | $0$ | variable |
+| Initial value of parameter P2 | $0$ | variable |
+| Initial value of parameter P3 | $0$ | variable |
+| Initial value of parameter P4 | $0$ | variable |
+| Initial value of parameter P5 | $0$ | variable |
+| Initial value of parameter P6 | $0$ | variable |
+| Initial value of parameter P7 | $0$ | variable |
+| Initial value of parameter P8 | $0$ | variable |
+| Initial value of parameter P9 | $0$ | variable |
+| Initial value of parameter P10 | $0$ | variable |
+| Initial value of parameter P11 | $0$ | variable |
+| Initial value of parameter P12 | $0$ | variable |
+| Initial value of parameter P13 | $0$ | variable |
+| Initial value of parameter P14 | $0$ | variable |
+| Initial value of parameter P15 | $0$ | variable |
+| Initial value of parameter P16 | $0$ | variable |
+| Initial value of parameter P17 | $0$ | variable |
+| Initial value of parameter P18 | $0$ | variable |
+| Initial value of parameter P19 | $0$ | variable |
+| Initial value of parameter P20 | $0$ | variable |
+| Initial value of parameter P21 | $0$ | variable |
+| Initial value of parameter P22 | $0$ | variable |
+| Initial value of parameter P23 | $0$ | variable |
+| Initial value of parameter P24 | $0$ | variable |
+| Initial value of parameter P25 | $0$ | variable |
+| Initial value of parameter P26 | $0$ | variable |
+| Initial value of parameter P27 | $0$ | variable |
+| Initial value of parameter P28 | $0$ | variable |
+| Initial value of parameter P29 | $0$ | variable |
+| Initial value of parameter P30 | $0$ | variable |
+| Initial value of parameter P31 | $0$ | variable |
+| Initial value of parameter P32 | $0$ | variable |
+| Initial value of parameter P33 | $0$ | variable |
+| Initial value of parameter P34 | $0$ | variable |
+| Initial value of parameter P35 | $0$ | variable |
+| Initial value of parameter P36 | $0$ | variable |
+| Initial value of parameter P37 | $0$ | variable |
+| Initial value of parameter P38 | $0$ | variable |
+| Initial value of parameter P39 | $0$ | variable |
+| Initial value of parameter P40 | $0$ | variable |
+| Initial value of parameter P41 | $0$ | variable |
+| Initial value of parameter P42 | $0$ | variable |
+| Initial value of parameter P43 | $0$ | variable |
+| Initial value of parameter P44 | $0$ | variable |
+| Initial value of parameter P45 | $0$ | variable |
+| Initial value of parameter P46 | $0$ | variable |
+| Initial value of parameter P47 | $0$ | variable |
+| Initial value of parameter P48 | $0$ | variable |
+| Initial value of parameter P49 | $0$ | variable |
+| Initial value of parameter P52 | $0$ | variable |]
+
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+
+*)
