9f979973f84df29a1c5c9e1d4ee8aecde0a77d11
Lucian Smith <lpsmith@uw.edu>
2017-03-01 13:11:28 -0800

    Testing assigning to a boundary species from elements with no MathML, leaving the species level unchanged.

---
+++ cases/semantic/01557/01557-model.m
@@ -0,0 +1,38 @@
+(*
+
+category:        Test
+synopsis:        A boundary species with an initial assignment with no math.
+componentTags:   Compartment, InitialAssignment, Reaction, Species
+testTags:        Amount, BoundaryCondition, NoMathML, NonUnityStoichiometry
+testType:        TimeCourse
+levels:          3.2
+generatedBy:     Analytic
+packagesPresent: 
+
+ In this model, the initial amount of a boundary species is 2, with an initial assignment to change it that doesn't have any mathML, leaving the value at 2.
+
+The model contains:
+* 2 species (S1, S2)
+* 1 compartment (C)
+
+There is one reaction:
+
+[{width:30em,margin: 1em auto}|  *Reaction*  |  *Rate*  |
+| J0: S1 -> 2S2 | $0.1$ |]
+
+The initial conditions are as follows:
+
+[{width:35em,margin: 1em auto}|       | *Value* | *Constant* |
+| Initial amount of species S1 | $2$ | variable |
+| Initial amount of species S2 | $3$ | variable |
+| Initial volume of compartment 'C' | $1$ | constant |]
+
+The species' initial quantities are given in terms of substance units to
+make it easier to use the model in a discrete stochastic simulator, but
+their symbols represent their values in concentration units where they
+appear in expressions.
+
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+
+*)
