3b193153f9444f7581d36a85f6a9cbd50d0f0675
Lucian Smith <lpsmith@uw.edu>
2017-03-14 15:40:51 -0700

    Spacing.

--- cases/semantic/01589/01589-model.m
+++ cases/semantic/01589/01589-model.m
@@ -1,12 +1,12 @@
 (*
 
 category:        Test
-synopsis:      Competing events with the same priority, jointly causing a parameter to monotonically increase, checking to make sure the two events are not exactly evently distributed.  NOTE:  STOCHASTIC TEST. Your software may fail periodically; it is only supposed to succeed in the majority of cases.
+synopsis:        Competing events with the same priority, jointly causing a parameter to monotonically increase, checking to make sure the two events are not exactly evently distributed.  NOTE:  STOCHASTIC TEST. Your software may fail periodically; it is only supposed to succeed in the majority of cases.
 componentTags:   AlgebraicRule, CSymbolTime, EventNoDelay, EventPriority, Parameter
 testTags:        EventIsNotPersistent, InitialValueReassigned, NonConstantParameter
 testType:        TimeCourse
 levels:          3.1, 3.2
-generatedBy:   Analytic
+generatedBy:     Analytic
 packagesPresent: 
 
 This model contains two events with the same trigger, the same effective priority, both set 'persistent=false', and both of which disable the trigger of the other.  This means that every .01 seconds, one fires and the other does not, at random, and increases the parameters Q or R, respectively.  A third parameter, S, is assigned the value of Q+R, meaning that it doesn't matter which one fires; S will increase monotonically.  A final parameter, 'error' checks to make sure neither Q nor R are chosen more frequently than the other--if the difference gets higher than 0.2, it triggers.  Note:  The 'errorLow' and 'errorHigh' parameters are a stochastic test, and may not always remain at '0' for all runs.  If your software fails, try running it again with a new random number seed, and it may succeed.  The values were chosen to be reasonable in the vast majority of cases, but still high enough to reveal problems in software that tends to pick both events either evenly or in an unbalanced manner.  This test is a repeat and synthesis of tests 952 and 962, with the additional wrinkle of 'S' being set with an algebraic rule instead of via an assignment rule.
