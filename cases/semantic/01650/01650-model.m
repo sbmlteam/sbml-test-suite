b698b4ca6e28a5b04fe7b9e2d25fab60e0fb5c1e
Lucian Smith <lpsmith@uw.edu>
2017-04-11 10:43:11 -0700

    Individual conversion factors duplicated in local parameters.

--- cases/semantic/01650/01650-model.m
+++ cases/semantic/01650/01650-model.m
@@ -1,7 +1,7 @@
 (*
 
 category:        Test
-synopsis:        Model and species conversion factors whose IDs are duplicated by local parameters.
+synopsis:        A model conversion factor whose ID is duplicated by a local parameter.
 componentTags:   Compartment, Parameter, Reaction, Species
 testTags:        Amount, ConversionFactors, LocalParameters
 testType:        TimeCourse
@@ -9,11 +9,11 @@ levels:          3.1, 3.2
 generatedBy:     Analytic
 packagesPresent: 
 
- In this model, model and species conversion factors IDs are duplicated by local parameters.
+ In this model, a model conversion factor ID is duplicated by a local parameter.
 
 The model contains:
 * 2 species (S1, S2)
-* 2 parameters (m_cf, s1_cf)
+* 1 parameter (m_cf)
 * 2 local parameters (J0.m_cf, J0.s1_cf)
 * 1 compartment (C)
 
@@ -23,7 +23,6 @@ There is one reaction:
 | J0: S1 -> S2 | $m_cf + s1_cf$ |]
 
 The conversion factor for the model is 'm_cf'
-The conversion factor for the species 'S1' is 's1_cf'
 
 The initial conditions are as follows:
 
@@ -31,7 +30,6 @@ The initial conditions are as follows:
 | Initial amount of species S1 | $2$ | variable |
 | Initial amount of species S2 | $3$ | variable |
 | Initial value of parameter m_cf | $3$ | constant |
-| Initial value of parameter s1_cf | $5$ | constant |
 | Initial value of local parameter 'J0.m_cf' | $0.003$ | constant |
 | Initial value of local parameter 'J0.s1_cf' | $0.007$ | constant |
 | Initial volume of compartment 'C' | $1$ | constant |]
