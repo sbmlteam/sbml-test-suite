345565700fbf5070ff9222ced4425d9c697589f6
Lucian Smith <lpsmith@uw.edu>
2017-04-14 18:09:56 -0700

    Persistent and non-persistent events that reference a stoichiometry.

---
+++ cases/semantic/01717/01717-model.m
@@ -0,0 +1,50 @@
+(*
+
+category:        Test
+synopsis:        A persistent event that references a stoichiometry.
+componentTags:   Compartment, EventWithDelay, Parameter, RateRule, Reaction, Species
+testTags:        Amount, AssignedVariableStoichiometry, EventIsPersistent, NonConstantParameter, NonUnityStoichiometry, SpeciesReferenceInMath
+testType:        TimeCourse
+levels:          3.1, 3.2
+generatedBy:     Analytic
+packagesPresent: 
+
+ In this model, a persistent event is triggered by changes in a stoichiometry.
+
+The model contains:
+* 1 species (S1)
+* 2 parameters (P1, k1)
+* 1 compartment (C)
+* 1 species reference (S1_stoich)
+
+There is one reaction:
+
+[{width:30em,margin: 1em auto}|  *Reaction*  |  *Rate*  |
+| J0: -> S1_stoich S1 | $0.1$ |]
+Note:  the following stoichiometries are set separately:  S1_stoich
+
+
+There is one event:
+
+[{width:35em,margin: 1em auto}|  *Event*  |  *Trigger*  |  *Delay*  | *Event Assignments* |
+| E1 | $(S1_stoich >= 2.3) && (S1_stoich <= 3.3)$ | $2$ | $P1 = 3$ |]
+
+
+There is one rule:
+
+[{width:30em,margin: 1em auto}|  *Type*  |  *Variable*  |  *Formula*  |
+| Rate | S1_stoich | $1$ |]
+
+The initial conditions are as follows:
+
+[{width:35em,margin: 1em auto}|       | *Value* | *Constant* |
+| Initial concentration of species S1 | $0$ | variable |
+| Initial value of parameter k1 | $2$ | constant |
+| Initial value of parameter P1 | $0$ | variable |
+| Initial volume of compartment 'C' | $1$ | constant |
+| Initial value of species reference 'S1_stoich' | $1$ | variable |]
+
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+
+*)
