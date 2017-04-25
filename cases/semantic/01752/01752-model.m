d96b5d817054d4452dd078ceae194ff74f302bbd
Lucian Smith <lpsmith@uw.edu>
2017-04-24 17:28:26 -0700

    A local parameter that shadows a boundary species.

---
+++ cases/semantic/01752/01752-model.m
@@ -0,0 +1,40 @@
+(*
+
+category:        Test
+synopsis:        A local parameter that shadows a boundary species.
+componentTags:   Compartment, Reaction, Species
+testTags:        Amount, BoundaryCondition, LocalParameters
+testType:        TimeCourse
+levels:          1.2, 2.1, 2.2, 2.3, 2.4, 2.5, 3.1
+generatedBy:     Analytic
+packagesPresent: 
+
+ In this model, a local parameter shadows a boundary species in that reaction.
+
+The model contains:
+* 2 species (S1, S2)
+* 1 local parameter (J0.S1)
+* 1 compartment (C)
+
+There is one reaction:
+
+[{width:30em,margin: 1em auto}|  *Reaction*  |  *Rate*  |
+| J0: S1 -> S2 | $S1$ |]
+
+The initial conditions are as follows:
+
+[{width:35em,margin: 1em auto}|       | *Value* | *Constant* |
+| Initial amount of species S1 | $2$ | variable |
+| Initial amount of species S2 | $3$ | variable |
+| Initial value of local parameter 'J0.S1' | $0.1$ | constant |
+| Initial volume of compartment 'C' | $1$ | constant |]
+
+The species' initial quantities are given in terms of substance units to
+make it easier to use the model in a discrete stochastic simulator, but
+their symbols represent their values in concentration units where they
+appear in expressions.
+
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+
+*)
