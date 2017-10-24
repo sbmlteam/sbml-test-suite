d37432a4a56f7f6552e6d1103cea40a01a9a6422
Lucian Smith <lpsmith@uw.edu>
2017-10-24 02:01:45 +0200

    Add note linking this test with 01779

--- cases/semantic/01506/01506-model.m
+++ cases/semantic/01506/01506-model.m
@@ -9,6 +9,8 @@ levels:          2.1, 2.2, 2.3, 2.4, 2.5, 3.1, 3.2
 generatedBy:     Numeric
 packagesPresent: 
 
+ NOTE:  This test is a more complicated version of test 1779.  If this isn't passing, make sure you pass test 1779 first, as it is likely that the issue here is actually reflective of the more fundamental issue tested in that test (i.e. the simultaneous assignment to a species concentration and compartment).
+ 
  A species in a non-unity compartment under control of a rate rule, and the species is set 'hasOnlySubstanceUnits=false', meaning that the species concentration is affected by the rate of change.  That value then gets assigned to the parameter 'x', since we typically only require Species output in amounts.  Partway through the simulation, an event changes the level of the species and of the compartment, forcing yet another change.
 
 The model contains:
