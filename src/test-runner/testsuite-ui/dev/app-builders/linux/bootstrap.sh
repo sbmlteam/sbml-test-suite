8260c0b80c08b4eec10d9557e98a915efcfb0307
Michael Hucka <mhucka@caltech.edu>
2017-10-25 21:24:13 -0700

    Work around Ubuntu-SWT interaction bug

--- src/test-runner/testsuite-ui/dev/app-builders/linux/bootstrap.sh
+++ src/test-runner/testsuite-ui/dev/app-builders/linux/bootstrap.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# The next line is a fix for an SWT interaction bug on Ubuntu 16.
+# See https://askubuntu.com/questions/761604/eclipse-not-working-in-16-04
+SWT_GTK3=0
 MYSELF=`which "$0" 2>/dev/null`
 [ $? -gt 0 -a -f "$0" ] && MYSELF="./$0"
 java=java
