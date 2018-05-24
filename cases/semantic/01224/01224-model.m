d74e08ae4019d495f871324232f1355d138ffc59
Lucian Smith <lpsmith@uw.edu>
2018-05-24 10:44:35 -0700

    L1v2 and l2v1 don't have initial assignments, and can't encode this model.

--- cases/semantic/01224/01224-model.m
+++ cases/semantic/01224/01224-model.m
@@ -5,7 +5,7 @@ synopsis:        A simple reaction whose identifier is used in an initial assign
 componentTags:   Compartment, InitialAssignment, Parameter, Reaction, Species
 testTags:        Amount, InitialValueReassigned
 testType:        TimeCourse
-levels:          1.2, 2.1, 2.2, 2.3, 2.4, 2.5, 3.1, 3.2
+levels:          2.2, 2.3, 2.4, 2.5, 3.1, 3.2
 generatedBy:     Analytic
 packagesPresent: 
 
