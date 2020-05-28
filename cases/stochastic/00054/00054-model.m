d1f99d1d54826c152c51f0b769618c076e59df40
Lucian Smith <lpsmith@uw.edu>
2020-05-27 18:37:42 -0700

    Add tests of the 'distrib' package.  For history before this commit, see https://github.com/luciansmith/stochastic-distrib-test-suite

---
+++ cases/stochastic/00054/00054-model.m
@@ -0,0 +1,41 @@
+(*
+
+category:        Test
+synopsis:        LaplaceDistribution test (one argument)
+componentTags:   Compartment, EventNoDelay, Parameter, RateRule, Species
+testTags:        Amount, HasOnlySubstanceUnits, NonConstantParameter
+testType:        StatisticalDistribution
+levels:          3.1, 3.2
+generatedBy:     Analytic
+packagesPresent: distrib
+
+Test of the LaplaceDistribution function with a single argument from the 'distrib' package.  The 'location' defaults to 0 in this situation.
+
+The model contains:
+* 1 species (X)
+* 1 parameter (t)
+* 1 compartment (C)
+
+There is one event:
+
+[{width:30em,margin: 1em auto}|  *Event*  |  *Trigger*  | *Event Assignments* |
+| E0 | $t >= 0.5$ | $X = laplace(2)$ |
+|  |  | $t = -0.5$ |]
+
+
+There is one rule:
+
+[{width:30em,margin: 1em auto}|  *Type*  |  *Variable*  |  *Formula*  |
+| Rate | t | $1$ |]
+
+The initial conditions are as follows:
+
+[{width:35em,margin: 1em auto}|       | *Value* | *Constant* |
+| Initial amount of species X | $0$ | variable |
+| Initial value of parameter t | $0$ | variable |
+| Initial volume of compartment 'C' | $1$ | constant |]
+
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+
+*)
---
+++ cases/stochastic/00074/00074-model.m
@@ -0,0 +1,42 @@
+(*
+
+category:        Test
+synopsis:        NormalDistribution test with upper and lower bounds
+componentTags:   Compartment, EventNoDelay, InitialAssignment, Parameter, RateRule, Species
+testTags:        Amount, HasOnlySubstanceUnits, InitialValueReassigned, NonConstantParameter
+testType:        StatisticalDistribution
+levels:          3.1, 3.2
+generatedBy:     Analytic
+packagesPresent: distrib
+
+Test of the NormalDistribution function from the 'distrib' package with upper and lower bounds, and defined with variance instead of standard deviation.
+
+The model contains:
+* 1 species (X)
+* 2 parameters (t, variance)
+* 1 compartment (C)
+
+There is one event:
+
+[{width:30em,margin: 1em auto}|  *Event*  |  *Trigger*  | *Event Assignments* |
+| E0 | $t >= 0.5$ | $X = normal(0, sqrt(variance), -0.5, 0.5)$ |
+|  |  | $t = -0.5$ |]
+
+
+There is one rule:
+
+[{width:30em,margin: 1em auto}|  *Type*  |  *Variable*  |  *Formula*  |
+| Rate | t | $1$ |]
+
+The initial conditions are as follows:
+
+[{width:35em,margin: 1em auto}|       | *Value* | *Constant* |
+| Initial amount of species X | $0$ | variable |
+| Initial value of parameter variance | $0.25$ | constant |
+| Initial value of parameter t | $0$ | variable |
+| Initial volume of compartment 'C' | $1$ | constant |]
+
+Note: The test data for this model was generated from an analytical
+solution of the system of equations.
+
+*)
