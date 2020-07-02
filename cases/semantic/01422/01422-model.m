102ccace400a374dbe2abe9291da959f8172ad2d
Lucian Smith <lpsmith@uw.edu>
2020-07-02 09:25:35 -0700

    Remove L1 versions of tests with negative stoichiometries.
    Negative stoichiometries were invalid in L1.

--- cases/semantic/01422/01422-model.m
+++ cases/semantic/01422/01422-model.m
@@ -5,7 +5,7 @@ synopsis:        Multiple species references to the same species
 componentTags:   Compartment, Reaction, Species
 testTags:        Amount, NonUnityStoichiometry
 testType:        TimeCourse
-levels:          1.2, 2.1, 2.2, 2.3, 2.4, 2.5, 3.1, 3.2
+levels:          2.1, 2.2, 2.3, 2.4, 2.5, 3.1, 3.2
 generatedBy:     Numeric
 packagesPresent: 
 
